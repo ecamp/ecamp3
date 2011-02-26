a04d737fa873f09c36a3d77376f70cacec2f4f0c
Urban Suppiger <u.suppiger@mybytes.ch>
2011-02-26 21:00:52 +0100

    Zend_Translate with gettext

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -71,6 +71,9 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	 */
 	protected function _initZtal()
 	{
+		//configure an autoload prefix for Ztal
+		Zend_Loader_Autoloader::getInstance()->registerNamespace('Ztal');
+		
 		//register the Ztal plugin
 		$plugin = new Ztal_Controller_Plugin_Ztal($this->getOption('ztal'));
 		Zend_Controller_Front::getInstance()->registerPlugin($plugin);
@@ -101,6 +104,90 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 			array('CampId' => '\d+', 'EntityId' => '\d+')));
 
 
+	}
+
+
+	/**
+	 * Basic setup of module support and layout support.
+	 *
+	 * @return void
+	 */
+	protected function _initBasicConfig()
+	{
+		// Set the timezone default
+		date_default_timezone_set('Europe/Zurich');
+
+		//configure the front controller to use modules and throw exceptions
+		Zend_Controller_Front::getInstance()
+			->throwExceptions(true);
+
+		// Configure the app namespace
+		$this->setAppNamespace('Application');
+
+		// create the app space autoloader
+		new Zend_Application_Module_Autoloader(array(
+			'basePath' => APPLICATION_PATH,
+			'namespace' => 'Application',));
+
+
+		//configure zend_layout
+		Zend_Layout::startMvc(array('layoutPath' => APPLICATION_PATH . '/layouts/scripts'));
+	}
+
+
+	/**
+	 * Init translation services and locale.
+	 *
+	 * @return void
+	 */
+	protected function _initTranslationService()
+	{
+		// Build the path for the languages folder in the current module
+		$languagesPath = APPLICATION_PATH . '/languages';
+
+		// Setup a cache
+		$frontendOptions = array();
+		$backendOptions = array();
+
+		$frontendOptions['automatic_serialization'] = true;
+		$frontendOptions['lifetime'] = '604800';
+		$frontendOptions['write_control'] = false;
+		$frontendOptions['master_files'] = array($languagesPath . '/en/default.mo');
+
+		$backendOptions['cache_dir'] = APPLICATION_PATH . '/../tmp/';
+		$backendOptions['hashed_directory_level'] = 1;
+
+		$cache = Zend_Cache::factory('File', 'File', $frontendOptions, $backendOptions);
+
+		Zend_Translate::setCache($cache);
+		Zend_Locale::setCache($cache);
+
+		// Create the translators
+		$translator_en = new Zend_Translate(array(
+			'adapter' => 'gettext',
+			'content' => $languagesPath . '/en/default.mo',
+			'locale'  => 'en'));
+
+		$translator_de = new Zend_Translate(array(
+			'adapter' => 'gettext',
+			'content' => $languagesPath . '/de/default.mo',
+			'locale'  => 'de'));
+
+		// Register the translator for system-wide use based on browser settings
+		// TODO: change to user settings later
+		$locale = new Zend_Locale();
+		switch($locale->getLanguage())
+		{
+			case 'de':
+				Zend_Registry::set('Zend_Translate', $translator_de);
+				break;
+			default:
+				Zend_Registry::set('Zend_Translate', $translator_en);
+				break;
+		}
+		
+		// Register the locale for system-wide use
+		Zend_Registry::set('Zend_Locale', new Zend_Locale($locale->getRegion()));
 	}
 }
 
