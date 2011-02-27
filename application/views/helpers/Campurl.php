9fdc1864ff29684ce59acfcd76e6a0fbda61a5e2
Urban Suppiger <u.suppiger@mybytes.ch>
2011-02-27 13:11:18 +0100

    add URL helpers

---
+++ application/views/helpers/Campurl.php
@@ -0,0 +1,24 @@
+<?php
+
+class Zend_View_Helper_Campurl extends Zend_View_Helper_Abstract
+{
+	public $view;
+
+	/**
+	 * this helper automatically adds the camp parameter to the url
+	 */
+    public function campurl( $urlOptions, $name = null, $reset = false, $encode = true)
+    {
+        $urlOptions['camp'] = Zend_Controller_Front::getInstance()->getRequest()->getParam('camp');
+	    
+	    if( ! isset($urlOptions['controller']))
+	       $urlOptions['controller'] = Zend_Controller_Front::getInstance()->getRequest()->getControllerName();
+			       
+        return $this->view->url( $urlOptions, $name, $reset, $encode);
+    }
+
+    public function setView(Zend_View_Interface $view)
+    {
+        $this->view = $view;
+    }
+}
