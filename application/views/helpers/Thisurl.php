8b5c0b772ee1459a40e134196023db3db40907d1
Urban Suppiger <u.suppiger@mybytes.ch>
2011-02-28 23:59:04 +0100

    workaround for method=Constructor problem

--- application/views/helpers/Thisurl.php
+++ application/views/helpers/Thisurl.php
@@ -6,7 +6,13 @@ class Thisurl extends \Zend_View_Helper_Abstract
 {
 	public $view;
 
-    public function thisurl( $urlOptions, $name = null, $reset = false, $encode = true)
+	public function __call($name, $arguments)
+	{
+		if( $name == "thisurl" )
+			return call_user_func_array(array($this,"execute"), $arguments);
+	}
+
+    public function execute( $urlOptions, $name = null, $reset = false, $encode = true)
     {
 	    if( ! isset($urlOptions['controller']))
 	       $urlOptions['controller'] = \Zend_Controller_Front::getInstance()->getRequest()->getControllerName();
