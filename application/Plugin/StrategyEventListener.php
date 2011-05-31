6ec3391f771852f46b12774b5b3963379950bc49
Pirmin Mattmann <forte@musegg.ch>
2011-05-31 21:53:55 +0200

    Bugfixes of require_once and Namespace declaration

--- application/Plugin/StrategyEventListener.php
+++ application/Plugin/StrategyEventListener.php
@@ -17,11 +17,11 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with eCamp.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+namespace Plugin;
  
 use \Doctrine\ORM,
 	\Doctrine\Common;
-
-namespace Plugin;
 	
 /**
  * The PluginStrategyEventListener will initialize a strategy after the
