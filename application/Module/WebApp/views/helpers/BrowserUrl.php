4e3b52700e97e43799dda516688677ec660da15d
Pirmin Mattmann <forte@musegg.ch>
2011-12-12 13:50:02 +0100

    Changed browserUrl for simpler usage
    The BrowserUrl now does recognize the new Controller and Action by it self.
    The URL is created by the Router. If a different Router then the current has to be used, it can be specified.
    Additionally, any parameter to the url can be attached.

--- application/Module/WebApp/views/helpers/BrowserUrl.php
+++ application/Module/WebApp/views/helpers/BrowserUrl.php
@@ -4,11 +4,24 @@ namespace WebApp\View\Helper;
 
 class BrowserUrl extends \Zend_View_Helper_Abstract
 {
-	
-	public function browserUrl($url = '/')
+
+	public function browserUrl($param = array(), $route = null)
 	{
+		$r = \Zend_Controller_Front::getInstance()->getRequest();
+		
+		$urlArray = 
+			array(
+				$r->getActionKey() 		=> $r->getActionName(), 
+				$r->getControllerKey() 	=> $r->getControllerName(), 
+				$r->getModuleKey() 		=> $r->getModuleName()
+			);
+		
+		$urlArray = array_merge($urlArray, $param);
+		
+		$url = $this->view->url($urlArray, $route, true);
+		
 		$this->view->BrowserUrlScript = 
-		"<script type='text/javascript'> window.history.replaceState({}, '', '$url'); </script>";
+			"<script type='text/javascript'> window.history.replaceState({}, '', '$url'); </script>";
 	}
 	
 }
