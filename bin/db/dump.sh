c58ed39e3fbe601a0e47c8c244199a774fa11ace
Pirmin Mattmann <forte@musegg.ch>
2012-03-07 23:11:33 +0100

    added --skip-comments to mysqldump

--- bin/db/dump.sh
+++ bin/db/dump.sh
@@ -4,5 +4,5 @@ if [ ! -d $DATAPATH ]; then
 	mkdir -p $DATAPATH
 fi
 
-mysqldump -u $DBUSER -p$DBPASS --skip-dump-date $DB > $FILE
+mysqldump -u $DBUSER -p$DBPASS --skip-dump-date --skip-comments $DB > $FILE
 
