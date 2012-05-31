7da04fac9c139aadbc51c589192e484d3253597b
Aurelian Ammon <aurelianammon@Aurelian-Ammons-MacBook-Pro.local>
2012-05-31 12:01:18 +0200

    admin_RequestGroup 2

--- application/CoreApi/Service/GroupRequestService.php
+++ application/CoreApi/Service/GroupRequestService.php
@@ -10,9 +10,9 @@ use CoreApi\Entity\Group;
 use CoreApi\Entity\GroupRequest;
 
 /**
- * @method CoreApi\Service\GroupService Simulate
+ * @method CoreApi\Service\GroupRequestService Simulate
  */
-class GroupRequestService
+class GroupRequestService 
 	extends ServiceBase
 {
 	/**
@@ -34,12 +34,11 @@ class GroupRequestService
 	public function _setupAcl()
 	{
 		$this->acl->allow(DefaultAcl::MEMBER, $this, 'Get');
-		$this->acl->allow(DefaultAcl::MEMBER, $this, 'GetRoots');
-		$this->acl->allow(DefaultAcl::MEMBER, $this, 'RequestGroup');
-		$this->acl->allow(DefaultAcl::GROUP_MEMBER, $this, 'CreateCamp');
-		$this->acl->allow(DefaultAcl::GROUP_MEMBER, $this, 'UpdateCamp');
-		$this->acl->allow(DefaultAcl::GROUP_MEMBER, $this, 'DeleteCamp');
 		
+		$this->acl->allow(DefaultAcl::MEMBER, $this, 'RequestGroup');
+
+		$this->acl->allow(DefaultAcl::GROUP_MEMBER, $this, 'acceptGroupRequest');
+		$this->acl->allow(DefaultAcl::GROUP_MEMBER, $this, 'rejectGroupRequest');
 	}
 	
 	/**
@@ -61,18 +60,6 @@ class GroupRequestService
 		{	return $id;	}
 		
 		return null;
-	}
-	
-	/**
-	 * Return all root groups
-	 * @return
-	 */
-	public function GetRoots()
-	{
-		return $this->groupRepo->createQueryBuilder("g")
-					->where("g.parent IS NULL ")
-					->getQuery()
-					->getResult();
 	}
 	
 	/**
@@ -121,87 +108,13 @@ class GroupRequestService
 		}
 	}
 	
-	/**
-	 * Creates a new Camp
-	 * @return \CoreApi\Entity\Camp
-	 */
-	public function CreateCamp(\Zend_Form $form)
+	public function acceptGroupRequest() 
 	{
-		/* check if camp with same name already exists */
-		if( ! $this->isCampNameUnique($form->getValue("name")) )
-		{
-			$form->getElement('name')->addError("Camp with same name already exists.");
-			$this->validationFailed();
-		}
 
-		/* create camp */
-		$camp = $this->campService->Create($form);
-		$camp->setGroup($this->contextProvider->getContext()->getGroup());
-		
-		return $camp;
 	}
 	
-	/**
-	 * Updates a Camp
-	 * @return \CoreApi\Entity\Camp
-	 */
-	public function UpdateCamp(\Zend_Form $form)
+	public function rejectGroupRequest()
 	{
-		$camp = $this->campService->Get($form->getValue('id'));
 		
-		if($camp->getGroup() != $this->contextProvider->getContext()->getGroup())
-			throw new \Exception("No Access");
-		
-		if( $form->getValue('name') != $camp->getName() )
-		{
-			if( ! $this->isCampNameUnique($form->getValue("name")) )
-			{
-				$form->getElement('name')->addError("Camp with same name already exists.");
-				$this->validationFailed();
-			}
-		}
-	
-		/* update camp */
-		$camp = $this->campService->Update($camp, $form);
-	
-		return $camp;
 	}
-	
-	public function DeleteCamp($id)
-	{
-		$camp = $this->campService->Get($id);
-		
-		if( $camp == null )
-			return false;
-		
-		if($camp->getGroup() != $this->contextProvider->getContext()->getGroup())
-			throw new \Exception("No Access");
-		
-		$this->campService->Delete($camp);
-		
-		return true;
-	}
-	
-	/**
-	 * @return bool
-	 */
-	private function isCampNameUnique($name)
-	{
-		/* check if camp with same name already exists */
-		$qb = $this->em->createQueryBuilder();
-		$qb->add('select', 'c')
-		->add('from', '\CoreApi\Entity\Camp c')
-		->add('where', 'c.group = ?1 AND c.name = ?2')
-		->setParameter(1,$this->contextProvider->getContext()->getGroup()->getId())
-		->setParameter(2, $name);
-		
-		$query = $qb->getQuery();
-		
-		if( count($query->getArrayResult()) > 0 ){
-			return false;
-		}
-		
-		return true;
-	}
-	
 }
