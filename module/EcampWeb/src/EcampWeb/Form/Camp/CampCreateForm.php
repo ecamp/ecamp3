9d5cfc26d17f0faacaf4516e2314012ca3ea0b50
Urban Suppiger <u.suppiger@mybytes.ch>
2015-01-04 13:33:59 +0100

    typo

--- module/EcampWeb/src/EcampWeb/Form/Camp/CampCreateForm.php
+++ module/EcampWeb/src/EcampWeb/Form/Camp/CampCreateForm.php
@@ -89,8 +89,6 @@ class CampCreateForm extends AjaxBaseForm implements InputFilterProviderInterfac
 
     public function getInputFilterSpecification()
     {
-        $this->
-
         return array(
             array(
                 'name' => 'name',
