6a2f8f03096a786251efc3550d315b1bb2979b93
Pirmin Mattmann <forte@musegg.ch>
2015-03-10 23:47:44 +0100

    Cleanup

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -16,29 +16,8 @@ class Bootstrap
     public static function init()
     {
         require './init_autoloader.php';
-        require_once './config/define.php';
 
-
-        // use ModuleManager to load this module and it's dependencies
-        $config = array(
-            'module_listener_options' => array(
-                'module_paths' => array(
-                    './module',
-                    './vendor',
-                    './plugins'
-                ),
-                'config_glob_paths' => array(
-                    'config/app.autoload/{,*.}{global,local}.php',
-                    'config/common.autoload/{,*.}{global,local}.php',
-                )
-            ),
-            'modules' => array(
-                'EcampLib',
-                'EcampCore',
-                'DoctrineModule',
-                'DoctrineORMModule',
-            )
-        );
+        $config = include './config/application.config.php';
 
         $serviceManager = new ServiceManager(new ServiceManagerConfig());
         $serviceManager->setService('ApplicationConfig', $config);
