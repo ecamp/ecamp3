eaedaf32ea595a5d22141923eaa60a3670f44953
Pirmin Mattmann <pimattmann@gmail.com>
2018-01-28 12:28:49 +0100

    path fix part2

---
+++ module/eCampLib/config/module.config.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'doctrine' => [
+        'driver' => [
+            'ecamp_lib_entities' => [
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => [ __DIR__ . '/../src/Entity' ]
+            ],
+
+            'orm_default' => [
+                'drivers' => [
+                    'eCamp\Lib\Entity' => 'ecamp_lib_entities'
+                ]
+            ]
+        ]
+    ],
+
+];
