d110b86aec1915ff93cac73983d688bfed3403f4
Pirmin Mattmann <pimattmann@gmail.com>
2018-04-08 16:38:35 +0200

    Default-Filter für Event

--- module/eCampCore/src/Service/EventService.php
+++ module/eCampCore/src/Service/EventService.php
@@ -2,6 +2,7 @@
 
 namespace eCamp\Core\Service;
 
+use eCamp\Core\Entity\Camp;
 use eCamp\Core\Hydrator\EventHydrator;
 use eCamp\Core\Entity\Event;
 use eCamp\Lib\Service\BaseService;
@@ -10,6 +11,20 @@ class EventService extends BaseService
 {
     public function __construct(EventHydrator $eventHydrator) {
         parent::__construct($eventHydrator, Event::class);
+    }
+
+    protected function fetchAllQueryBuilder($params = []) {
+        $q = parent::fetchAllQueryBuilder($params);
+        $q->andWhere($this->createFilter($q, Camp::class, 'row', 'camp'));
+
+        return $q;
+    }
+
+    protected function fetchQueryBuilder($id) {
+        $q = parent::fetchQueryBuilder($id);
+        $q->andWhere($this->createFilter($q, Camp::class, 'row', 'camp'));
+
+        return $q;
     }
 
 }
