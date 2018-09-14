bdff4d539801b5a88a6c326e5dabc9428b65b8c0
Carlo Beltrame <beltrame@puzzle.ch>
2018-09-14 14:45:03 +0200

    Add saving API call and reformat template

--- public/js/CampDetails.js
+++ public/js/CampDetails.js
@@ -3,10 +3,25 @@ Vue.component('camp-details', {
     data: function() {
         return {
             editing: false,
+            campDetails: {},
         }
     },
+    created: function() {
+        this.campDetails = this.fetchFromAPI();
+    },
     computed: {
-        campDetails: function() {
+        ownerName: function () {
+            return this.campDetails._embedded.owner.name;
+        },
+        periods: function () {
+            return this.campDetails._embedded.periods;
+        },
+        buttonText: function () {
+            return this.editing ? 'Speichern' : 'Bearbeiten';
+        },
+    },
+    methods: {
+        fetchFromAPI: function() {
             // Since we don't yet include vue.js with NPM and webpack, we cannot use npm modules such as vue-fetch, and
             // also cannot use advanced javascript features such as async / await (which are required for the
             // fetch API). Therefore, to prove our point, we use a good old-fashioned XMLHttpRequest here to query our
@@ -22,41 +37,52 @@ Vue.component('camp-details', {
 
             return JSON.parse(request.responseText);
         },
-        ownerName: function() {
-            return this.campDetails._embedded.owner.name;
+        saveToAPI: function() {
+            var request = new XMLHttpRequest();
+            request.open("PUT", "/api/camp/" + this.campId, false);
+            request.send(this.campDetails);
+
+            if (request.status !== 200) {
+                console.log('can not save camp details');
+                return this.campDetails;
+            }
+
+            console.log(request.responseText);
+            return JSON.parse(request.responseText);
         },
-        periods: function() {
-            return this.campDetails._embedded.periods;
-        },
-        buttonText: function() {
-            return this.editing ? 'Speichern' : 'Bearbeiten';
+        toggleEdit: function() {
+            if (this.editing) {
+                this.campDetails = this.saveToAPI();
+            }
+            this.editing = !this.editing;
         },
     },
-    template:
-        '        <div class="card" style="margin-bottom: 10px">\n' +
-        '            <div class="card-body">\n' +
-        '                <button style="float: right" class="btn btn-sm" v-bind:class="{ \'btn-primary\': editing, \'btn-outline-primary\': !editing }" v-on:click="editing=!editing">{{ buttonText }}</button>' +
-        '                Vue.js Infos zu genau einem Lager, id {{ campId }}\n' +
-        '                <ul v-bind:style="{display: editing ? \'none\' : \'\'}">\n' +
-        '                    <li>Name: {{ campDetails.name }}</li>\n' +
-        '                    <li>Titel: {{ campDetails.title }}</li>\n' +
-        '                    <li>Motto: {{ campDetails.motto }}</li>\n' +
-        '                    <li>Besitzer Name: {{ ownerName }}</li>\n' +
-        '                    <li>Lager-Perioden:' +
-        '                        <ul><li v-for="period in periods">{{ period.description }} ({{ period.start }} - {{ period.end }})</li></ul>' +
-        '                    </li>\n' +
-        '                </ul>\n' +
-        '                <ul v-bind:style="{display: editing ? \'\' : \'none\'}">\n' +
-        '                    <li>Name: {{ campDetails.name }}</li>\n' +
-        '                    <li>Titel: <input v-model="campDetails.title"></li>\n' +
-        '                    <li>Motto: <input v-model="campDetails.motto"></li>\n' +
-        '                    <li>Besitzer Name: {{ ownerName }}</li>\n' +
-        '                    <li>Lager-Perioden:' +
-        '                        <ul><li v-for="period in periods">{{ period.description }} ({{ period.start }} - {{ period.end }})</li></ul>' +
-        '                    </li>\n' +
-        '                </ul>\n' +
-        '            </div>\n' +
-        '        </div>'
+    template: '\
+        <div class="card" style="margin-bottom: 10px">\
+            <div class="card-body">\
+                <button style="float: right" class="btn btn-sm" v-bind:class="{ \'btn-primary\': editing, \'btn-outline-primary\': !editing }" v-on:click="toggleEdit">{{ buttonText }}</button>\
+                Vue.js Infos zu genau einem Lager, id {{ campId }}\
+                <ul v-bind:style="{display: editing ? \'none\' : \'\'}">\
+                    <li>Name: {{ campDetails.name }}</li>\
+                    <li>Titel: {{ campDetails.title }}</li>\
+                    <li>Motto: {{ campDetails.motto }}</li>\
+                    <li>Besitzer Name: {{ ownerName }}</li>\
+                    <li>Lager-Perioden:\
+                        <ul><li v-for="period in periods">{{ period.description }} ({{ period.start }} - {{ period.end }})</li></ul>\
+                    </li>\
+                </ul>\
+                <ul v-bind:style="{display: editing ? \'\' : \'none\'}">\
+                    <li>Name: {{ campDetails.name }}</li>\
+                    <li>Titel: <input class="form-control" v-model="campDetails.title"></li>\
+                    <li>Motto: <input class="form-control" v-model="campDetails.motto"></li>\
+                    <li>Besitzer Name: {{ ownerName }}</li>\
+                    <li>Lager-Perioden:\
+                        <ul><li v-for="period in periods">{{ period.description }} ({{ period.start }} - {{ period.end }})</li></ul>\
+                    </li>\
+                </ul>\
+            </div>\
+        </div>\
+        ',
 });
 
 new Vue({ el: '#camp-details' });
