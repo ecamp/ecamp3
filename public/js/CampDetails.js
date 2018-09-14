a20b75aacfbb1a1b127a1f4b341f79a1a69de823
Carlo Beltrame <beltrame@puzzle.ch>
2018-09-14 16:30:59 +0200

    Merge the two versions of the detail display and enable pressing enter to submit

--- public/js/CampDetails.js
+++ public/js/CampDetails.js
@@ -60,26 +60,21 @@ Vue.component('camp-details', {
         <div v-for="message in messages" role="alert" class="alert" v-bind:class="\'alert-\' + message.type">{{ message.text }}</div>\
         <div class="card" style="margin-bottom: 10px">\
             <div class="card-body">\
-                <button style="float: right" class="btn btn-sm" v-bind:class="{ \'btn-primary\': editing, \'btn-outline-primary\': !editing }" v-on:click="toggleEdit">{{ buttonText }}</button>\
-                Vue.js Infos zu genau einem Lager, id {{ campId }}\
-                <ul v-bind:style="{display: editing ? \'none\' : \'\'}">\
-                    <li>Name: {{ campDetails.name }}</li>\
-                    <li>Titel: {{ campDetails.title }}</li>\
-                    <li>Motto: {{ campDetails.motto }}</li>\
-                    <li>Besitzer Name: {{ ownerName }}</li>\
-                    <li>Lager-Perioden:\
-                        <ul><li v-for="period in periods">{{ period.description }} ({{ period.start }} - {{ period.end }})</li></ul>\
-                    </li>\
-                </ul>\
-                <ul v-bind:style="{display: editing ? \'\' : \'none\'}">\
-                    <li>Name: {{ campDetails.name }}</li>\
-                    <li>Titel: <input class="form-control" v-model="campDetails.title"></li>\
-                    <li>Motto: <input class="form-control" v-model="campDetails.motto"></li>\
-                    <li>Besitzer Name: {{ ownerName }}</li>\
-                    <li>Lager-Perioden:\
-                        <ul><li v-for="period in periods">{{ period.description }} ({{ period.start }} - {{ period.end }})</li></ul>\
-                    </li>\
-                </ul>\
+                <form v-on:submit.prevent="toggleEdit">\
+                    <button type="submit" style="float: right" class="btn btn-sm" v-bind:class="{ \'btn-primary\': editing, \'btn-outline-primary\': !editing }">{{ buttonText }}</button>\
+                    Vue.js Infos zu genau einem Lager, id {{ campId }}\
+                    <ul>\
+                        <li>Name: {{ campDetails.name }}</li>\
+                        <li v-bind:style="{display: editing ? \'none\' : \'\'}">Titel: {{ campDetails.title }}</li>\
+                        <li v-bind:style="{display: editing ? \'\' : \'none\'}">Titel: <input class="form-control" v-model="campDetails.title"></li>\
+                        <li v-bind:style="{display: editing ? \'none\' : \'\'}">Motto: {{ campDetails.motto }}</li>\
+                        <li v-bind:style="{display: editing ? \'\' : \'none\'}">Motto: <input class="form-control" v-model="campDetails.motto"></li>\
+                        <li>Besitzer Name: {{ ownerName }}</li>\
+                        <li>Lager-Perioden:\
+                            <ul><li v-for="period in periods">{{ period.description }} ({{ period.start }} - {{ period.end }})</li></ul>\
+                        </li>\
+                    </ul>\
+                </form>\
             </div>\
         </div>\
     </div>\
