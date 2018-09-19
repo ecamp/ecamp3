7224ef8129d87b2b2fe80e604d0f9bdf739d4ed4
Carlo Beltrame <beltrame@puzzle.ch>
2018-09-19 15:35:13 +0200

    Make root component handling more generic

--- module/eCampWeb/vue/src/main.js
+++ module/eCampWeb/vue/src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import CampDetails from './components/camp-details'
 
-new Vue({
-    el: '#camp-details',
-    render(h) {
-        return h(CampDetails, { props: this.$el.dataset } );
-    },
-});
+const vueApps = {
+    'camp-details': CampDetails,
+};
+
+for (let id in vueApps) {
+    if (!vueApps.hasOwnProperty(id)) continue;
+    if (document.getElementById(id)) {
+        new Vue({
+            el: '#' + id,
+            render(h) {
+                return h(vueApps[id], { props: this.$el.dataset });
+            }
+        });
+    }
+}
