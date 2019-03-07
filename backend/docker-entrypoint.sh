45d0b66e2ad1c15e5be3b8a33ca224dfb8a55575
Carlo Beltrame <carlo@beltra.me>
2019-03-07 10:41:57 +0100

    Rename DB check PHP script

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -6,7 +6,7 @@ DB_CONFIG_FILE="config/autoload/doctrine.local.dev.php"
 if [ ! -f "$DB_CONFIG_FILE" ]; then
     sed 's/127.0.0.1/db/g' config/autoload/doctrine.local.dev.dist > "$DB_CONFIG_FILE"
 fi
-php bin/check-db-connection.php
+php bin/wait-for-db.php
 vendor/bin/doctrine orm:schema-tool:update --force --complete
 
 mkdir -p data/DoctrineORMModule/Proxy
