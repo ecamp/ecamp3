91d1fe5f095cfde4e65003f3dbeab40ff2cf298a
Carlo Beltrame <carlo@beltra.me>
2019-04-08 16:34:54 +0200

    Make backend wait for composer install before attempting to start

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,6 +4,7 @@ DB_CONFIG_FILE="config/autoload/doctrine.local.dev.php"
 if [ ! -s "$DB_CONFIG_FILE" ]; then
     sed 's/127.0.0.1/db/g' config/autoload/doctrine.local.dev.dist > "$DB_CONFIG_FILE"
 fi
+php bin/wait-for-composer-install.php
 php bin/wait-for-db.php
 vendor/bin/doctrine orm:schema-tool:update --force --complete
 
