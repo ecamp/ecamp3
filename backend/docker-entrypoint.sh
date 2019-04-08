5cd9a1c01ea029d6ab46128c4da4fc53579a1704
Carlo Beltrame <carlo@beltra.me>
2019-04-08 15:56:23 +0200

    Extract composer into a separate container and minimize read-write access of containers

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-composer install --ignore-platform-reqs --no-interaction --no-plugins --no-scripts --prefer-dist
-
 DB_CONFIG_FILE="config/autoload/doctrine.local.dev.php"
-if [ ! -f "$DB_CONFIG_FILE" ]; then
+if [ ! -s "$DB_CONFIG_FILE" ]; then
     sed 's/127.0.0.1/db/g' config/autoload/doctrine.local.dev.dist > "$DB_CONFIG_FILE"
 fi
 php bin/wait-for-db.php
 vendor/bin/doctrine orm:schema-tool:update --force --complete
-
-composer di-generate-aot
 
 apache2-foreground
