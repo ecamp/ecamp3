559df75d8709d5da3db6d6de3f3e0db77c4794be
Manuel Meister <news.manuelsworld@gmail.com>
2019-04-08 21:39:45 +0200

    Removed unnecessary card

--- frontend/src/components/camp/Periods.vue
+++ frontend/src/components/camp/Periods.vue
@@ -12,58 +12,54 @@ Displays periods of a single camp.
       class="alert">
       {{ message.text }}
     </div>
-    <div class="card camp-detail-card">
-      <div class="card-body">
-        Vue.js Infos zu den Perioden genau eines Lagers
-        <ul>
-          <li
-            v-for="period in periods"
-            :key="period.id">
-            {{ period.description }} ({{ period.start }} - {{ period.end }})
-          </li>
-        </ul>
-      </div>
-    </div>
+    Vue.js Infos zu den Perioden genau eines Lagers
+    <ul>
+      <li
+        v-for="period in periods"
+        :key="period.id">
+        {{ period.description }} ({{ period.start }} - {{ period.end }})
+      </li>
+    </ul>
   </div>
 </template>
 <script>
-export default {
-  name: 'Periods',
-  props: {
-    campId: { type: String, required: true }
-  },
-  data () {
-    return {
-      editing: false,
-      campDetails: { title: '', motto: '', _embedded: { owner: {} } },
-      messages: []
-    }
-  },
-  computed: {
-    periods () {
-      if (this.campDetails._embedded == null) return []
-      return this.campDetails._embedded.periods
+  export default {
+    name: 'Periods',
+    props: {
+      campId: {type: String, required: true}
     },
-    buttonText () {
-      return this.editing ? 'Speichern' : 'Bearbeiten'
+    data() {
+      return {
+        editing: false,
+        campDetails: {title: '', motto: '', _embedded: {owner: {}}},
+        messages: []
+      }
     },
-    apiUrl () {
-      return process.env.VUE_APP_ROOT_API + '/camp/' + this.campId
-    }
-  },
-  created () {
-    this.fetchFromAPI()
-  },
-  methods: {
-    async fetchFromAPI () {
-      try {
-        this.campDetails = (await this.axios.get(this.apiUrl)).data
-      } catch (error) {
-        this.messages = [{ type: 'danger', text: 'Could not get camp details for id ' + this.campId + '. ' + error }]
+    computed: {
+      periods() {
+        if (this.campDetails._embedded == null) return []
+        return this.campDetails._embedded.periods
+      },
+      buttonText() {
+        return this.editing ? 'Speichern' : 'Bearbeiten'
+      },
+      apiUrl() {
+        return process.env.VUE_APP_ROOT_API + '/camp/' + this.campId
+      }
+    },
+    created() {
+      this.fetchFromAPI()
+    },
+    methods: {
+      async fetchFromAPI() {
+        try {
+          this.campDetails = (await this.axios.get(this.apiUrl)).data
+        } catch (error) {
+          this.messages = [{type: 'danger', text: 'Could not get camp details for id ' + this.campId + '. ' + error}]
+        }
       }
     }
   }
-}
 </script>
 <style scoped>
   .camp-detail-card {
