d2101fe534fbdb53f2df95ecb4ee782071c9c8b3
Carlo Beltrame <carlo@beltra.me>
2019-04-09 16:00:45 +0200

    Use delegated everywhere, use volumes for write-intensive folders and copy vendor and node_modules back to host

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,6 +2,11 @@
 
 chmod -R a+rw data
 
+DB_CONFIG_FILE="config/autoload/doctrine.local.dev.php"
+if [ ! -f "$DB_CONFIG_FILE" ]; then
+    sed 's/127.0.0.1/db/g' config/autoload/doctrine.local.dev.dist > "$DB_CONFIG_FILE"
+fi
+
 php bin/wait-for-composer-install.php
 php bin/wait-for-db.php
 vendor/bin/doctrine orm:schema-tool:update --force --complete
