e6dc2907cbec85e6637fb27b19c1c4c9d034e8f1
Carlo Beltrame <carlo@beltra.me>
2019-04-11 16:58:38 +0200

    Only set the hosts file entry on Linux hosts

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,11 +2,13 @@
 
 chmod -R a+rw data
 
-# Add hostname of docker host for XDebug to connect to. This can be removed once host.docker.internal is also supported in Docker for Linux (currently is only Win and Mac)
-if ! grep "host.docker.internal" /etc/hosts > /dev/null ; then
-    DOCKER_INTERNAL_IP=`/sbin/ip -4 route list match 0/0 | awk '{ print $3 }' | head -n 1`
-    echo -e "$DOCKER_INTERNAL_IP\thost.docker.internal" >> /etc/hosts
-    echo "Added host.docker.internal to /etc/hosts"
+# Add hostname of docker host for XDebug to connect to, on Linux. On Win and Mac, Docker automatically resolves host.docker.internal
+if [[ ! `getent hosts host.docker.internal | cut -d' ' -f1` ]]; then
+    if ! grep "host.docker.internal" /etc/hosts > /dev/null ; then
+        DOCKER_INTERNAL_IP=`/sbin/ip -4 route list match 0/0 | awk '{ print $3 }' | head -n 1`
+        echo -e "$DOCKER_INTERNAL_IP\thost.docker.internal" >> /etc/hosts
+        echo "Added host.docker.internal to /etc/hosts"
+    fi
 fi
 touch /var/log/xdebug.log && chmod a+rw /tmp/xdebug.log
 
