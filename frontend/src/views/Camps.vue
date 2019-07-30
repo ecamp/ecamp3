08808519eee1ae230766452a266c63e6f59313ca
Carlo Beltrame <carlo@beltra.me>
2019-07-30 17:47:52 +0200

    Simple demonstration of the simple features of the Vuex API abstraction layer

--- frontend/src/views/Camps.vue
+++ frontend/src/views/Camps.vue
@@ -3,43 +3,34 @@
     <h3>Camps</h3>
     <ul>
       <li
-        v-for="campId in campIdList"
-        :key="campId">
+        v-for="camp in camps"
+        :key="camp().id">
         <router-link
-          :to="{ name: 'camp', params: { campId: campId } }">
-          {{ campId }}
+          :to="{ name: 'camp', params: { campId: camp().id } }">
+          {{ camp().name }} - {{ camp().motto }}
         </router-link>
       </li>
     </ul>
+    <button class="btn btn-primary" @click="changeCampName">Change camp #1 name in Vuex store</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Camps',
-  data () {
-    return {
-      campIdList: []
-    }
-  },
   computed: {
     apiUrl () {
       return process.env.VUE_APP_ROOT_API + '/camp'
+    },
+    camps () {
+      return this.api('/camp').items
     }
   },
-  created () {
-    this.fetchFromAPI()
-  },
   methods: {
-    async fetchFromAPI () {
-      try {
-        this.campIdList = (await this.axios.get(this.apiUrl)).data._embedded.items.map(item => item.id)
-      } catch (error) {
-        this.messages = [{
-          type: 'danger',
-          text: 'Could not get camp list. ' + error
-        }]
-      }
+    changeCampName () {
+      const changedCamp = { ...this.camps[0]() }
+      changedCamp.name = changedCamp.name + ' HELLO'
+      this.$store.commit('add', changedCamp)
     }
   }
 }
