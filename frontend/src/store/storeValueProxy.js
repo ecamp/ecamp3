2fcdeae27c57d984bc6c8d2ce24d57065736ac94
Carlo Beltrame <carlo@beltra.me>
2019-09-16 14:39:58 +0200

    Work less with Proxies and more with vanilla JS because of performance concerns

--- frontend/src/store/storeValueProxy.js
+++ frontend/src/store/storeValueProxy.js
@@ -4,40 +4,58 @@ function isLink (object) {
   return objectKeys.length === 1 && objectKeys[0] === 'href'
 }
 
+function isCollection (object) {
+  if (!object) return false
+  // TODO is this check appropriate? Should we check for _page in _meta instead?
+  return !object.hasOwnProperty('id') && object.hasOwnProperty('items') && Array.isArray(object.items)
+}
+
 function loadingProxy () {
   const handler = {
     get: function (target, prop, receiver) {
+      // TODO can the loading flag maybe be removed once we use loadingProxies everywhere?
+      if (prop === 'loading') {
+        return true
+      }
+      return loadingProxy()
+    },
+    apply: function () {
       return loadingProxy()
     }
   }
-  let result = () => new Proxy({ _meta: { loading: true } }, handler)
-  result.toString = () => ''
-  return result
+  return new Proxy({}, handler)
+}
+
+function collectionProxy (vm, array) {
+  return {
+    get items () {
+      return array.map(entry => {
+        if (isLink(entry)) {
+          return vm.api(entry.href)
+        }
+      })
+    }
+  }
 }
 
 export default function storeValueProxy (vm, data) {
-  const handler = {
-    get: function (target, prop, receiver) {
-      if (!target.hasOwnProperty(prop)) {
-        if (target._meta.loading) {
-          return loadingProxy()
-        }
-        return Reflect.get(...arguments)
-      }
-      const value = Reflect.get(...arguments)
-      if (Array.isArray(value)) {
-        // TODO implement collections properly to hide the difference between embedded and paginated collection
-        return () => value.map(entry => {
-          if (isLink(entry)) {
-            return vm.api(entry.href)
-          }
-        })
-      }
-      if (isLink(value)) {
-        return () => vm.api(value.href)
-      }
-      return value
-    }
+  if ((data._meta || {}).loading) {
+    return loadingProxy()
   }
-  return new Proxy(data, handler)
+  if (isCollection(data)) {
+    // TODO implement paginated collections properly to hide the difference to embedded collections
+    console.error('paginated collections not implemented')
+  }
+  const result = {}
+  Object.keys(data).forEach(key => {
+    const value = data[key]
+    if (Array.isArray(value)) {
+      result[key] = () => collectionProxy(vm, value)
+    } else if (isLink(value)) {
+      result[key] = () => vm.api(value.href)
+    } else {
+      Object.defineProperty(result, key, { get: () => data[key] })
+    }
+  })
+  return result
 }
