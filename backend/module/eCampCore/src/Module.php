62f92fb21ba8b0b7dd9bd9dfe26d67323c06b85f
Pirmin Mattmann <pimattmann@gmail.com>
2019-10-05 14:51:02 +0200

    BugFix, missing commit

--- backend/module/eCampCore/src/Module.php
+++ backend/module/eCampCore/src/Module.php
@@ -14,18 +14,21 @@ class Module {
     public function onBootstrap(MvcEvent $e) {
         /** @var Application $app */
         $app = $e->getApplication();
+        $events = $app->getEventManager();
         /** @var EntityManager $em */
         $em = $app->getServiceManager()->get('doctrine.entitymanager.orm_default');
 
-        $app->getEventManager()->attach(MvcEvent::EVENT_FINISH, function (MvcEvent $e) use ($em) {
+        $events->attach(MvcEvent::EVENT_DISPATCH, function (MvcEvent $e) use ($em) {
+            $em->beginTransaction();
+        });
+
+        $events->attach(MvcEvent::EVENT_FINISH, function (MvcEvent $e) use ($em) {
             if ($e->getError()) {
                 if ($em->getConnection()->isTransactionActive()) {
                     $em->rollback();
                 }
             } else {
-                if ($em->getConnection()->isTransactionActive()) {
-                    $em->flush();
-                }
+                $em->flush();
             }
         });
     }
