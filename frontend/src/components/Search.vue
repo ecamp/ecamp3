9da48b4d8a3b921e683d4ab7598609f1af4b0484
Manuel Meister <news.manuelsworld@gmail.com>
2020-02-29 23:51:09 +0100

    Added searchbar (only mockup)

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-responsive :max-width="searchFocused ? 350 : 200" class="align-center transition-swing">
+    <v-text-field rounded
+                  hide-details solo
+                  dark label="Suchen…"
+                  solo-inverted single-line
+                  flat class="align-center px-2"
+                  append-icon="mdi-magnify"
+                  @focus="searchFocused = true"
+                  @blur="searchFocused = false"
+                  color="white" dense />
+  </v-responsive>
+</template>
+
+<script>
+export default {
+  name: 'GlobalSearch',
+  data () {
+    return {
+      searchFocused: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
