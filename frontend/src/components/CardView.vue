5a24f37a88daea7b71285d5f92290b3bda99f127
Manuel Meister <news.manuelsworld@gmail.com>
2020-02-29 23:32:37 +0100

    Created CardView as a container for the main content

---
+++ frontend/src/components/CardView.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card :max-width="maxWidth" class="mx-auto">
+    <v-card-title v-if="title">
+      {{ title }}
+    </v-card-title>
+    <v-skeleton-loader v-if="!loaded" type="article" />
+    <slot v-else />
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'CardView',
+  props: {
+    loaded: { type: Boolean, required: false, default: true },
+    title: { type: String, required: false, default: '' },
+    icon: { type: String, required: false, default: '' },
+    maxWidth: { type: String, default: '' }
+  }
+}
+</script>
+
+<style scoped>
+  .v-toolbar {
+    border-bottom: 1px solid;
+  }
+
+  .static {
+    position: static;
+  }
+</style>
