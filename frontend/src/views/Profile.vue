cf46642f0e1db024f0f7a9072c646e4a3085c4cd
Manuel Meister <news.manuelsworld@gmail.com>
2020-03-01 00:19:23 +0100

    Added profile with email (read-only) TODO: Implement backend-methods to change e-mail address

---
+++ frontend/src/views/Profile.vue
@@ -0,0 +1,31 @@
+<template>
+  <card-view :title="'Profil: ' + profile.username" max-width="900">
+    <v-col>
+      <api-input
+        label="Email"
+        :uri="profile._meta.self"
+        fieldname="mail"
+        :value="profile.mail"
+        :editing="false"
+        required />
+    </v-col>
+  </card-view>
+</template>
+
+<script>
+import ApiInput from '../components/form/ApiInput'
+
+const CardView = () => import('../components/CardView')
+export default {
+  name: 'Home',
+  components: { ApiInput, CardView },
+  computed: {
+    profile () {
+      return this.api.get().profile()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
