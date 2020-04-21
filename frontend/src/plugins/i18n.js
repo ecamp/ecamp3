99ad2f3bb2ea9efb7218ef44b488bf9a57856543
Manuel Meister <news.manuelsworld@gmail.com>
2020-04-21 22:01:41 +0200

    Simplified lang
    Co-Authored-By: Carlo Beltrame <carlo@beltra.me>

--- frontend/src/plugins/i18n.js
+++ frontend/src/plugins/i18n.js
@@ -3,17 +3,11 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
+import en from '@/locales/en.json'
+import de from '@/locales/de.json'
+
 function loadLocaleMessages () {
-  const locales = require.context('@/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-    if (matched && matched.length > 1) {
-      const locale = matched[1]
-      messages[locale] = locales(key)
-    }
-  })
-  return messages
+  return { en, de }
 }
 
 export default new VueI18n({
