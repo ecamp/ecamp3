c5a1314b31ea645d3aaa131945317d5ec1217558
Pirmin Mattmann <pimattmann@gmail.com>
2020-06-12 22:35:22 +0200

    add de-scout

--- frontend/src/plugins/i18n.js
+++ frontend/src/plugins/i18n.js
@@ -3,11 +3,12 @@ import VueI18n from 'vue-i18n'
 
 import en from '@/locales/en.json'
 import de from '@/locales/de.json'
+import deScout from '@/locales/de-scout.json'
 
 Vue.use(VueI18n)
 
 function loadLocaleMessages () {
-  return { en, de }
+  return { en, de, 'de-scout': deScout }
 }
 
 export default new VueI18n({
@@ -15,5 +16,5 @@ export default new VueI18n({
   fallbackLocale: 'en',
   messages: loadLocaleMessages(),
   silentTranslationWarn: true,
-  availableLocales: ['de', 'en', 'fr', 'it']
+  availableLocales: ['de', 'de-scout', 'en', 'fr', 'it']
 })
