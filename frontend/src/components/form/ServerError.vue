464f38ac7bd5e8ad7d0376872c142b7efb0f7bde
Pirmin Mattmann <pimattmann@gmail.com>
2020-06-13 01:03:53 +0200

    Client & Server-Validation

---
+++ frontend/src/components/form/ServerError.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-alert v-if="serverError" class="background-color: red">
+    <div v-if="serverError.name === 'ServerException' && serverError.response && serverError.response.status === 422">
+      <div class="title">Validation error:</div>
+      <div v-for="(validation_messages, name) in serverError.response.data.validation_messages" :key="name">
+        <div>
+        <b>{{ name }}</b>:
+        <span v-for="(message, idx) in validation_messages" :key="idx">
+          {{ message }}<span v-if="idx === validation_messages.length">, </span>
+        </span>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      {{ serverError.message }}
+    </div>
+  </v-alert>
+</template>
+
+<script>
+
+export default {
+  name: 'ServerError',
+  props: {
+    serverError: {
+      type: Object,
+      default: null
+    }
+  }
+}
+
+</script>
--- frontend/src/views/CampCreate.vue
+++ frontend/src/views/CampCreate.vue
@@ -8,83 +8,100 @@
         </v-toolbar-title>
       </v-toolbar>
       <v-card-text>
-        <v-form ref="form" @submit.prevent="createCamp">
-          <e-text-field
-            v-model="camp.name"
-            :label="$t('camp.name')"
-            required
-            autofocus />
-          <e-text-field
-            v-model="camp.title"
-            :label="$t('camp.title')"
-            required />
-          <e-text-field
-            v-model="camp.motto"
-            :label="$t('camp.motto')"
-            required />
-          <e-select
-            v-model="camp.campTypeId"
-            :label="$t('camp.campType')"
-            :items="campTypes">
-            <template v-slot:item="data">
-              <v-list-item v-bind="data.attrs" v-on="data.on">
-                <v-list-item-content>
-                  {{ data.item.text }}
-                </v-list-item-content>
-                <v-list-item-action-text>
-                  <v-icon v-if="data.item.object.isCourse" left>
-                    mdi-school
-                  </v-icon>
-                </v-list-item-action-text>
-              </v-list-item>
-            </template>
-          </e-select>
-          <v-list>
-            <v-list-item>
-              <v-list-item-title>
-                {{ $t('camp.periods') }}
-              </v-list-item-title>
-              <v-list-item-action>
-                <v-btn @click="addPeriod">
-                  <v-icon>mdi-plus</v-icon>
-                </v-btn>
-              </v-list-item-action>
-            </v-list-item>
-            <div v-for="(period, i) in camp.periods" :key="period.key">
+        <ValidationObserver v-slot="{ handleSubmit }">
+          <v-form ref="form" @submit.prevent="handleSubmit(createCamp)">
+            <server-error :server-error="serverError" />
+            <e-text-field
+              v-model="camp.name"
+              :label="$t('camp.name')"
+              :name="$t('camp.name')"
+              vee-rules="required"
+              required
+              autofocus />
+            <e-text-field
+              v-model="camp.title"
+              :label="$t('camp.title')"
+              :name="$t('camp.title')"
+              vee-rules="required"
+              required />
+            <e-text-field
+              v-model="camp.motto"
+              :label="$t('camp.motto')"
+              :name="$t('camp.motto')"
+              vee-rules="required"
+              required />
+            <e-select
+              v-model="camp.campTypeId"
+              :label="$t('camp.campType')"
+              :name="$t('camp.campType')"
+              vee-rules="required"
+              :items="campTypes">
+              <template v-slot:item="data">
+                <v-list-item v-bind="data.attrs" v-on="data.on">
+                  <v-list-item-content>
+                    {{ data.item.text }}
+                  </v-list-item-content>
+                  <v-list-item-action-text>
+                    <v-icon v-if="data.item.object.isCourse" left>
+                      mdi-school
+                    </v-icon>
+                  </v-list-item-action-text>
+                </v-list-item>
+              </template>
+            </e-select>
+            <v-list>
               <v-list-item>
-                <v-list-item-content>
-                  <e-text-field
-                    v-model="period.description"
-                    :label="$t('period.description')"
-                    required />
-                </v-list-item-content>
+                <v-list-item-title>
+                  {{ $t('camp.periods') }}
+                </v-list-item-title>
                 <v-list-item-action>
-                  <v-btn :disabled="!periodDeletable" @click="deletePeriod(i)">
-                    <v-icon>mdi-delete</v-icon>
+                  <v-btn @click="addPeriod">
+                    <v-icon>mdi-plus</v-icon>
                   </v-btn>
                 </v-list-item-action>
               </v-list-item>
-              <v-list-item>
-                <v-list-item-content>
-                  <e-date-picker
-                    v-model="period.start"
-                    :label="$t('period.start')"
-                    required />
-                  <e-date-picker
-                    v-model="period.end"
-                    :label="$t('period.end')"
-                    required />
-                </v-list-item-content>
-              </v-list-item>
-              <v-divider />
+              <div v-for="(period, i) in camp.periods" :key="period.key">
+                <v-list-item>
+                  <v-list-item-content>
+                    <e-text-field
+                      v-model="period.description"
+                      :label="$t('period.description')"
+                      :name="$t('period.description')"
+                      vee-rules="required"
+                      required />
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-btn :disabled="!periodDeletable" @click="deletePeriod(i)">
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                  </v-list-item-action>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-content>
+                    <e-date-picker
+                      v-model="period.start"
+                      :label="$t('period.start')"
+                      :name="$t('period.start')"
+                      vee-rules="required"
+                      required />
+                    <e-date-picker
+                      v-model="period.end"
+                      :label="$t('period.end')"
+                      :name="$t('period.end')"
+                      vee-rules="required"
+                      required />
+                  </v-list-item-content>
+                </v-list-item>
+                <v-divider />
+              </div>
+            </v-list>
+            <div class="text-right">
+              <ButtonAdd type="submit">
+                {{ $t('camp.create') }}
+              </ButtonAdd>
             </div>
-          </v-list>
-          <div class="text-right">
-            <ButtonAdd type="submit">
-              {{ $t('camp.create') }}
-            </ButtonAdd>
-          </div>
-        </v-form>
+          </v-form>
+        </ValidationObserver>
       </v-card-text>
     </content-card>
   </v-container>
@@ -98,6 +115,8 @@ import EDatePicker from '@/components/form/base/EDatePicker'
 import ETextField from '@/components/form/base/ETextField'
 import ESelect from '@/components/form/base/ESelect'
 import { campRoute } from '@/router'
+import ServerError from '@/components/form/ServerError'
+import { ValidationObserver } from 'vee-validate'
 
 export default {
   name: 'Camps',
@@ -107,7 +126,9 @@ export default {
     ContentCard,
     EDatePicker,
     ETextField,
-    ESelect
+    ESelect,
+    ValidationObserver,
+    ServerError
   },
   data () {
     return {
@@ -125,6 +146,7 @@ export default {
           }
         ]
       },
+      serverError: null,
       periodKey: 0
     }
   },
@@ -150,7 +172,7 @@ export default {
       this.api.post(this.campsUrl, this.camp).then(c => {
         this.$router.push(campRoute(c, 'admin'))
         this.api.reload(this.campsUrl)
-      })
+      }, (error) => { this.serverError = error })
     },
     addPeriod: function () {
       this.camp.periods.push({
