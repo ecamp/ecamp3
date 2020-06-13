a9283e6f3e610e40c26f5f64bc31cf16c970a7cb
Manuel Meister <news.manuelsworld@gmail.com>
2020-06-13 12:55:33 +0200

    Fixed wrong store used in instance

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
-import { vuetify, auth, store, filterLoading, formBaseComponents, ignoreNativeBindingWarnMessages, i18n, veeValidate } from '@/plugins'
+import { vuetify, auth, apiStore, filterLoading, formBaseComponents, ignoreNativeBindingWarnMessages, i18n, veeValidate } from '@/plugins'
+import { store } from '@/plugins/store'
 
 Vue.use(auth)
-Vue.use(store)
+Vue.use(apiStore)
 Vue.use(filterLoading)
 Vue.use(formBaseComponents)
 Vue.use(ignoreNativeBindingWarnMessages)
