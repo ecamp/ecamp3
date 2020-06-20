55ab39c0269232e0dde007cde6319f5837d4b606
Pirmin Mattmann <pimattmann@gmail.com>
2020-06-20 12:57:59 +0200

    Added SiKo to LA

--- backend/module/eCampCore/data/prod/ActivityTypeData.php
+++ backend/module/eCampCore/data/prod/ActivityTypeData.php
@@ -54,6 +54,7 @@ class ActivityTypeData extends AbstractFixture implements DependentFixtureInterf
             // add allowed content types
             //$this->addContentType($activityType, $this->getReference(ContentTypeData::$STORYBOARD));
             $this->addContentType($activityType, $this->getReference(ContentTypeData::$STORYCONTEXT));
+            $this->addContentType($activityType, $this->getReference(ContentTypeData::$SIKO));
 
             $activityTypeFactory = new ActivityTypeFactory();
             $activityTypeFactory->setName('TABS');
