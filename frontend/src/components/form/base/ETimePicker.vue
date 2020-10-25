61299ac56db2db236becc05ee4d1d92a77620dc9
Manuel Meister <news.manuelsworld@gmail.com>
2020-10-25 19:36:07 +0100

    Added format and parse to timepicker

--- frontend/src/components/form/base/ETimePicker.vue
+++ frontend/src/components/form/base/ETimePicker.vue
@@ -7,6 +7,8 @@ Allows 15min steps only
   <base-picker
     :icon="icon"
     :value="value"
+    :format="format"
+    :parse="parse"
     v-bind="$attrs"
     @input="$emit('input', $event)">
     <template slot-scope="picker">
@@ -40,7 +42,33 @@ export default {
     value: { type: String, required: true }
   },
   methods: {
-    allowedStep: m => m % 15 === 0
+    allowedStep: m => m % 15 === 0,
+    format (val) {
+      if (val !== '') {
+        return this.$moment(val, this.$moment.HTML5_FMT.TIME, this.$i18n.locale).format('LT')
+      }
+      return ''
+    },
+    parse (val) {
+      if (val) {
+        const m = this.$moment(val, [this.$moment.HTML5_FMT.TIME, 'LT'])
+        if (m.isValid()) {
+          return Promise.resolve(m.format(this.$moment.HTML5_FMT.TIME))
+        } else {
+          switch (m.parsingFlags().overflow) {
+            case 0: // Year
+              return Promise.reject(new Error('invalid year'))
+            case 1: // Month
+              return Promise.reject(new Error('invalid month'))
+            case 2: // Day
+              return Promise.reject(new Error('invalid day'))
+          }
+          return Promise.reject(new Error('invalid format'))
+        }
+      } else {
+        return Promise.resolve('')
+      }
+    }
   }
 }
 </script>
