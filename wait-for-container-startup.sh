78a3f8c5c4519f8ec93074b8912218c2eaa54414
Pirmin Mattmann <pimattmann@gmail.com>
2020-10-26 19:33:40 +0100

    test

--- wait-for-container-startup.sh
+++ wait-for-container-startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Waiting for backend container to start up and migrate DB..."
-until curl --output /dev/null --silent --fail http://localhost:3001/api
+until curl --fail http://localhost:3001/api
 do
   sleep 2
 done
@@ -9,7 +9,7 @@ echo "Backend container is ready."
 
 
 echo "Waiting for frontend container to start up..."
-until curl --output /dev/null --silent --head --fail http://localhost:3000
+until curl --head --fail http://localhost:3000
 do
   sleep 2
 done
