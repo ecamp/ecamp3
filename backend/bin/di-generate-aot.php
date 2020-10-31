d082e2acd345bb0b35e5aebf2e4b11e506b56ed9
Urban Suppiger <urban@suppiger.net>
2020-10-31 13:02:57 +0100

    fix AOT & deployment (composer 2)

--- backend/bin/di-generate-aot.php
+++ backend/bin/di-generate-aot.php
@@ -2,60 +2,58 @@
 
 namespace eCamp\AoT;
 
-use Psr\Container\ContainerInterface;
 use Laminas\Code\Scanner\DirectoryScanner;
 use Laminas\Di\CodeGenerator\InjectorGenerator;
 use Laminas\Di\Config;
-use Laminas\Di\ConfigInterface;
 use Laminas\Di\Definition\RuntimeDefinition;
 use Laminas\Di\Resolver\DependencyResolver;
+use Psr\Container\ContainerInterface;
 
 //require __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../autoload.php';
+require_once __DIR__.'/../autoload.php';
 
 function getClassNames(ContainerInterface $container): iterable {
     // Define the source directories to scan for classes to generate
     // AoT factories for
     $directories = [
-        __DIR__ . '/../module/eCampLib/src',
-        __DIR__ . '/../module/eCampCore/src',
-        __DIR__ . '/../module/eCampApi/src',
-        __DIR__ . '/../content-type/eCampStoryboard/src',
-        __DIR__ . '/../content-type/eCampTextarea/src',
+        __DIR__.'/../module/eCampLib/src',
+        __DIR__.'/../module/eCampCore/src',
+        __DIR__.'/../module/eCampApi/src',
+        __DIR__.'/../content-type/eCampStoryboard/src',
+        __DIR__.'/../content-type/eCampSingleText/src',
     ];
 
     $scanner = new DirectoryScanner($directories);
 
     /** @var \Laminas\Code\Scanner\ClassScanner $class */
     foreach ($scanner->getClasses() as $class) {
-
-        /**
+        /*
          * omit classes that
          * - already have a factory (or abstract factory), e.g. discoverable by service manager without using Zend\DI
          * - are not instantiable (e.g. abstract classes)
          * - have no constructor
          * - have a constructor with 0 arguments
          */
-        if (!$container->has($class->getName()) && $class->isInstantiable() && $class->getMethod('__construct') && $class->getMethod('__construct')->getNumberOfParameters()>0) {
-            printf("ADDING ".$class->getName()."\n");
+        if (!$container->has($class->getName()) && $class->isInstantiable() && $class->getMethod('__construct') && $class->getMethod('__construct')->getNumberOfParameters() > 0) {
+            printf('ADDING '.$class->getName()."\n");
             yield $class->getName();
         }
     }
 }
 
-/* we need the App without DI to check which factories are missing... */
+// we need the App without DI to check which factories are missing...
 $smWithoutDi = \eCampApp::CreateServiceManagerWithoutDi();
 
-/* ... but for generating the code, we need the full App with DI */
+// ... but for generating the code, we need the full App with DI
 $appWithDi = \eCampApp::CreateApp();
 
 $container = $appWithDi->getServiceManager();
 $config = $container->get('configuration');
-$di_config = new Config( $config['dependencies']['auto'] );
+$di_config = new Config($config['dependencies']['auto']);
 
 $resolver = new DependencyResolver(new RuntimeDefinition(), $di_config);
 $resolver->setContainer($container);
 
-$generator = new InjectorGenerator($di_config, $resolver, __NAMESPACE__ . '\Generated');
-$generator->setOutputDirectory(__DIR__ . '/../module/eCampAoT/gen');
+$generator = new InjectorGenerator($di_config, $resolver, __NAMESPACE__.'\Generated');
+$generator->setOutputDirectory(__DIR__.'/../module/eCampAoT/gen');
 $generator->generate(getClassNames($smWithoutDi));
