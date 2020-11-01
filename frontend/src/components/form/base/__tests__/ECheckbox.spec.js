2cdc0c355e7c24d5dc40c9d253c8e0a29153a1cf
BacLuc <lucius.bachmann@gmx.ch>
2020-11-01 11:31:12 +0100

    ECheckbox: add test

---
+++ frontend/src/components/form/base/__tests__/ECheckbox.spec.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+import { formBaseComponents } from '@/plugins'
+
+import { mount as mountComponent } from '@vue/test-utils'
+import ECheckbox from '../ECheckbox'
+
+Vue.use(Vuetify)
+Vue.use(formBaseComponents)
+
+describe('An ECheckbox', () => {
+  let vuetify
+
+  const mount = (options) => mountComponent(ECheckbox, { vuetify, ...options })
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+  test('looks like a checkbox', async () => {
+    const wrapper = mount()
+    await wrapper.setProps({ vModel: false })
+    expect(wrapper.element).toMatchSnapshot('unchecked')
+
+    await wrapper.setProps({ vModel: true })
+    expect(wrapper.element).toMatchSnapshot('checked')
+  })
+
+  test('is checked when initialized checked', () => {
+    const wrapper = mount({
+      propsData: {
+        vModel: true
+      }
+    })
+    expect(wrapper.find('input[type=checkbox]').element.getAttribute('vmodel')).toBe('true')
+  })
+
+  test('updates checkbox when vModel changes', async () => {
+    const wrapper = mount()
+    expect(wrapper.find('input[type=checkbox]').element.getAttribute('vmodel')).toBeNull()
+
+    await wrapper.setProps({ vModel: true })
+    expect(wrapper.find('input[type=checkbox]').element.getAttribute('vmodel')).toBe('true')
+
+    await wrapper.setProps({ vModel: false })
+    expect(wrapper.find('input[type=checkbox]').element.getAttribute('vmodel')).toBeNull()
+  })
+})
