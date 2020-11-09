66c64891305af68b9ed0cf40feddc7d1d492a825
feigenmann <faebe@fa.localdomain>
2020-11-09 23:03:18 +0100

    fixed intendation and } typo

--- frontend/src/views/camp/SideBarPeriods.vue
+++ frontend/src/views/camp/SideBarPeriods.vue
@@ -15,10 +15,10 @@ Displays periods of a single camp.
           :to="periodRoute(item)"
           two-line>
           <v-list-item-content>
-            <v-list-item-title>{{ item.description }</v-list-item-title>
-          <v-list-item-subtitle>
-        {{ $moment.utc(period.start).format($tc('global.moment.dateShort')) }} - {{ $moment.utc(period.end).format($tc('global.moment.dateShort')) }}
-      </v-list-item-subtitle>
+            <v-list-item-title>{{ item.description }}</v-list-item-title>
+              <v-list-item-subtitle>
+                {{ $moment.utc(period.start).format($tc('global.moment.dateShort')) }} - {{ $moment.utc(period.end).format($tc('global.moment.dateShort')) }}
+              </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-list>
