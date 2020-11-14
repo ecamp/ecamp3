bebd43edac0dd192898b4a113472ad968c3b4984
Carlo Beltrame <carlo@beltra.me>
2020-11-14 04:08:37 +0100

    Refactor again to only display schedule entries and days that actually contain story content

--- frontend/src/components/camp/StoryPeriod.vue
+++ frontend/src/components/camp/StoryPeriod.vue
@@ -1,34 +1,50 @@
 <template>
   <div>
-    <h4>
-      {{ period.description }}
-    </h4>
-    <template v-for="(scheduleEntries, uri) in scheduleEntriesGroupedByDay">
-      <story-day v-if="scheduleEntries.length"
-                 :key="uri"
-                 :schedule-entries="scheduleEntries" />
-    </template>
+    <h2>{{ period.description }}</h2>
+    <div v-for="(entries, dayUri) in storyContentsGroupedByDay" :key="dayUri">
+      <h3>{{ dayName(entries[0].scheduleEntry.day()) }}</h3>
+      <div v-for="{ scheduleEntry, storyChapters } in entries" :key="scheduleEntry._meta.uri">
+        <h4>{{ scheduleEntry.activity().title }}</h4>
+        <p v-for="chapter in storyChapters" :key="chapter" v-html="chapter"></p>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { groupBy, sortBy } from 'lodash'
-import StoryDay from '@/components/camp/StoryDay'
 
 export default {
   name: 'StoryPeriod',
-  components: { StoryDay },
   props: {
     period: { type: Object, required: true }
   },
   computed: {
-    scheduleEntriesGroupedByDay () {
-      return groupBy(
-        sortBy(
-          this.period.scheduleEntries().items,
-          scheduleEntry => scheduleEntry.periodOffset
-        ),
-        scheduleEntry => scheduleEntry.day()._meta.self
-      )
+    sortedScheduleEntries () {
+      return sortBy(this.period.scheduleEntries().items, scheduleEntry => scheduleEntry.periodOffset)
+    },
+    scheduleEntriesWithStory () {
+      return this.sortedScheduleEntries.map(scheduleEntry => {
+        return {
+          scheduleEntry: scheduleEntry,
+          storyChapters: (scheduleEntry.activity().activityContents() || { items: [] })
+            .items
+            .filter(activityContent => activityContent.contentTypeName === 'Storycontext')
+            .map(activityContent => activityContent.singleText().text)
+            .filter(text => text)
+        }
+      }).filter(({ storyChapters }) => storyChapters.length)
+    },
+    storyContentsGroupedByDay () {
+      return groupBy(this.scheduleEntriesWithStory, ({ scheduleEntry }) => scheduleEntry.day()._meta.self)
+    }
+  },
+  methods: {
+    dayName (day) {
+      const date = this.addDays(this.period.start, day.dayOffset)
+      return this.$moment.utc(date).format(this.$tc('global.moment.dateLong'))
+    },
+    addDays (date, days) {
+      return Date.parse(date) + days * 24 * 60 * 60 * 1000
     }
   }
 }
