3b8234f008044e77bd2e2173486668cf4adfbe61
feigenmann <feigenmann@outlook.com>
2020-11-22 20:58:03 +0100

    done

--- frontend/src/components/navigation/SearchMobile.vue
+++ frontend/src/components/navigation/SearchMobile.vue
@@ -10,7 +10,7 @@
       </v-btn>
       <v-text-field
         hide-details
-        :label="searchOpen ? $tc('components.navigation.searchMobile.search') : $tc('components.navigation.searchMobile.searchActivitiesAndCamps')"
+        :label="searchOpen ? $tc('global.button.search') : $tc('components.navigation.searchMobile.searchActivitiesAndCamps')"
         single-line
         @click="searchOpen = !searchOpen" />
       <router-link :to="{name: 'profile'}">
