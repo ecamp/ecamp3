f419fa465575158f887511ce30ae6732b47cf10e
BacLuc <lucius.bachmann@gmx.ch>
2020-12-07 22:19:51 +0100

    ESwitch.spec.js: switch to explicit rendering
    I think this improves the documentation because the component is
    used as it would be in a view or another component.
    
    Plus we don't have to use these input event hooks but
    can verify the viewmodel of the outer component.
    
    And it seems to change the rendering anyway, and i want
    to test the rendering as it would be in a component/view.

--- frontend/src/components/form/base/__tests__/ESwitch.spec.js
+++ frontend/src/components/form/base/__tests__/ESwitch.spec.js
@@ -13,87 +13,90 @@ Vue.use(formBaseComponents)
 describe('An ESwitch', () => {
   let vuetify
 
-  const mount = (options) => mountComponent(ESwitch, { vuetify, ...options })
+  const mount = (options) => {
+    const app = Vue.component('App', {
+      components: { ESwitch },
+      data: function () {
+        return {
+          data: null
+        }
+      },
+      template: `
+        <div data-app>
+          <e-switch v-model="data"/>
+        </div>
+      `
+    })
+    return mountComponent(app, { vuetify, attachTo: document.body, ...options })
+  }
   beforeEach(() => {
     vuetify = new Vuetify()
   })
   test('looks like a switch', async () => {
     const wrapper = mount()
-    await wrapper.setProps({ vModel: false })
+    await wrapper.setData({ data: false })
     expect(wrapper.element).toMatchSnapshot('unchecked')
 
-    await wrapper.setProps({ vModel: true })
+    await wrapper.setData({ data: true })
     expect(wrapper.element).toMatchSnapshot('checked')
   })
 
   test('is checked when initialized checked', () => {
     const wrapper = mount({
-      propsData: {
-        vModel: true
+      data: function () {
+        return {
+          data: true
+        }
       }
     })
-    expect(wrapper.find('input[type=checkbox]').element.getAttribute('vmodel')).toBe('true')
+    expect(wrapper.find('input[type=checkbox]').element.getAttribute('aria-checked')).toBe('true')
   })
 
   test('updates switch when vModel changes', async () => {
     const wrapper = mount()
-    expect(wrapper.find('input[type=checkbox]').element.getAttribute('vmodel')).toBeNull()
+    expect(wrapper.find('input[type=checkbox]').element.getAttribute('aria-checked')).toBe('false')
 
-    await wrapper.setProps({ vModel: true })
-    expect(wrapper.find('input[type=checkbox]').element.getAttribute('vmodel')).toBe('true')
+    await wrapper.setData({ data: true })
+    expect(wrapper.find('input[type=checkbox]').element.getAttribute('aria-checked')).toBe('true')
 
-    await wrapper.setProps({ vModel: false })
-    expect(wrapper.find('input[type=checkbox]').element.getAttribute('vmodel')).toBeNull()
+    await wrapper.setData({ data: false })
+    expect(wrapper.find('input[type=checkbox]').element.getAttribute('aria-checked')).toBe('false')
   })
 
   test('updates vModel when user toggles with click', async () => {
     const wrapper = mount()
     const input = wrapper.find('input')
 
-    const change = jest.fn()
-    wrapper.vm.$on('input', (event) => change(event))
+    await input.trigger('click')
+    expect(wrapper.vm.data).toBe(true)
 
     await input.trigger('click')
-    expect(change).toBeCalledTimes(1)
-    expect(change).toBeCalledWith(true)
-
-    jest.resetAllMocks()
-    await input.trigger('click')
-    expect(change).toBeCalledTimes(1)
-    expect(change).toBeCalledWith(false)
+    expect(wrapper.vm.data).toBe(false)
   })
 
   test('updates vModel when user toggles with touch swipe', async () => {
     const wrapper = mount()
 
-    const change = jest.fn()
-    wrapper.vm.$on('input', (event) => change(event))
-
     touch(wrapper.find('.v-input--selection-controls__ripple')).start(0, 0).end(20, 0)
-    expect(change).toBeCalledWith(true)
+    expect(wrapper.vm.data).toBe(true)
 
     jest.resetAllMocks()
     touch(wrapper.find('.v-input--selection-controls__ripple')).start(0, 0).end(-20, 0)
-    expect(change).toBeCalledWith(false)
+    expect(wrapper.vm.data).toBe(false)
   })
 
   test('updates vModel when user toggles with keys', async () => {
     const wrapper = mount()
     const input = wrapper.find('input')
 
-    const change = jest.fn()
-    wrapper.vm.$on('input', (event) => change(event))
+    input.trigger('keydown.right')
+    expect(wrapper.vm.data).toBe(true)
 
     input.trigger('keydown.right')
-    expect(change).toHaveBeenCalledWith(true)
-    expect(change).toHaveBeenCalledTimes(1)
-
-    input.trigger('keydown.right')
-    expect(change).toHaveBeenCalledTimes(1)
+    expect(wrapper.vm.data).toBe(true)
 
     jest.resetAllMocks()
     input.trigger('keydown.left')
-    expect(change).toHaveBeenCalledWith(false)
-    expect(change).toHaveBeenCalledTimes(1)
+    expect(wrapper.vm.data).toBe(false)
   })
 })
