d65a9868c40343269a6ab182c5704045939a75ff
Carlo Beltrame <carlo@beltra.me>
2020-12-10 16:08:15 +0100

    Remove obsolete files

--- .docker-hub/push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
-docker-compose -f .docker-hub/docker-compose.yml build
-docker-compose -f .docker-hub/docker-compose.yml push backend frontend print worker-print-puppeteer worker-print-weasy
