6c0b551aa93d141fe4119304bc5019482a3e1121
Carlo Beltrame <carlo@beltra.me>
2021-01-20 16:12:03 +0100

    Use v-if / v-else instead of if / else in template

--- frontend/src/components/camp/CollaboratorListItem.vue
+++ frontend/src/components/camp/CollaboratorListItem.vue
@@ -4,8 +4,12 @@
       <v-img src="https://i.pravatar.cc/300" />
     </v-list-item-avatar>
     <v-list-item-content>
-      <v-list-item-title>{{ collaborator.user ? collaborator.user().displayName : "" }}</v-list-item-title>
-      <v-list-item-subtitle>{{ collaborator.inviteEmail }}</v-list-item-subtitle>
+      <v-list-item-title v-if="collaborator.user">
+        {{ collaborator.user().displayName }}
+      </v-list-item-title>
+      <v-list-item-subtitle v-else>
+        {{ collaborator.inviteEmail }}
+      </v-list-item-subtitle>
     </v-list-item-content>
     <api-select
       :value="collaborator.role"
