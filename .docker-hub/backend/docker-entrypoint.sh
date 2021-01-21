a40823ef9e4c2bde09fee157dfbd2232b33711c1
Carlo Beltrame <carlo@beltra.me>
2021-01-21 09:53:29 +0100

    Remove DoctrineORMModule directory after recreating the database
    The database reset commands run on container initialization as root.
    Since these commands also create the Doctrine proxies, the proxy files
    are then owned by root. This becomes a problem later when a www-data
    owned process tries to write to the proxy directory. However if we just
    remove everything that the console commands have created in the data
    directory, www-data can create its own proxy files and works happily.

--- .docker-hub/backend/docker-entrypoint.sh
+++ .docker-hub/backend/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -euo pipefail
 
 php bin/wait-for-db.php
 vendor/bin/laminas rebuild-database
 vendor/bin/laminas load-data-fixtures --path=module/eCampCore/data/*
+rm -rf data/DoctrineORMModule
 
 apache2-foreground
