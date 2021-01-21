280bab60beeb078b5093102aabc134afc1dada8b
Carlo Beltrame <carlo@beltra.me>
2021-01-21 14:57:08 +0100

    Remove unused imports

--- backend/module/eCampLib/src/Command/RebuildDatabaseSchemaCommand.php
+++ backend/module/eCampLib/src/Command/RebuildDatabaseSchemaCommand.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Filesystem;
 
 class RebuildDatabaseSchemaCommand extends Command {
     private $entityManager;
--- backend/module/eCampLib/src/Command/UpdateDatabaseSchemaCommand.php
+++ backend/module/eCampLib/src/Command/UpdateDatabaseSchemaCommand.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\Tools\SchemaTool;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Filesystem;
 
 class UpdateDatabaseSchemaCommand extends Command {
     private $entityManager;
