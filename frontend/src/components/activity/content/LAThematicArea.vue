3aa6f104bd3f6942ad98dfa9fce86a30468edebf
Manuel Meister <news.manuelsworld@gmail.com>
2021-01-25 20:37:50 +0100

    Implement vuetify active list item

--- frontend/src/components/activity/content/LAThematicArea.vue
+++ frontend/src/components/activity/content/LAThematicArea.vue
@@ -1,13 +1,15 @@
 <template>
   <v-list three-line>
     <v-list-item v-for="option in activityContent.options().items" :key="option.id" class="px-0">
-      <v-list-item-action>
-        <api-checkbox fieldname="checked" :uri="option._meta.self" />
-      </v-list-item-action>
-      <v-list-item-content>
-        <v-list-item-title>{{ $tc(`activityContent.laThematicArea.entity.option.${option.translateKey}.name`) }}</v-list-item-title>
-        <v-list-item-subtitle>{{ $tc(`activityContent.laThematicArea.entity.option.${option.translateKey}.description`) }}</v-list-item-subtitle>
-      </v-list-item-content>
+      <template #default="{ active }">
+        <v-list-item-action>
+          <api-checkbox fieldname="checked" :value="active" :uri="option._meta.self" />
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>{{ $tc(`activityContent.laThematicArea.entity.option.${option.translateKey}.name`) }}</v-list-item-title>
+          <v-list-item-subtitle>{{ $tc(`activityContent.laThematicArea.entity.option.${option.translateKey}.description`) }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </template>
     </v-list-item>
   </v-list>
 </template>
