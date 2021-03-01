f6b52d6832e9a81cafcf25bea5174b5783daf5b3
Carlo Beltrame <carlo@beltra.me>
2021-03-01 09:15:00 +0100

    Remove the obsolete vue-cli-plugin-i18n
    The features of this package are:
    
    * vue-i18n basic scaffolding
      :arrow_right: is a one-time operation, was done during i18n setup
    
    * Locale messages in Single File components with vue-i18n-loader
      :arrow_right: we don't use `<i18n>` blocks in .vue single file components anymore since https://github.com/ecamp/ecamp3/pull/480/commits/fbf2aac04840edd87da51a4f06e72a171cc2c58e (was discussed in some meeting I can't find, probably the hackathon last summer)
    
    * Locale messages missing & unused reporting (experimental)
      :arrow_right: was anyone using that feature? I don't think it will work well since we have dynamically constructed translation keys in some places (e.g. https://github.com/ecamp/ecamp3/blob/devel/frontend/src/components/activity/ContentNode.vue#L168)
    
    * Env Variables containing the current locale and fallback locale
      :arrow_right: this is pretty much useless to us since the Env Variables are only available during build, while the language is determined at runtime depending on the user's browser and language preference in the API.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -44,12 +44,6 @@ module.exports = {
   },
 
   pluginOptions: {
-    i18n: {
-      locale: 'de',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: true
-    },
     jestSerializer: {
       attributesToClear: ['id'],
       formatting: {
