76d1c1c339965c5e4320c3db4f414b41b209250a
Pirmin Mattmann <pimattmann@gmail.com>
2021-03-31 23:19:18 +0200

    fix materialList reloading

--- frontend/src/views/camp/Admin.vue
+++ frontend/src/views/camp/Admin.vue
@@ -48,7 +48,7 @@ export default {
   },
   mounted () {
     this.api.reload(this.camp())
-    this.api.reload(this.camp().materialLists)
+    this.api.reload(this.camp().materialLists())
   }
 }
 </script>
