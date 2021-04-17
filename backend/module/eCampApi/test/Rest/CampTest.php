2ea8d5fe09020640729dd66faa93bcf1cb7d37ae
Pirmin Mattmann <pimattmann@gmail.com>
2021-04-17 16:01:37 +0200

    fix unittests

--- backend/module/eCampApi/test/Rest/CampTest.php
+++ backend/module/eCampApi/test/Rest/CampTest.php
@@ -117,8 +117,7 @@ JSON;
 
         $this->assertResponseStatusCode(200);
 
-        $this->assertEquals('CampName', $this->getResponseContent()->name); // camp name not changeable
-
+        $this->assertEquals('CampName3', $this->getResponseContent()->name);
         $this->assertEquals('CampTitle3', $this->getResponseContent()->title);
         $this->assertEquals('CampMotto3', $this->getResponseContent()->motto);
         $this->assertEquals('CampTitle3', $this->camp->getTitle());
