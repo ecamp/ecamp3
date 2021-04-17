1d37b7341624ec5a61913ea14575eda9af47ebcb
simfeld <sml.imfeld@gmail.com>
2021-04-17 18:34:09 +0200

    Update local column contents after patch

--- frontend/src/components/activity/content/ColumnLayout.vue
+++ frontend/src/components/activity/content/ColumnLayout.vue
@@ -139,6 +139,9 @@ export default {
         this.localColumnContents = this.columnContents
         this.setLocalColumnWidths()
       }
+    },
+    columnContents () {
+      this.localColumnContents = this.columnContents
     }
   },
   mounted () {
