f2df7fd2764795cd083e0b9fd2cc8a9f78a9976c
Urban Suppiger <urban@suppiger.net>
2021-04-17 13:09:00 +0200

    temporary workaround for require.context

--- frontend/src/components/activity/ContentNode.vue
+++ frontend/src/components/activity/ContentNode.vue
@@ -9,11 +9,41 @@
 </template>
 
 <script>
+
+import ColumnLayout from './content/ColumnLayout.vue'
+import Notes from './content/Notes.vue'
+import Material from './content/Material.vue'
+import LAThematicArea from './content/LAThematicArea.vue'
+import SafetyConcept from './content/SafetyConcept.vue'
+import Storycontext from './content/Storycontext.vue'
+
+let contentNodeComponents = {}
+/*
+// Webpack is running (require.context needs to be available)
 const context = require.context('@/components/activity/content', false)
-const contentNodeComponents = Object.fromEntries(context.keys().map(contentNodeComponent => {
+contentNodeComponents = Object.fromEntries(context.keys().map(contentNodeComponent => {
   const component = context(contentNodeComponent).default
   return [component.name, component]
 }))
+
+// Vite is running (import.meta needs be available)
+  const context = import.meta.globEager('/src/components/activity/content/*')
+  contentNodeComponents = Object.fromEntries(Object.keys(context).map(contentNodeComponent => {
+    const component = context[contentNodeComponent].default
+    return [component.name, component]
+  }))
+}
+*/
+
+// manual import
+contentNodeComponents = {
+  ColumnLayout,
+  Notes,
+  Material,
+  LAThematicArea,
+  SafetyConcept,
+  Storycontext
+}
 
 export default {
   name: 'ContentNode',
