11e17d58237e4bfeefecd11613258ae5c070c0e7
Urban Suppiger <urban@suppiger.net>
2021-04-18 10:04:57 +0200

    initial draft with v-data-table

--- frontend/src/components/activity/content/Material.vue
+++ frontend/src/components/activity/content/Material.vue
@@ -1,6 +1,7 @@
 <template>
   <card-content-node v-bind="$props">
     <div class="mb-3">
+      <!--
       <v-simple-table dense>
         <colgroup>
           <col style="width: 55px;">
@@ -104,25 +105,85 @@
             </tr>
           </template>
         </tbody>
-      </v-simple-table>
+      </v-simple-table> -->
 
-      <template v-if="!layoutMode">
-        <material-create-item
-          v-if="$vuetify.breakpoint.smAndUp"
-          :camp="camp"
-          :content-node="contentNode"
-          @item-adding="onItemAdding" />
+      <v-data-table
+        :headers="headers"
+        :items="materialItemsData"
+        :disable-pagination="true"
+        hide-default-footer>
+        <template #[`item.quantity`]="{ item }">
+          <api-text-field
+            v-if="!item.new"
+            dense
+            :uri="item.uri"
+            fieldname="quantity" />
+          <span v-if="item.new">{{ item.quanity }}</span>
+        </template>
+        <template #[`item.unit`]="{ item }">
+          <api-text-field
+            v-if="!item.new"
+            dense
+            :uri="item.uri"
+            fieldname="unit" />
+          <span v-if="item.new">{{ item.unit }}</span>
+        </template>
+        <template #[`item.article`]="{ item }">
+          <api-text-field
+            v-if="!item.new"
+            dense
+            :uri="item.uri"
+            fieldname="article" />
+          <span v-if="item.new">{{ item.article }}</span>
+        </template>
+        <template #[`item.listName`]="{ item }">
+          <api-select
+            v-if="!item.new"
+            dense
+            :uri="item.uri"
+            relation="materialList"
+            fieldname="materialListId"
+            :items="materialLists" />
+          <span v-if="item.new">{{ item.listName }}</span>
+        </template>
+        <template #[`item.actions`]="{ item }">
+          <v-icon
+            v-if="!item.new"
+            small
+            @click="deleteMaterialItem(item)">
+            mdi-delete
+          </v-icon>
+          <v-progress-circular
+            v-if="item.new"
+            size="16"
+            indeterminate
+            color="primary" />
+        </template>
+        <template #no-data>
+          <v-btn
+            color="primary">
+            No material items found
+          </v-btn>
+        </template>
 
-        <div v-else style="margin-top: 20px; text-align: right">
-          <dialog-material-item-create :camp="camp" :content-node="contentNode">
-            <template #activator="{ on }">
-              <v-btn color="success" v-on="on">
-                {{ $tc('components.camp.periodMaterialLists.addNewItem') }}
-              </v-btn>
-            </template>
-          </dialog-material-item-create>
-        </div>
-      </template>
+        <template v-if="!layoutMode" #[`body.append`]>
+          <material-create-item
+            v-if="$vuetify.breakpoint.smAndUp"
+            :camp="camp"
+            :content-node="contentNode"
+            @item-adding="onItemAdding" />
+
+          <div v-else style="margin-top: 20px; text-align: right">
+            <dialog-material-item-create :camp="camp" :content-node="contentNode">
+              <template #activator="{ on }">
+                <v-btn color="success" v-on="on">
+                  {{ $tc('components.camp.periodMaterialLists.addNewItem') }}
+                </v-btn>
+              </template>
+            </dialog-material-item-create>
+          </div>
+        </template>
+      </v-data-table>
     </div>
   </card-content-node>
 </template>
@@ -131,7 +192,7 @@
 import ApiTextField from '../../form/api/ApiTextField.vue'
 import ApiSelect from '../../form/api/ApiSelect.vue'
 import DialogMaterialItemCreate from '../../dialog/DialogMaterialItemCreate.vue'
-import DialogMaterialItemEdit from '../../dialog/DialogMaterialItemEdit.vue'
+// import DialogMaterialItemEdit from '../../dialog/DialogMaterialItemEdit.vue'
 import MaterialCreateItem from '../../camp/MaterialCreateItem.vue'
 import CardContentNode from '@/components/activity/CardContentNode.vue'
 import { contentNodeMixin } from '@/mixins/contentNodeMixin.js'
@@ -143,13 +204,26 @@ export default {
     ApiTextField,
     ApiSelect,
     DialogMaterialItemCreate,
-    DialogMaterialItemEdit,
+    /* DialogMaterialItemEdit, */
     MaterialCreateItem
   },
   mixins: [contentNodeMixin],
   data () {
     return {
-      newMaterialItems: {}
+      newMaterialItems: {},
+      headers: [
+        {
+          text: this.$tc('entity.materialItem.fields.quantity'),
+          align: 'start',
+          sortable: false,
+          value: 'quantity',
+          width: '10%'
+        },
+        { text: this.$tc('entity.materialItem.fields.unit'), value: 'unit', sortable: false, width: '10%' },
+        { text: this.$tc('entity.materialItem.fields.article'), value: 'article', width: '40%' },
+        { text: this.$tc('entity.materialList.name'), value: 'listName', width: '35%' },
+        { text: '', value: 'actions', sortable: false, width: '5%' }
+      ]
     }
   },
   computed: {
@@ -161,6 +235,31 @@ export default {
     },
     materialItems () {
       return this.api.get().materialItems({ contentNodeId: this.contentNode.id })
+    },
+    materialItemsData () {
+      const items = this.materialItems.items.map(item => ({
+        id: item.id,
+        uri: item._meta.self,
+        quantity: item.quantity,
+        unit: item.unit,
+        article: item.article,
+        listId: item.materialList().id,
+        listName: item.materialList().name
+      }))
+
+      // eager add new Items
+      for (const key in this.newMaterialItems) {
+        const mi = this.newMaterialItems[key]
+        items.push({
+          id: key,
+          quantity: mi.quantity,
+          unit: mi.unit,
+          article: mi.article,
+          new: true
+        })
+      }
+
+      return items
     },
     materialItemsSorted () {
       const items = this.materialItems.items
@@ -184,7 +283,7 @@ export default {
       return this.materialItems.filter(mi => mi.materialList().id === materialList.id)
     },
     deleteMaterialItem (materialItem) {
-      this.api.del(materialItem)
+      this.api.del(materialItem.uri)
     },
     onItemAdding (key, data, res) {
       this.$set(this.newMaterialItems, key, data)
--- frontend/src/components/camp/MaterialCreateItem.vue
+++ frontend/src/components/camp/MaterialCreateItem.vue
@@ -1,78 +1,69 @@
 <template>
-  <div v-if="materialLists.length > 0">
-    <ValidationObserver ref="validation" v-slot="{ handleSubmit }">
-      <v-form @submit.prevent="handleSubmit(createMaterialItem)">
-        <v-simple-table style="margin-top: 10px">
-          <colgroup>
-            <col style="width: 55px;">
-            <col style="width: 15%;">
-            <col>
-            <col style="width: 20%;">
-            <col style="width: 20%;">
-          </colgroup>
-          <tbody>
-            <tr>
-              <td style="vertical-align: top;">
-                <e-text-field
-                  ref="quantity"
-                  v-model="materialItem.quantity"
-                  dense
-                  :name="$tc('entity.materialItem.fields.quantity')"
-                  fieldname="quantity" />
-              </td>
-              <td style="vertical-align: top;">
-                <e-text-field
-                  v-model="materialItem.unit"
-                  dense
-                  :name="$tc('entity.materialItem.fields.unit')"
-                  fieldname="unit" />
-              </td>
-              <td style="vertical-align: top;">
-                <e-text-field
-                  v-model="materialItem.article"
-                  dense
-                  vee-rules="required"
-                  :name="$tc('entity.materialItem.fields.article')"
-                  fieldname="article" />
-              </td>
-              <td style="vertical-align: top;">
-                <e-select
-                  v-model="materialItem.materialListId"
-                  dense
-                  vee-rules="required"
-                  :name="$tc('entity.materialList.name')"
-                  fieldname="materialListId"
-                  :items="materialLists" />
-              </td>
-              <td style="vertical-align: top; padding-top: 7px;">
-                <v-btn type="submit">
-                  {{ $tc('global.button.add') }}
-                </v-btn>
-              </td>
-            </tr>
-          </tbody>
-        </v-simple-table>
-      </v-form>
-    </validationobserver>
-  </div>
-  <div v-else>
-    <p>
-      {{ $tc('components.camp.materialCreateItem.noMaterialListAvailable') }}
-    </p>
-    <v-btn :to="campRoute(camp, 'admin')">
-      <v-icon :left="$vuetify.breakpoint.mdAndUp">mdi-cogs</v-icon>
-      {{ $tc('views.camp.navigationCamp.admin') }}
-    </v-btn>
-  </div>
+  <ValidationObserver v-if="materialLists.length > 0"
+                      ref="validation"
+                      tag="tr"
+                      style="margin-top: 10px"
+                      @submit.prevent="createMaterialItem">
+    <td style="vertical-align: top;">
+      <e-text-field
+        ref="quantity"
+        v-model="materialItem.quantity"
+        dense
+        :name="$tc('entity.materialItem.fields.quantity')"
+        fieldname="quantity" />
+    </td>
+    <td style="vertical-align: top;">
+      <e-text-field
+        v-model="materialItem.unit"
+        dense
+        :name="$tc('entity.materialItem.fields.unit')"
+        fieldname="unit" />
+    </td>
+    <td style="vertical-align: top;">
+      <e-text-field
+        v-model="materialItem.article"
+        dense
+        vee-rules="required"
+        :name="$tc('entity.materialItem.fields.article')"
+        fieldname="article" />
+    </td>
+    <td style="vertical-align: top;">
+      <e-select
+        v-model="materialItem.materialListId"
+        dense
+        vee-rules="required"
+        :name="$tc('entity.materialList.name')"
+        fieldname="materialListId"
+        :items="materialLists" />
+    </td>
+    <td style="vertical-align: top; padding-top: 7px;">
+      <button-add hide-label @click="submitForm" />
+    </td>
+  </ValidationObserver>
+
+  <tr v-else>
+    <td colspan="5">
+      <div>
+        <p>
+          {{ $tc('components.camp.materialCreateItem.noMaterialListAvailable') }}
+        </p>
+        <v-btn :to="campRoute(camp, 'admin')">
+          <v-icon :left="$vuetify.breakpoint.mdAndUp">mdi-cogs</v-icon>
+          {{ $tc('views.camp.navigationCamp.admin') }}
+        </v-btn>
+      </div>
+    </td>
+  </tr>
 </template>
 
 <script>
 import { campRoute } from '@/router.js'
 import { ValidationObserver } from 'vee-validate'
+import ButtonAdd from '@/components/buttons/ButtonAdd.vue'
 
 export default {
   name: 'MaterialCreateItem',
-  components: { ValidationObserver },
+  components: { ValidationObserver, ButtonAdd },
   props: {
     camp: { type: Object, required: true },
     period: { type: Object, default: null },
@@ -92,6 +83,12 @@ export default {
     }
   },
   methods: {
+    async submitForm () {
+      const isValid = await this.$refs.validation.validate()
+      if (isValid) {
+        this.createMaterialItem()
+      }
+    },
     createMaterialItem () {
       this.api.href(this.api.get(), 'materialItems').then(uri => {
         const key = Date.now()
@@ -109,6 +106,8 @@ export default {
         this.$refs.validation.reset()
 
         const res = this.api.post(uri, data)
+
+        // fire event to allow for eager adding before post has finished
         this.$emit('item-adding', key, data, res)
       })
     },
