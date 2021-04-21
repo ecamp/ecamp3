4bcaf17c3a344ea7e267785721e74decadd1ba6a
Pirmin Mattmann <pimattmann@gmail.com>
2021-04-21 17:41:18 +0200

    use laminas-math-rand; unittest

--- backend/module/eCampLib/src/Util/IdGenerator.php
+++ backend/module/eCampLib/src/Util/IdGenerator.php
@@ -2,14 +2,10 @@
 
 namespace eCamp\Lib\Util;
 
-use RuntimeException;
+use Laminas\Math\Rand;
 
 class IdGenerator {
     public static function generateRandomHexString(int $length): string {
-        try {
-            return bin2hex(random_bytes($length / 2));
-        } catch (\Exception $e) {
-            throw new RuntimeException($e);
-        }
+        return bin2hex(Rand::getBytes($length / 2));
     }
 }
