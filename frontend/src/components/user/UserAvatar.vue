5420970ac49a93fb7d9ee4b3dbf4d3131c6bf96f
Pirmin Mattmann <pimattmann@gmail.com>
2021-04-27 10:41:02 +0200

    simlify color- and initials-calculation

--- frontend/src/components/user/UserAvatar.vue
+++ frontend/src/components/user/UserAvatar.vue
@@ -23,81 +23,30 @@ export default {
     },
     color () {
       const id = this.user.id
-      const h = (parseInt(id, 16) % 256) / 256
-      return this.hsvToRgb(h, 1, 0.6)
+      const h = (parseInt(id, 16) % 360)
+      return `hsl(${h}, 100%, 40%)`
     },
     initials () {
+      let displayName = ''
       if (this.user.displayName) {
-        const items = this.user.displayName.split(/[ ,._-]/)
-        if (items.length === 1) {
-          return this.user.displayName.substr(0, 2)
-        } else {
-          return items[0].substr(0, 1) + items[1].substr(0, 1)
-        }
+        displayName = this.user.displayName
       } else if (this.value.inviteEmail) {
-        const localPart = this.value.inviteEmail.split('@').shift()
-        const items = localPart.split(/[ ,._-]/)
-        if (items.length === 1) {
-          return this.value.inviteEmail.substr(0, 2)
-        } else {
-          return items[0].substr(0, 1) + items[1].substr(0, 1)
-        }
+        displayName = this.value.inviteEmail.split('@', 2).shift()
       }
-      return ''
+      let items = displayName.split(' ', 2)
+      if (items.length === 1) {
+        items = items.shift().split(/[,._-]/, 2)
+      }
+      if (items.length === 1) {
+        return displayName.substr(0, 2)
+      } else {
+        return items[0].substr(0, 1) + items[1].substr(0, 1)
+      }
     },
     style () {
       return {
         fontSize: (this.size / 2.5) + 'px'
       }
-    }
-  },
-  methods: {
-    hsvToRgb (h, s, v) {
-      let r, g, b
-
-      const i = Math.floor(h * 6)
-      const f = h * 6 - i
-      const p = v * (1 - s)
-      const q = v * (1 - f * s)
-      const t = v * (1 - (1 - f) * s)
-
-      switch (i % 6) {
-        case 0:
-          r = Math.floor(v * 255)
-          g = Math.floor(t * 255)
-          b = Math.floor(p * 255)
-          break
-        case 1:
-          r = Math.floor(q * 255)
-          g = Math.floor(v * 255)
-          b = Math.floor(p * 255)
-          break
-        case 2:
-          r = Math.floor(p * 255)
-          g = Math.floor(v * 255)
-          b = Math.floor(t * 255)
-          break
-        case 3:
-          r = Math.floor(p * 255)
-          g = Math.floor(q * 255)
-          b = Math.floor(v * 255)
-          break
-        case 4:
-          r = Math.floor(t * 255)
-          g = Math.floor(p * 255)
-          b = Math.floor(v * 255)
-          break
-        case 5:
-          r = Math.floor(v * 255)
-          g = Math.floor(p * 255)
-          b = Math.floor(q * 255)
-          break
-      }
-
-      return '#' +
-        r.toString(16).padStart(2, '0') +
-        g.toString(16).padStart(2, '0') +
-        b.toString(16).padStart(2, '0')
     }
   }
 }
