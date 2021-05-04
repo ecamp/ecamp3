db01044d92aaae893961bf89ca2a129e97760bd7
Pirmin Mattmann <pimattmann@gmail.com>
2021-05-04 23:24:21 +0200

    embed user in profile-endpoint

--- backend/module/eCampCore/test/Hydrator/UserHydratorTest.php
+++ backend/module/eCampCore/test/Hydrator/UserHydratorTest.php
@@ -13,19 +13,34 @@ class UserHydratorTest extends AbstractTestCase {
     public function testExtract(): void {
         $user = new User();
         $user->setUsername('test');
+        $user->setFirstname('fname');
+        $user->setSurname('sname');
+        $user->setNickname('nname');
 
         $hydrator = new UserHydrator();
         $data = $hydrator->extract($user);
 
         $this->assertEquals('test', $data['username']);
+        $this->assertEquals('fname', $data['firstname']);
+        $this->assertEquals('sname', $data['surname']);
+        $this->assertEquals('nname', $data['nickname']);
     }
 
     public function testHydrate(): void {
         $user = new User();
+        $data = [
+            'username' => 'test',
+            'firstname' => 'fname',
+            'surname' => 'sname',
+            'nickname' => 'nname',
+        ];
 
         $hydrator = new UserHydrator();
-        $hydrator->hydrate(['username' => 'test'], $user);
+        $hydrator->hydrate($data, $user);
 
         $this->assertEquals('test', $user->getUsername());
+        $this->assertEquals('fname', $user->getFirstname());
+        $this->assertEquals('sname', $user->getSurname());
+        $this->assertEquals('nname', $user->getNickname());
     }
 }
