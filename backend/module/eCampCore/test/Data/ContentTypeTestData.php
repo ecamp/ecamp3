abddb2ba6edc606cd605d27fae5779344f23fe8a
Pirmin Mattmann <pimattmann@gmail.com>
2021-05-10 21:20:17 +0200

    UnitTest ContentType Strategy (SingleText and Storyboard)

--- backend/module/eCampCore/test/Data/ContentTypeTestData.php
+++ backend/module/eCampCore/test/Data/ContentTypeTestData.php
@@ -6,6 +6,7 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Persistence\ObjectManager;
 use eCamp\ContentType\ColumnLayout\Strategy as ColumnLayoutStrategy;
 use eCamp\ContentType\Material\Strategy as MaterialStrategy;
+use eCamp\ContentType\SingleText\Strategy as SingleTextStrategy;
 use eCamp\ContentType\Storyboard\Strategy as StoryboardStrategy;
 use eCamp\Core\Entity\ContentType;
 
@@ -13,6 +14,7 @@ class ContentTypeTestData extends AbstractFixture {
     public static $TYPE_COLUMN_LAYOUT = ContentType::class.':COLUMN_LAYOUT';
     public static $TYPE1 = ContentType::class.':TYPE1';
     public static $TYPE_MATERIAL = ContentType::class.':TYPE_MATERIAL';
+    public static $TYPE_SINGLE_TEXT = ContentType::class.':SINGLE_TEXT';
 
     public function load(ObjectManager $manager): void {
         $contentType = new ContentType();
@@ -41,5 +43,14 @@ class ContentTypeTestData extends AbstractFixture {
         $manager->flush();
 
         $this->addReference(self::$TYPE_COLUMN_LAYOUT, $contentType);
+
+        $contentType = new ContentType();
+        $contentType->setName('SingleText');
+        $contentType->setStrategyClass(SingleTextStrategy::class);
+
+        $manager->persist($contentType);
+        $manager->flush();
+
+        $this->addReference(self::$TYPE_SINGLE_TEXT, $contentType);
     }
 }
