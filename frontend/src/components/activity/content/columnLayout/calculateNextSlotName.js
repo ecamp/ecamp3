7ae16a3c151a418a0373adcf2f8c9bee7e67edd8
Carlo Beltrame <carlo@beltra.me>
2021-05-11 09:10:13 +0200

    Simplify algorithm for generating slot names

--- frontend/src/components/activity/content/columnLayout/calculateNextSlotName.js
+++ frontend/src/components/activity/content/columnLayout/calculateNextSlotName.js
@@ -1,34 +1,10 @@
 import cloneDeep from 'lodash/cloneDeep'
 
-const max = function (slotNames) {
-  return slotNames.reduce((max, slotName) => {
-    if (slotName.length > max.length) return slotName
-    if (slotName.length < max.length) return max
-    return slotName > max ? slotName : max
-  }, '')
-}
-
-const isIncrementable = function (character) {
-  return (character >= '0' && character < '9') ||
-    (character >= 'a' && character < 'z') ||
-    (character >= 'A' && character < 'Z')
-}
-
-const next = function (slotName) {
-  if (slotName === '') return '1'
-
-  let i = slotName.length - 1
-  while (!isIncrementable(slotName[i]) && i >= 0) i--
-
-  if (i === -1) return '1' + slotName
-
-  const chars = slotName.split('')
-  chars[i] = String.fromCharCode(chars[i].charCodeAt(0) + 1)
-  return chars.join('')
-}
-
 export function calculateNextSlotName (slotNames) {
-  return next(max(slotNames))
+  let i = 0
+  while (true) {
+    if (!slotNames.includes((++i).toString())) return i.toString()
+  }
 }
 
 export function limitColumnWidths (columns, minWidth = 3, maxWidth = 12) {
