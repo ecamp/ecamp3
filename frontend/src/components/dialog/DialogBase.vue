d680206e7990decf80f94a94993642a283fae7af
Carlo Beltrame <carlo@beltra.me>
2021-05-11 15:02:58 +0200

    Fix events emitted in catch from dialog inside menu

--- frontend/src/components/dialog/DialogBase.vue
+++ frontend/src/components/dialog/DialogBase.vue
@@ -50,21 +50,21 @@ export default {
     },
     create () {
       this.error = null
-      const promise = this.api.post(this.entityUri, this.entityData).then(this.close, this.onError)
+      const _events = this._events
+      this.api.post(this.entityUri, this.entityData).then(this.close, e => this.onError(_events, e))
       this.$emit('submit')
-      return promise
     },
     update () {
       this.error = null
-      const promise = this.api.patch(this.entityUri, this.entityData).then(this.close, this.onError)
+      const _events = this._events
+      this.api.patch(this.entityUri, this.entityData).then(this.close, e => this.onError(_events, e))
       this.$emit('submit')
-      return promise
     },
     del () {
       this.error = null
-      const promise = this.api.del(this.entityUri).then(this.close, this.onError)
+      const _events = this._events
+      this.api.del(this.entityUri).then(this.close, e => this.onError(_events, e))
       this.$emit('submit')
-      return promise
     },
     onSuccess () {
       this.$emit('success')
@@ -73,8 +73,16 @@ export default {
     close () {
       this.showDialog = false
     },
-    onError (e) {
+    onError (originalHandlers, e) {
+      // By the time we get here, the dialog might be closed because an enclosing menu might be closed.
+      // See https://github.com/vuetifyjs/vuetify/issues/7021
+      // In this case, the event handlers in here are cleared, so we need to temporarily restore them
+      // to make the $emit work correctly
+      const eventHandlers = this._events
+      this._events = originalHandlers
       this.$emit('error', e)
+      this._events = eventHandlers
+
       this.error = e.message
       if (e.response) {
         if (e.response.status === 409 /* Conflict */) {
