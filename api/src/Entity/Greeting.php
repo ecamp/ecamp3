eb828c94e513360db9056b77bcba7313a6099dd5
Carlo Beltrame <carlo@beltra.me>
2021-05-12 12:54:47 +0200

    Add API platform

---
+++ api/src/Entity/Greeting.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * This is a dummy entity. Remove it!
+ *
+ * @ORM\Entity
+ */
+#[ApiResource(mercure: true)]
+class Greeting
+{
+    /**
+     * The entity ID
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id = null;
+
+    /**
+     * A nice person
+     *
+     * @ORM\Column
+     */
+    #[Assert\NotBlank]
+    public string $name = '';
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
