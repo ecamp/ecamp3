212c4f339fa75e68579535846738431e16dfcc5f
Carlo Beltrame <carlo@beltra.me>
2021-05-14 21:01:37 +0200

    Install symbok to avoid having to write boilerplate code

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Mtarld\SymbokBundle\SymbokBundle::class => ['all' => true],
 ];
