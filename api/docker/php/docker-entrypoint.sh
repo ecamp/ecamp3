bb1d9c7cff48115695bcd89e95187c45efa23f98
Carlo Beltrame <carlo@beltra.me>
2021-05-14 13:48:37 +0200

    Run php container as non-root

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -11,11 +11,11 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	if [ "$APP_ENV" != 'prod' ]; then
 		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
 	fi
-	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+	#ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
 
 	mkdir -p var/cache var/log
-	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+	#setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+	#setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
 	if [ "$APP_ENV" != 'prod' ]; then
 		composer install --prefer-dist --no-progress --no-interaction
