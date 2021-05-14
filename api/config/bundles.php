2581c24e056cb4502d16e895756d7ffe0bc86083
Carlo Beltrame <carlo@beltra.me>
2021-05-14 19:37:36 +0200

    Install stof doctrine extensions bundle
    This provides implementations for many useful database related tasks,
    such as creation/update dates, tree structures in the DB, and more.

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
