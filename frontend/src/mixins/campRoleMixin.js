7b0eec8596314dc6af58a8946ee02e5badd8dcee
BacLuc <lucius.bachmann@gmx.ch>
2021-06-06 16:00:02 +0200

    frontend/Story.vue: disable editable switch if user is guest
    Pass camp as function instead of object into campRoleMixin.
    
    Issue: #1415

--- frontend/src/mixins/campRoleMixin.js
+++ frontend/src/mixins/campRoleMixin.js
@@ -4,18 +4,24 @@ export const campRoleMixin = {
       return this.isMember || this.isManager
     },
     isGuest () {
-      return this.camp?.role === 'guest'
+      return this.role === 'guest'
     },
     isManager () {
-      return this.camp?.role === 'manager'
+      return this.role === 'manager'
     },
     isMember () {
-      return this.camp?.role === 'member'
+      return this.role === 'member'
+    },
+    role () {
+      if (typeof this.camp === 'function') {
+        return this.camp()?.role
+      }
+      return this.camp?.role
     }
   },
   mounted () {
-    if (typeof this.camp !== 'object') {
-      throw new Error('User of the campRoleMixin must expose a method/proxy camp()')
+    if (typeof this.camp !== 'object' && typeof this.camp !== 'function') {
+      throw new Error('User of the campRoleMixin must expose a camp as object proxy or function')
     }
   }
 }
