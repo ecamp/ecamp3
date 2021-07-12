66cf4764acfaceceb3a86387f6e54e20188eb797
Carlo Beltrame <carlo@beltra.me>
2021-07-12 16:28:12 +0200

    Fix deprecation warnings for api platform 2.6

--- api/src/Validator/MaterialItemUpdateGroupSequence.php
+++ api/src/Validator/MaterialItemUpdateGroupSequence.php
@@ -2,10 +2,11 @@
 
 namespace App\Validator;
 
+use ApiPlatform\Core\Bridge\Symfony\Validator\ValidationGroupsGeneratorInterface;
 use Symfony\Component\Validator\Constraints\GroupSequence;
 
-class MaterialItemUpdateGroupSequence {
-    public function __invoke() {
+class MaterialItemUpdateGroupSequence implements ValidationGroupsGeneratorInterface {
+    public function __invoke($object) {
         return new GroupSequence(['materialItem:update', 'Default']); // now, no matter which is first in the class declaration, it will be tested in this order.
     }
 }
