7f123d8474c2768e22cd2a53c5b8f6823354c03a
Carlo Beltrame <carlo@beltra.me>
2021-09-07 13:44:32 +0200

    Extract the filtering by camp collaboration to a trait

--- api/src/Repository/CampRepository.php
+++ api/src/Repository/CampRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repository;
 
 use App\Entity\Camp;
-use App\Entity\CampCollaboration;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\QueryBuilder;
@@ -16,16 +15,14 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Camp[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class CampRepository extends ServiceEntityRepository implements CanFilterByUserInterface {
+    use FiltersByCampCollaboration;
+
     public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, Camp::class);
     }
 
     public function filterByUser(QueryBuilder $queryBuilder, User $user): void {
         $rootAlias = $queryBuilder->getRootAliases()[0];
-        $queryBuilder->leftJoin("{$rootAlias}.collaborations", 'campCollaboration');
-        $queryBuilder->andWhere("(campCollaboration.user = :current_user and campCollaboration.status = :established) or {$rootAlias}.isPrototype = :true");
-        $queryBuilder->setParameter('current_user', $user->getId());
-        $queryBuilder->setParameter('established', CampCollaboration::STATUS_ESTABLISHED);
-        $queryBuilder->setParameter('true', true);
+        $this->filterByCampCollaboration($queryBuilder, $user, $rootAlias);
     }
 }
---
+++ api/src/Repository/FiltersByCampCollaboration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CampCollaboration;
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+
+trait FiltersByCampCollaboration {
+    /**
+     * Applies a filter that checks for an active campCollaboration of the passed user, or that
+     * the camp is a prototype.
+     * Assumes the queryBuilder already knows how to get to the corresponding camp. You can pass
+     * the alias of the camp as the third argument if it's anything other than "camp".
+     *
+     * @param string $campAlias
+     */
+    public function filterByCampCollaboration(QueryBuilder $queryBuilder, User $user, $campAlias = 'camp'): void {
+        $queryBuilder->leftJoin("{$campAlias}.collaborations", 'filter_campCollaboration');
+        $queryBuilder->andWhere("(filter_campCollaboration.user = :current_user and filter_campCollaboration.status = :established) or {$campAlias}.isPrototype = :true");
+        $queryBuilder->setParameter('current_user', $user->getId());
+        $queryBuilder->setParameter('established', CampCollaboration::STATUS_ESTABLISHED);
+        $queryBuilder->setParameter('true', true);
+    }
+}
