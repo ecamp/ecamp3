f2f773df66f6d77ca87e63f6be8b8e35d14fb1a3
BacLuc <lucius.bachmann@gmx.ch>
2021-10-02 16:06:05 +0200

    api/docker-entrypoint.sh: make keys readable by www-data
    For the deployment, we create the keys with root, but must read them with www-data.

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -24,8 +24,8 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ] || [ "$1
 			mkdir -p config/jwt
 			echo "$jwt_passphrase" | openssl genpkey -out config/jwt/private.pem -pass stdin -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
 			echo "$jwt_passphrase" | openssl pkey -in config/jwt/private.pem -passin stdin -out config/jwt/public.pem -pubout
-			#setfacl -R -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
-			#setfacl -dR -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
+			setfacl -R -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
+			setfacl -dR -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
 		fi
 	fi
 
