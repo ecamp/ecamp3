0180456cd256c9af7008989c0dc56478bd857e74
BacLuc <lucius.bachmann@gmx.ch>
2021-10-02 20:21:20 +0200

    api: handle incomplete JWT Tokens as if there were none
    The provided SplitCookieExtractor does not check, if all needed cookies are set.
    Thus we replace it.
    https://github.com/lexik/LexikJWTAuthenticationBundle/issues/930
    
    Issue: #1999

---
+++ api/src/Security/JWT/SplitCookieExtractor.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Security\JWT;
+
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\TokenExtractorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class SplitCookieExtractor implements TokenExtractorInterface {
+    /**
+     * @var array
+     */
+    private $cookies;
+
+    /**
+     * @param array $cookies
+     */
+    public function __construct($cookies) {
+        $this->cookies = $cookies;
+    }
+
+    public function extract(Request $request): bool|string {
+        $jwtCookies = [];
+
+        foreach ($this->cookies as $cookie) {
+            $inputBag = $request->cookies->get($cookie, false);
+            if ($inputBag) {
+                $jwtCookies[] = $inputBag;
+            }
+        }
+
+        if (count($this->cookies) != count($jwtCookies)) {
+            return false;
+        }
+
+        return implode('.', $jwtCookies);
+    }
+}
