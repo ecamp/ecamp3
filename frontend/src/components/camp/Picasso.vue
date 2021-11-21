c82af2aaff6d11014d5977e49fefdf53985d78f1
Manuel Meister <news.manuelsworld@gmail.cm>
2021-11-21 17:31:55 +0100

    Make picasso interval sizing smaller

--- frontend/src/components/camp/Picasso.vue
+++ frontend/src/components/camp/Picasso.vue
@@ -174,7 +174,7 @@ export default {
       return this.period().camp()
     },
     computedIntervalHeight () {
-      return this.intervalHeight !== 0 ? this.intervalHeight : this.$vuetify.breakpoint.xsOnly ? (this.$vuetify.breakpoint.height - 140) / 19 : Math.max((this.$vuetify.breakpoint.height - 174) / 19, 32)
+      return this.intervalHeight !== 0 ? this.intervalHeight : this.$vuetify.breakpoint.xsOnly ? (this.$vuetify.breakpoint.height - 140) / 19 : Math.max((this.$vuetify.breakpoint.height - 204) / 19, 32)
     }
   },
   mounted () {
