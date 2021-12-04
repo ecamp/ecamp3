e007ff5b0d9f2618a42d22a398b25f10f81c52e9
Urban Suppiger <urban@suppiger.net>
2021-12-04 23:37:57 +0100

    remove unnecessary reactives/ref

--- frontend/src/components/camp/picasso/useDragAndDrop.js
+++ frontend/src/components/camp/picasso/useDragAndDrop.js
@@ -10,22 +10,22 @@ export default function useDragAndDrop (editable, period, dialogActivityCreate, 
    */
 
   // timestamp of mouse location when drag & drop event started
-  const mouseStartTime = ref(null)
+  let mouseStartTime = null
 
   // reference to dragged scheduleEntry (null = no drag & drop ongoing)
-  const draggedEntry = ref(null)
+  let draggedEntry = null
 
   // offset between (dragged) entry startTime and mouse location
-  const draggedEntryMouseOffset = ref(null)
+  let draggedEntryMouseOffset = null
 
   // existing entry that is being resized
-  const resizedEntry = ref(null)
+  let resizedEntry = null
 
   // original end time of entry which is being resized
-  const resizedEntryOldEndTime = ref(null)
+  let resizedEntryOldEndTime = null
 
   // used to detect mouse down events that were interpreted as clicks
-  const openedInNewTab = ref(false)
+  let openedInNewTab = false
 
   /**
    * external data
@@ -46,52 +46,52 @@ export default function useDragAndDrop (editable, period, dialogActivityCreate, 
 
   // move an existing entry
   const moveDraggedEntry = (mouse) => {
-    const start = draggedEntry.value.startTime
-    const end = draggedEntry.value.endTime
+    const start = draggedEntry.startTime
+    const end = draggedEntry.endTime
     const duration = end - start
 
-    const newStart = roundTimeDown((mouse - draggedEntryMouseOffset.value))
+    const newStart = roundTimeDown((mouse - draggedEntryMouseOffset))
     const newEnd = newStart + duration
 
-    draggedEntry.value.startTime = newStart
-    draggedEntry.value.endTime = newEnd
+    draggedEntry.startTime = newStart
+    draggedEntry.endTime = newEnd
   }
 
   // resize an entry (existing or placeholder)
   const resizeExistingEntry = (mouse) => {
     const mouseRounded = roundTimeUp(mouse)
-    const min = Math.min(mouseRounded, roundTimeDown(mouseStartTime.value))
-    const max = Math.max(mouseRounded, roundTimeDown(mouseStartTime.value))
+    const min = Math.min(mouseRounded, roundTimeDown(mouseStartTime))
+    const max = Math.max(mouseRounded, roundTimeDown(mouseStartTime))
 
-    resizedEntry.value.startTime = min
-    resizedEntry.value.endTime = max
+    resizedEntry.startTime = min
+    resizedEntry.endTime = max
   }
 
   // resize the placeholder for new entry
   const resizeNewEntry = (mouse) => {
     const mouseRounded = roundTimeUp(mouse)
-    const min = Math.min(mouseRounded, roundTimeDown(mouseStartTime.value))
-    const max = Math.max(mouseRounded, roundTimeDown(mouseStartTime.value))
+    const min = Math.min(mouseRounded, roundTimeDown(mouseStartTime))
+    const max = Math.max(mouseRounded, roundTimeDown(mouseStartTime))
 
     newEntry.value.startTime = min
     newEntry.value.endTime = max
   }
 
   const clearResizedEntry = () => {
-    resizedEntry.value = null
-    resizedEntryOldEndTime.value = null
-    mouseStartTime.value = null
+    resizedEntry = null
+    resizedEntryOldEndTime = null
+    mouseStartTime = null
   }
 
   const clearDraggedEntry = () => {
-    draggedEntryMouseOffset.value = null
-    draggedEntry.value = null
-    mouseStartTime.value = null
+    draggedEntryMouseOffset = null
+    draggedEntry = null
+    mouseStartTime = null
   }
 
   const clearNewEntry = () => {
     newEntry.value = null
-    mouseStartTime.value = null
+    mouseStartTime = null
   }
 
   // this creates a placeholder for a new schedule entry and make it resizable
@@ -145,14 +145,14 @@ export default function useDragAndDrop (editable, period, dialogActivityCreate, 
     if (!entry.tmpEvent && (nativeEvent.button === 1 || nativeEvent.metaKey || nativeEvent.ctrlKey)) {
       // Click with middle mouse button, or click while holding cmd/ctrl opens new tab
       showScheduleEntry(entry, true)
-      openedInNewTab.value = true
+      openedInNewTab = true
     } else if (nativeEvent.button === 2) {
       // don't move event if middle mouse button
     } else if (editable.value) {
       if (entry && timed) {
         // start Drag & Drop
-        draggedEntry.value = entry
-        draggedEntryMouseOffset.value = null // not know yet: will be populated by timeMouseDown event
+        draggedEntry = entry
+        draggedEntryMouseOffset = null // not know yet: will be populated by timeMouseDown event
       }
     }
   }
@@ -160,22 +160,22 @@ export default function useDragAndDrop (editable, period, dialogActivityCreate, 
   // triggered with MouseDown event anywhere on the calendar (independent of clicking on entry or not)
   const timeMouseDown = (tms) => {
     // if entryMouseDown was interpreted as click event --> cancel drag & drop and return early
-    if (openedInNewTab.value) {
-      openedInNewTab.value = false
+    if (openedInNewTab) {
+      openedInNewTab = false
       return
     }
 
     if (editable.value) {
       const mouse = toTime(tms)
 
-      if (mouseStartTime.value === null) {
-        mouseStartTime.value = mouse
+      if (mouseStartTime === null) {
+        mouseStartTime = mouse
       }
 
       // Drag hast just started
-      if (draggedEntry.value) {
-        const start = draggedEntry.value.startTime
-        draggedEntryMouseOffset.value = mouse - start
+      if (draggedEntry) {
+        const start = draggedEntry.startTime
+        draggedEntryMouseOffset = mouse - start
       } else {
       // No entry is being dragged --> create a placeholder for a new schedule entry
         createNewEntry(mouse)
@@ -188,9 +188,9 @@ export default function useDragAndDrop (editable, period, dialogActivityCreate, 
     if (editable.value) {
       const mouse = toTime(tms)
 
-      if (draggedEntry.value) {
+      if (draggedEntry) {
         moveDraggedEntry(mouse)
-      } else if (resizedEntry.value) {
+      } else if (resizedEntry) {
         resizeExistingEntry(mouse)
       } else if (newEntry.value) {
         resizeNewEntry(mouse)
@@ -205,24 +205,24 @@ export default function useDragAndDrop (editable, period, dialogActivityCreate, 
     }
 
     // Drag & Drop
-    if (draggedEntry.value) {
+    if (draggedEntry) {
       const minuteThreshold = 15
       const threshold = minuteThreshold * 60 * 1000
       const now = toTime(tms)
 
       // interpret shifts below 15min as a click event
-      if (Math.abs(now - mouseStartTime.value) < threshold) {
-        showScheduleEntry(draggedEntry.value)
+      if (Math.abs(now - mouseStartTime) < threshold) {
+        showScheduleEntry(draggedEntry)
         return
       }
 
       // save to API
       const patchedScheduleEntry = {
-        periodOffset: draggedEntry.value.periodOffset,
-        length: draggedEntry.value.length
+        periodOffset: draggedEntry.periodOffset,
+        length: draggedEntry.length
       }
       isSaving.value = true
-      api.patch(draggedEntry.value._meta.self, patchedScheduleEntry).then(() => {
+      api.patch(draggedEntry._meta.self, patchedScheduleEntry).then(() => {
         patchError.value = null
         isSaving.value = false
       }).catch((error) => {
@@ -234,15 +234,15 @@ export default function useDragAndDrop (editable, period, dialogActivityCreate, 
     } else if (newEntry.value) {
       // placeholder for new schedule entry was created --> open dialog to create new activity
       dialogActivityCreate(newEntry.value, clearNewEntry)
-    } else if (resizedEntry.value) {
-      if (resizedEntry.value.endTime !== resizedEntryOldEndTime.value) {
+    } else if (resizedEntry) {
+      if (resizedEntry.endTime !== resizedEntryOldEndTime) {
       // existing entry was resized --> save to API
         const patchedScheduleEntry = {
-          periodOffset: resizedEntry.value.periodOffset,
-          length: resizedEntry.value.length
+          periodOffset: resizedEntry.periodOffset,
+          length: resizedEntry.length
         }
         isSaving.value = true
-        api.patch(resizedEntry.value._meta.self, patchedScheduleEntry).then(() => {
+        api.patch(resizedEntry._meta.self, patchedScheduleEntry).then(() => {
           patchError.value = null
           isSaving.value = false
         }).catch((error) => {
@@ -257,9 +257,9 @@ export default function useDragAndDrop (editable, period, dialogActivityCreate, 
   // TODO docu: for which use case is this needed??
   const nativeMouseUp = () => {
     if (editable.value) {
-      if (resizedEntry.value) {
-        if (resizedEntryOldEndTime.value) {
-          resizedEntry.value.endTime = resizedEntryOldEndTime.value
+      if (resizedEntry) {
+        if (resizedEntryOldEndTime) {
+          resizedEntry.endTime = resizedEntryOldEndTime
         }
       }
       clearDraggedEntry()
@@ -268,9 +268,9 @@ export default function useDragAndDrop (editable, period, dialogActivityCreate, 
   }
 
   const extendBottom = (event) => {
-    resizedEntry.value = event
-    mouseStartTime.value = event.startTime
-    resizedEntryOldEndTime.value = event.endTime
+    resizedEntry = event
+    mouseStartTime = event.startTime
+    resizedEntryOldEndTime = event.endTime
   }
 
   return {
