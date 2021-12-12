eeada4ab3acf5024089a5cbfc96b3a9c25b004e8
Urban Suppiger <urban@suppiger.net>
2021-12-12 08:35:59 +0100

    remove comment

--- frontend/src/components/form/api/ApiSortable.vue
+++ frontend/src/components/form/api/ApiSortable.vue
@@ -70,7 +70,6 @@ export default {
     entities: {
       handler: function (entities) {
         const hrefList = entities.map(entry => entry._meta.self)
-        // const hrefList = entities.map(entry => ({ href: entry._meta.self, position: entry.position, id: entry.id }))
 
         // update local sorting with external sorting if not dirty
         // or if number of items don't match (new incoming items or deleted items)
