101b940972c047a1af26caef456ceb3f3246a433
Carlo Beltrame <carlo@beltra.me>
2021-12-16 18:18:41 +0100

    Repeatedly check the database connection before running migrations

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -36,30 +36,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ] || [ "$1
 		composer install --prefer-dist --no-progress --no-interaction
 
     if grep -q DATABASE_URL= .env; then
-      echo "Waiting for database to be ready..."
-      ATTEMPTS_LEFT_TO_REACH_DATABASE=60
-      until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
-        if [ $? -eq 255 ]; then
-          # If the Doctrine command exits with 255, an unrecoverable error occurred
-          ATTEMPTS_LEFT_TO_REACH_DATABASE=0
-          break
-        fi
-        sleep 1
-        ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
-        echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
-      done
-
-      if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
-        echo "The database is not up or not reachable:"
-        echo "$DATABASE_ERROR"
-        exit 1
-      else
-        echo "The database is now ready and reachable"
-      fi
-
-      if ls -A migrations/*.php >/dev/null 2>&1; then
-        php bin/console doctrine:migrations:migrate --no-interaction
-      fi
+      migrate-database || exit 1
 
 			if ls -A fixtures/*.yml >/dev/null 2>&1; then
 				php bin/console hautelook:fixtures:load --quiet
