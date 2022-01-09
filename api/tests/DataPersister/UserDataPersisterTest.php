67e9d974f9d676a59895ee533f17babe561d5882
BacLuc <lucius.bachmann@gmx.ch>
2022-01-09 15:37:03 +0100

    api: simplify UserDataPersisterTest

--- api/tests/DataPersister/UserDataPersisterTest.php
+++ api/tests/DataPersister/UserDataPersisterTest.php
@@ -2,7 +2,6 @@
 
 namespace App\Tests\DataPersister;
 
-use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
 use App\DataPersister\UserDataPersister;
 use App\DataPersister\Util\DataPersisterObservable;
 use App\Entity\User;
@@ -17,7 +16,6 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
  */
 class UserDataPersisterTest extends TestCase {
     private UserDataPersister $dataPersister;
-    private MockObject|ContextAwareDataPersisterInterface $decoratedMock;
     private MockObject|UserPasswordHasherInterface $userPasswordHasher;
     private MockObject|MailService $mailService;
     private User $user;
@@ -26,58 +24,25 @@ class UserDataPersisterTest extends TestCase {
      * @throws \ReflectionException
      */
     protected function setUp(): void {
-        $this->decoratedMock = $this->createMock(ContextAwareDataPersisterInterface::class);
-        $this->userPasswordHasher = $this->createMock(UserPasswordHasher::class);
-        $this->mailService = $this->createMock(MailService::class);
         $this->user = new User();
 
-        $requestStack = RequestStackMockFactory::create(fn (string $className) => $this->createMock($className));
-        $dataPersisterObservable = new DataPersisterObservable($this->decoratedMock, $requestStack);
-
-        $this->dataPersister = new UserDataPersister($dataPersisterObservable, $this->userPasswordHasher, $this->mailService);
+        $this->userPasswordHasher = $this->createMock(UserPasswordHasher::class);
+        $this->mailService = $this->createMock(MailService::class);
+        $dataPersisterObservable = $this->createMock(DataPersisterObservable::class);
+        $this->dataPersister = new UserDataPersister(
+            $dataPersisterObservable,
+            $this->userPasswordHasher,
+            $this->mailService
+        );
     }
 
-    public function testDelegatesSupportCheckToDecorated() {
-        $this->decoratedMock
-            ->expects($this->exactly(2))
-            ->method('supports')
-            ->willReturnOnConsecutiveCalls(true, false)
-        ;
-
-        $this->assertTrue($this->dataPersister->supports($this->user, []));
-        $this->assertFalse($this->dataPersister->supports($this->user, []));
-    }
-
-    public function testDoesNotSupportNonUser() {
-        $this->decoratedMock
-            ->method('supports')
-            ->willReturn(true)
-        ;
-
-        $this->assertFalse($this->dataPersister->supports([], []));
-    }
-
-    public function testDelegatesPersistToDecorated() {
-        // given
-        $this->decoratedMock->expects($this->once())
-            ->method('persist')
-            ->willReturnArgument(0)
-        ;
-
-        // when
-        $this->dataPersister->persist($this->user, []);
-
-        // then
-    }
-
-    public function testDoesNothingNormally() {
+    public function testDoesNotHashWhenNoPasswordIsSet() {
         // given
         $this->userPasswordHasher->expects($this->never())->method('hashPassword');
-        $this->decoratedMock->expects($this->once())->method('persist')->willReturnArgument(0);
 
         // when
         /** @var User $result */
-        $data = $this->dataPersister->persist($this->user, ['collection_operation_name' => 'post']);
+        $data = $this->dataPersister->beforeCreate($this->user);
 
         // then
         $this->assertNull($data->password);
@@ -88,11 +53,10 @@ class UserDataPersisterTest extends TestCase {
         // given
         $this->user->plainPassword = 'test plain password';
         $this->userPasswordHasher->expects($this->once())->method('hashPassword')->willReturn('test hash');
-        $this->decoratedMock->expects($this->once())->method('persist')->willReturnArgument(0);
 
         // when
         /** @var User $result */
-        $data = $this->dataPersister->persist($this->user, ['collection_operation_name' => 'post']);
+        $data = $this->dataPersister->beforeCreate($this->user);
 
         // then
         $this->assertEquals('test hash', $data->password);
@@ -102,11 +66,11 @@ class UserDataPersisterTest extends TestCase {
     public function testCreateAndSendActivationKey() {
         // given
         $this->mailService->expects($this->once())->method('sendUserActivationMail');
-        $this->decoratedMock->expects($this->once())->method('persist')->willReturnArgument(0);
 
         // when
         /** @var User $data */
-        $data = $this->dataPersister->persist($this->user, ['collection_operation_name' => 'post']);
+        $data = $this->dataPersister->beforeCreate($this->user);
+        $this->dataPersister->afterCreate($this->user);
 
         // then
         $this->assertNotNull($data->activationKeyHash);
