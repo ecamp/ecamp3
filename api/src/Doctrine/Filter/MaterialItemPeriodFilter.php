c433dd62ab76886fa8ef03debb121bd60d34531c
Urban Suppiger <urban@suppiger.net>
2022-01-10 16:52:07 +0100

    unit test for MaterialItemPeriodFilter

--- api/src/Doctrine/Filter/MaterialItemPeriodFilter.php
+++ api/src/Doctrine/Filter/MaterialItemPeriodFilter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Filter;
+namespace App\Doctrine\Filter;
 
 use ApiPlatform\Core\Api\IriConverterInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractContextAwareFilter;
@@ -9,14 +9,24 @@ use App\Entity\Activity;
 use App\Entity\MaterialItem;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\PropertyInfo\Type;
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
 
 final class MaterialItemPeriodFilter extends AbstractContextAwareFilter {
     public const PERIOD_QUERY_NAME = 'period';
 
     public function __construct(
         private IriConverterInterface $iriConverter,
+        ManagerRegistry $managerRegistry,
+        ?RequestStack $requestStack = null,
+        LoggerInterface $logger = null,
+        array $properties = null,
+        NameConverterInterface $nameConverter = null
     ) {
+        parent::__construct($managerRegistry, $requestStack, $logger, $properties, $nameConverter);
     }
 
     // This function is only used to hook in documentation generators (supported by Swagger and Hydra)
@@ -33,7 +43,7 @@ final class MaterialItemPeriodFilter extends AbstractContextAwareFilter {
 
     protected function filterProperty(string $property, $value, QueryBuilder $q, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null) {
         if (MaterialItem::class !== $resourceClass) {
-            throw new \Exception("MaterialItemPeriodFilter can only be applies to entities of type MaterialItem (received: {$resourceClass}).");
+            throw new \Exception("MaterialItemPeriodFilter can only be applied to entities of type MaterialItem (received: {$resourceClass}).");
         }
 
         if (self::PERIOD_QUERY_NAME !== $property) {
