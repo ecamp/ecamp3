11c999cce36b073354f322c78f3ef985e78ab538
Pirmin Mattmann <pimattmann@gmail.com>
2022-01-16 18:58:33 +0100

    remove User->copyFromPrototype; no more required

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -8,7 +8,6 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Exception;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -267,9 +266,5 @@ class User extends BaseEntity implements UserInterface, PasswordAuthenticatedUse
         }
 
         return $this;
-    }
-
-    public function copyFromPrototype($prototype, &$entityMap = null) {
-        throw new Exception('User can not be copied from a prototype');
     }
 }
