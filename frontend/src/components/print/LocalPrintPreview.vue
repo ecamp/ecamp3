f5795152ccbc73fac256cd6d0a0e7136038f54ff
Carlo Beltrame <carlo@beltra.me>
2022-02-11 14:26:18 +0100

    Clean up

--- frontend/src/components/print/LocalPrintPreview.vue
+++ frontend/src/components/print/LocalPrintPreview.vue
@@ -38,7 +38,7 @@ export default {
         config: this.config,
         storeData: this.$store.state,
         translationData: this.$i18n.messages,
-        renderInWorker: process.env.NODE_ENV === 'production'
+        renderInWorker: true
       })
 
       if (error) {
