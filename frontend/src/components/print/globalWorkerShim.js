0de878509db23e3e1e64b779a07b8fe151d34df3
Carlo Beltrame <carlo@beltra.me>
2022-02-17 12:31:11 +0100

    Fix React warnings for missing requestAnimationFrame in Firefox workers
    Unfortunately, this requires us to bring back globalWorkerShim.js,
    because the code in vite-plugin-shim-react-pdf only runs immediately
    before React, and not before importing the raf polyfill.

---
+++ frontend/src/components/print/globalWorkerShim.js
@@ -0,0 +1,11 @@
+/**
+ * This file provides the `global` and `window` variables in web workers.
+ * It must be its own file so it can be imported at the very top of any worker scripts that need it,
+ * above all other imports.
+ */
+
+// eslint-disable-next-line no-undef
+if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
+  self.global = self
+  self.window = self
+}
--- frontend/src/components/print/renderPdf.worker.js
+++ frontend/src/components/print/renderPdf.worker.js
@@ -1,5 +1,7 @@
 import * as Comlink from 'comlink'
 import { renderPdf } from './renderPdf.js'
+import './globalWorkerShim.js'
+import 'raf/polyfill' // must be imported before renderingDependencies
 import renderingDependencies from './renderingDependencies.js'
 
 const renderPdfInWorker = async (data) => {
