87d8d928b72a8a145a5abfc5d38411f87f1d0821
simfeld <sml.imfeld@gmail.com>
2022-03-19 18:58:18 +0100

    cs-fix

--- api/src/OAuth/UrlGeneratorDecorator.php
+++ api/src/OAuth/UrlGeneratorDecorator.php
@@ -5,33 +5,30 @@ namespace App\OAuth;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RequestContext;
 
-class UrlGeneratorDecorator implements UrlGeneratorInterface
-{
+class UrlGeneratorDecorator implements UrlGeneratorInterface {
     public function __construct(
         private UrlGeneratorInterface $decorated,
         private string $env
     ) {
     }
 
-    public function setContext(RequestContext $context)
-    {
+    public function setContext(RequestContext $context) {
         return $this->decorated->setContext($context);
     }
 
-    public function getContext(): RequestContext
-    {
+    public function getContext(): RequestContext {
         return $this->decorated->getContext();
     }
 
-    public function generate(string $name, array $parameters = [], int $referenceType = self::ABSOLUTE_PATH): string
-    {
+    public function generate(string $name, array $parameters = [], int $referenceType = self::ABSOLUTE_PATH): string {
         $url = $this->decorated->generate($name, $parameters, $referenceType);
-        if ($this->env === 'prod') {
+        if ('prod' === $this->env) {
             $url = preg_replace('/^http:\/\//', 'https://', $url);
             if (is_null($url)) {
                 throw new \Exception('Unexpected redirect URI');
             }
         }
+
         return $url;
     }
 }
