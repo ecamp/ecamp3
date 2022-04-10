81a8123ec59050a0fffb64946cd288630d96da5c
Urban Suppiger <urban@suppiger.net>
2022-04-10 21:58:38 +0200

    no rebuild; load vite first

--- wait-for-container-startup.sh
+++ wait-for-container-startup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Waiting for frontend container to start up..."
+until curl --output /dev/null --silent --fail http://localhost:3000
+do
+  sleep 2
+done
+echo "Frontend container is ready."
+
+
 echo "Waiting for API container to start up and migrate DB..."
 until curl --output /dev/null --silent --fail http://localhost:3001
 do
@@ -7,10 +15,3 @@ do
 done
 echo "API container is ready."
 
-
-echo "Waiting for frontend container to start up..."
-until curl --output /dev/null --silent --fail http://localhost:3000
-do
-  sleep 2
-done
-echo "Frontend container is ready."
