41300085b4c4423ff258e62c85fdafe3ee13782e
Manuel Meister <news.manuelsworld@gmail.com>
2022-06-16 22:49:54 +0200

    Make SidebarListItem more configurable

--- frontend/src/components/layout/SidebarListItem.vue
+++ frontend/src/components/layout/SidebarListItem.vue
@@ -1,12 +1,15 @@
 <template>
-  <v-list-item link :to="to" @click="$emit('click',$event)">
-    <v-list-item-avatar>
-      <v-icon>{{ icon }}</v-icon>
-    </v-list-item-avatar>
+  <v-list-item link :to="to" @click="$emit('click', $event)">
+    <slot name="pre">
+      <v-list-item-avatar v-if="!hideAvatar">
+        <v-icon v-if="icon">{{ icon }}</v-icon>
+      </v-list-item-avatar>
+    </slot>
     <v-list-item-content>
       <v-list-item-title>{{ title }}</v-list-item-title>
+      <v-list-item-subtitle v-if="subtitle">{{ subtitle }}</v-list-item-subtitle>
     </v-list-item-content>
-    <v-list-item-action>
+    <v-list-item-action v-if="!hideChevron">
       <v-icon>mdi-chevron-right</v-icon>
     </v-list-item-action>
   </v-list-item>
@@ -17,14 +20,13 @@ export default {
   name: 'SidebarListItem',
   props: {
     title: { type: String, required: true },
-    icon: { type: String, required: true },
-    to: {
-      type: Object,
-      default: null
-    }
-  }
+    subtitle: { type: String, required: false },
+    icon: { type: String, required: false },
+    hideAvatar: { type: Boolean, default: false },
+    hideChevron: { type: Boolean, default: false },
+    to: { type: [String, Object], default: null },
+  },
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
