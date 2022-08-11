412951e70e8944eb57bd4111af2000b2bed186e9
BacLuc <lucius.bachmann@gmx.ch>
2022-08-11 14:27:47 +0200

    print: make compatible with node 18
    Some webpack issues needed a fix:
    - create directory /home/node/.cache/node-gyp/18.6.0
    - NODE_OPTIONS=--openssl-legacy-provider (not needed with webpack >=5), see https://stackoverflow.com/questions/69394632/webpack-build-failing-with-err-ossl-evp-unsupported/69394785#69394785
    - disable sourcemap, because that does not seem to be supported yet.
    (because the error happened in the sourcemap package:
    
    Error: No element indexed by 18
    ecamp3-print   |     at ArraySet.ArraySet_at [as at] (/app/node_modules/webpack-sources/node_modules/source-map/lib/array-set.js:109:9)
    ecamp3-print   |     at BasicSourceMapConsumer.<anonymous> (/app/node_modules/webpack-sources/node_modules/source-map/lib/source-map-consumer.js:155:58)
    ecamp3-print   |     at Array.map (<anonymous>)
    ecamp3-print   |     at BasicSourceMapConsumer.SourceMapConsumer_eachMapping [as eachMapping] (/app/node_modules/webpack-sources/node_modules/source-map/lib/source-map-consumer.js:146:14)
    ecamp3-print   |     at Function.SourceNode_fromStringWithSourceMap [as fromStringWithSourceMap] (/app/node_modules/webpack-sources/node_modules/source-map/lib/source-node.js:86:24)
    ecamp3-print   |     at SourceMapSource.node (/app/node_modules/webpack-sources/lib/SourceMapSource.js:32:25)
    ecamp3-print   |     at ReplaceSource.node (/app/node_modules/webpack-sources/lib/ReplaceSource.js:82:27)
    ecamp3-print   |     at CachedSource.node (/app/node_modules/webpack-sources/lib/CachedSource.js:18:24)
    ecamp3-print   |     at /app/node_modules/webpack-sources/lib/ConcatSource.js:59:50
    ecamp3-print   |     at Array.map (<anonymous>)
    ecamp3-print   |     at ConcatSource.node (/app/node_modules/webpack-sources/lib/ConcatSource.js:58:63)
    ecamp3-print   |     at proto.sourceAndMap (/app/node_modules/webpack-sources/lib/SourceAndMapMixin.js:29:18)
    ecamp3-print   |     at CachedSource.sourceAndMap (/app/node_modules/webpack-sources/lib/CachedSource.js:58:31)
    ecamp3-print   |     at getTaskForFile (/app/node_modules/webpack/lib/SourceMapDevToolPlugin.js:65:30)
    ecamp3-print   |     at /app/node_modules/webpack/lib/SourceMapDevToolPlugin.js:215:20
    ecamp3-print   |     at Array.forEach (<anonymous>)

--- print/docker-setup.sh
+++ print/docker-setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-npm ci
+mkdir -p /home/node/.cache/node-gyp/18.6.0
 
-npm run dev
+usermod -u $USER_ID node
+groupmod -g $USER_ID node
+chown -R node:node /home/node/.cache
+
+exec su node -c "npm ci && npm run dev"
