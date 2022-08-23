03a05b1db61a3828f819e59fb626fa3b1dfca621
Urban Suppiger <urban@suppiger.net>
2022-08-23 06:59:45 +0200

    fix frontend tests

--- frontend/__mocks__/lodash.js
+++ frontend/__mocks__/lodash.js
@@ -1,11 +1,19 @@
-export const debounce = jest.fn().mockImplementation(
-  function (callback, delay) {
-    let timer
-    return function (...args) {
-      clearTimeout(timer)
-      var args = [].slice.call(arguments)
-      timer = setTimeout(() => {
-        callback.apply(this, args)
-      }, 100)
-    }
-  })
+export const debounce = jest.fn().mockImplementation(function (callback, delay) {
+  let timer
+  return function (...args) {
+    clearTimeout(timer)
+    var args = [].slice.call(arguments)
+    timer = setTimeout(() => {
+      callback.apply(this, args)
+    }, 100)
+  }
+})
+
+export const get = jest.fn().mockImplementation(function (object, path) {
+  return object[path]
+})
+
+export const set = jest.fn().mockImplementation(function (object, path, value) {
+  object[path] = value
+  return object
+})
