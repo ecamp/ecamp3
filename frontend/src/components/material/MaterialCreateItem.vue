5199c9384cc3f4c302d67c2c99ba44b7b3ad2e47
Manuel Meister <news.manuelsworld@gmail.com>
2022-08-25 18:00:09 +0200

    Fix i18n key for materialCreateItem

--- frontend/src/components/material/MaterialCreateItem.vue
+++ frontend/src/components/material/MaterialCreateItem.vue
@@ -57,7 +57,7 @@
         </p>
         <v-btn :to="campRoute(camp, 'admin')">
           <v-icon :left="$vuetify.breakpoint.mdAndUp">mdi-cogs</v-icon>
-          {{ $tc('components.camp.campSettings.title') }}
+          {{ $tc('components.camp.materialCreateItem.campSettingsButton') }}
         </v-btn>
       </div>
     </td>
