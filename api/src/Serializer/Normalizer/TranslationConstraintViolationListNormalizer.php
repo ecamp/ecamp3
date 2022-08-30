62cc739dd158a3f85b768098ffc56d08c97b8ace
BacLuc <lucius.bachmann@gmx.ch>
2022-08-30 15:30:52 +0200

    api: add translation key and parameters to constraint violation errors
    Issue: #2902

---
+++ api/src/Serializer/Normalizer/TranslationConstraintViolationListNormalizer.php
@@ -0,0 +1,55 @@
+<?php
+
+/** @noinspection PhpInternalEntityUsedInspection */
+
+namespace App\Serializer\Normalizer;
+
+use ApiPlatform\Core\Serializer\AbstractConstraintViolationListNormalizer;
+use App\Serializer\Normalizer\Error\TranslationInfoOfConstraintViolation;
+use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+class TranslationConstraintViolationListNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface {
+    public function __construct(
+        private readonly AbstractConstraintViolationListNormalizer $decorated,
+        private readonly TranslationInfoOfConstraintViolation $translationInfoOfConstraintViolation
+    ) {
+    }
+
+    public function supportsNormalization(mixed $data, string $format = null, array $context = []): bool {
+        return $this->decorated->supportsNormalization($data, $format);
+    }
+
+    public function normalize(mixed $object, string $format = null, array $context = []): float|int|bool|\ArrayObject|array|string|null {
+        $result = $this->decorated->normalize($object, $format, $context);
+
+        /** @var ConstraintViolationList $object */
+        foreach ($object as $violation) {
+            foreach ($result['violations'] as &$resultItem) {
+                $code = $resultItem['code'];
+                $propertyPath = $resultItem['propertyPath'];
+                $message = $resultItem['message'];
+
+                if ($violation->getPropertyPath() === $propertyPath
+                    && $violation->getCode() === $code
+                    && $violation->getMessage() == $message) {
+                    $translationInfo = [];
+                    if ($violation instanceof ConstraintViolation) {
+                        $translationInfo = $this->translationInfoOfConstraintViolation->extract($violation);
+                    }
+                    $resultItem = ['i18n' => [...(array) $translationInfo], ...$resultItem];
+
+                    break;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public function hasCacheableSupportsMethod(): bool {
+        return $this->decorated->hasCacheableSupportsMethod();
+    }
+}
---
+++ api/tests/Integration/Serializer/Normalizer/TranslationConstraintViolationListNormalizerIntegrationTest.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Tests\Integration\Serializer\Normalizer;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestAssertionsTrait;
+use ApiPlatform\Core\Problem\Serializer\ConstraintViolationListNormalizer;
+use App\Entity\CampCollaboration;
+use App\Serializer\Normalizer\TranslationConstraintViolationListNormalizer;
+use App\Validator\AllowTransition\AssertAllowTransitions;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
+
+/**
+ * @internal
+ */
+class TranslationConstraintViolationListNormalizerIntegrationTest extends KernelTestCase {
+    use ApiTestAssertionsTrait;
+
+    private TranslationConstraintViolationListNormalizer $translationConstraintViolationListNormalizer;
+
+    /**
+     * @throws \Exception
+     */
+    protected function setUp(): void {
+        self::bootKernel();
+        parent::setUp();
+
+        /** @var TranslationConstraintViolationListNormalizer $obj */
+        $obj = self::getContainer()->get('api_platform.problem.normalizer.constraint_violation_list');
+        $this->translationConstraintViolationListNormalizer = $obj;
+    }
+
+    /**
+     * @throws ExceptionInterface
+     * @throws \Exception
+     */
+    public function testAddsTranslationKeyAndParameters() {
+        $constraintViolationList = new ConstraintViolationList(self::getConstraintViolations());
+
+        $result = $this->translationConstraintViolationListNormalizer->normalize(
+            $constraintViolationList,
+            ConstraintViolationListNormalizer::FORMAT,
+            []
+        );
+
+        self::assertArraySubset(['violations' => [
+            [
+                'i18n' => [
+                    'key' => 'app.validator.allowtransition.assertallowtransitions',
+                    'parameters' => [
+                        'to' => 'inactive',
+                        'value' => 'established',
+                    ],
+                ],
+            ],
+            [
+                'i18n' => [
+                    'key' => 'symfony.component.validator.constraints.notblank',
+                    'parameters' => [
+                        'value' => '""',
+                    ],
+                ],
+            ],
+            [
+                'i18n' => [
+                    'key' => 'symfony.component.validator.constraints.notnull',
+                    'parameters' => [],
+                ],
+            ],
+        ]], $result);
+    }
+
+    public static function getConstraintViolations(): array {
+        return [
+            new ConstraintViolation(
+                message: 'value must be one of inactive, was established',
+                messageTemplate: 'value must be one of {{ to }}, was {{ value }}',
+                parameters: ['{{ to }}' => 'inactive', '{{ value }}' => 'established'],
+                root: new CampCollaboration(),
+                propertyPath: 'status',
+                invalidValue: 'established',
+                plural: null,
+                code: null,
+                constraint: new AssertAllowTransitions(transitions: [])
+            ),
+            new ConstraintViolation(
+                message: 'This value should not be blank.',
+                messageTemplate: 'This value should not be blank.',
+                parameters: ['{{ value }}' => '""'],
+                root: new CampCollaboration(),
+                propertyPath: 'name',
+                invalidValue: '',
+                plural: null,
+                code: 'c1051bb4-d103-4f74-8988-acbcafc7fdc3',
+                constraint: new NotBlank()
+            ),
+            new ConstraintViolation(
+                message: 'This value should not be null.',
+                messageTemplate: 'This value should not be null.',
+                parameters: [],
+                root: new CampCollaboration(),
+                propertyPath: 'name',
+                invalidValue: '',
+                plural: null,
+                code: 'c1051bb4-d103-4f74-8988-acbcafc7fdc3',
+                constraint: new NotNull()
+            ),
+        ];
+    }
+}
