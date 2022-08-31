0dca321e8b76ccb4c371930f2242bfee32441b0b
Carlo Beltrame <carlo@beltra.me>
2022-08-31 13:07:41 +0200

    Fix documentation comment

--- common/helpers/userInitials.js
+++ common/helpers/userInitials.js
@@ -2,7 +2,7 @@ import userDisplayName from './userDisplayName.js'
 import initials from './initials.js'
 
 /**
- * Returns two characters to display for a camp collaboration based on its user
+ * Returns two characters to display for a user
  */
 export default function (user) {
   return initials(userDisplayName(user))
