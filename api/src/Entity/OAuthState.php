151e748c22b82802d709c7ca911794c3b73d1b57
Carlo Beltrame <carlo@beltra.me>
2022-09-05 22:48:35 +0200

    Specify length of db column

--- api/src/Entity/OAuthState.php
+++ api/src/Entity/OAuthState.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: OAuthStateRepository::class)]
 #[ORM\Index(columns: ['expireTime', 'state'], name: 'IDX_466EF70CD49BE2B9A393D2FB')]
 class OAuthState extends BaseEntity {
-    #[ORM\Column(type: 'string')]
+    #[ORM\Column(type: 'string', length: 32)]
     public string $state;
 
     #[ORM\Column(type: 'datetime')]
