48e4700c2d575934a92a06f22078f9d18916712c
Carlo Beltrame <carlo@beltra.me>
2022-10-07 21:20:40 +0200

    Fix vee-validate built-in rule by introducing a validation-observer

--- frontend/src/views/auth/Register.vue
+++ frontend/src/views/auth/Register.vue
@@ -2,98 +2,99 @@
   <auth-container>
     <h1 class="display-1 text-center">{{ $tc('views.auth.register.title') }}</h1>
     <v-form @submit.prevent="register">
-      <e-text-field
-        v-model="firstname"
-        :name="$tc('entity.user.fields.firstname')"
-        vee-rules="required"
-        append-icon="mdi-account-outline"
-        dense
-        type="text"
-      />
+      <validation-observer>
+        <e-text-field
+          v-model="firstname"
+          :name="$tc('entity.user.fields.firstname')"
+          vee-rules="required"
+          append-icon="mdi-account-outline"
+          dense
+          type="text"
+        />
 
-      <e-text-field
-        v-model="surname"
-        :name="$tc('entity.user.fields.surname')"
-        vee-rules="required"
-        append-icon="mdi-account-outline"
-        dense
-        type="text"
-      />
+        <e-text-field
+          v-model="surname"
+          :name="$tc('entity.user.fields.surname')"
+          vee-rules="required"
+          append-icon="mdi-account-outline"
+          dense
+          type="text"
+        />
 
-      <e-text-field
-        v-model="email"
-        :name="$tc('entity.user.fields.email')"
-        vee-rules="email|required"
-        append-icon="mdi-at"
-        dense
-        type="text"
-      />
+        <e-text-field
+          v-model="email"
+          :name="$tc('entity.user.fields.email')"
+          vee-rules="email|required"
+          append-icon="mdi-at"
+          dense
+          type="text"
+        />
 
-      <e-text-field
-        v-model="pw1"
-        :name="$tc('entity.user.fields.password')"
-        vee-id="password"
-        vee-rules="required|min:12"
-        validate-on-blur
-        append-icon="mdi-lock-outline"
-        dense
-        type="password"
-      />
+        <e-text-field
+          v-model="pw1"
+          :name="$tc('entity.user.fields.password')"
+          vee-id="password"
+          vee-rules="required|min:12|max:128"
+          validate-on-blur
+          append-icon="mdi-lock-outline"
+          dense
+          type="password"
+        />
 
-      <e-text-field
-        v-model="pw2"
-        :name="$tc('views.auth.register.passwordConfirmation')"
-        :rules="pw2Rules"
-        vee-rules="required"
-        validate-on-blur
-        dense
-        append-icon="mdi-lock-outline"
-        type="password"
-      />
+        <e-text-field
+          v-model="pw2"
+          :name="$tc('views.auth.register.passwordConfirmation')"
+          vee-rules="required|confirmed:password"
+          validate-on-blur
+          dense
+          append-icon="mdi-lock-outline"
+          type="password"
+        />
 
-      <e-select
-        v-model="language"
-        :name="$tc('entity.user.fields.language')"
-        dense
-        :items="availableLocales"
-      />
+        <e-select
+          v-model="language"
+          :name="$tc('entity.user.fields.language')"
+          dense
+          :items="availableLocales"
+        />
 
-      <e-checkbox v-model="tos" vee-rules="required" class="align-center">
-        <template #label>
-          <span style="hyphens: auto" :class="{ 'body-2': $vuetify.breakpoint.xsOnly }">
-            {{ $tc('views.auth.register.acceptTermsOfUse') }}
-          </span>
-        </template>
-        <template #append>
-          <v-btn
-            text
-            dense
-            min-width="0"
-            :title="$tc('global.button.open')"
-            target="_blank"
-            class="px-1"
-            to="#"
-            tabindex="-1"
-          >
-            <v-icon small>mdi-open-in-new</v-icon>
-          </v-btn>
-        </template>
-      </e-checkbox>
+        <e-checkbox v-model="tos" vee-rules="required" class="align-center">
+          <template #label>
+            <span style="hyphens: auto" :class="{ 'body-2': $vuetify.breakpoint.xsOnly }">
+              {{ $tc('views.auth.register.acceptTermsOfUse') }}
+            </span>
+          </template>
+          <template #append>
+            <v-btn
+              text
+              dense
+              min-width="0"
+              :title="$tc('global.button.open')"
+              target="_blank"
+              class="px-1"
+              to="#"
+              tabindex="-1"
+            >
+              <v-icon small>mdi-open-in-new</v-icon>
+            </v-btn>
+          </template>
+        </e-checkbox>
 
-      <p class="mt-0 mb-4 text--secondary text-left">
-        <small>
-          <span style="color: #d32f2f">*</span>
-          {{ $tc('views.auth.register.requiredField') }}
-        </small>
-      </p>
+        <p class="mt-0 mb-4 text--secondary text-left">
+          <small>
+            <span style="color: #d32f2f">*</span>
+            {{ $tc('views.auth.register.requiredField') }}
+          </small>
+        </p>
 
-      <v-btn type="submit" color="primary" :disabled="!formComplete" block x-large>
-        <v-progress-circular v-if="registering" indeterminate size="24" />
-        <v-spacer />
-        <span>{{ $tc('views.auth.register.register') }}</span>
-        <v-spacer />
-        <icon-spacer />
-      </v-btn>
+        <v-btn type="submit" color="primary" :disabled="!formComplete" block x-large>
+          <v-progress-circular v-if="registering" indeterminate size="24" />
+          <v-spacer />
+          <span>{{ $tc('views.auth.register.register') }}</span>
+          <v-spacer />
+          <icon-spacer />
+        </v-btn>
+      </validation-observer>
     </v-form>
 
     <p class="mt-8 mb-0 text--secondary text-center">
@@ -109,11 +110,13 @@
 import { load } from 'recaptcha-v3'
 import AuthContainer from '@/components/layout/AuthContainer.vue'
 import VueI18n from '@/plugins/i18n'
+import { ValidationObserver } from 'vee-validate'
 
 export default {
   name: 'Register',
   components: {
     AuthContainer,
+    ValidationObserver,
   },
   data() {
     return {
