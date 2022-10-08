85469fec7ca1910b20b067ad1669fdcbdb1f48fa
Pirmin Mattmann <pimattmann@gmail.com>
2022-10-08 15:15:37 +0200

    create material xlsx

--- frontend/src/views/camp/Material.vue
+++ frontend/src/views/camp/Material.vue
@@ -18,9 +18,14 @@ Admin screen of a camp: Displays MaterialLists and MaterialItems
       <e-switch
         v-if="$vuetify.breakpoint.smAndUp"
         v-model="groupByList"
-        class="ml-15"
+        class="ml-10"
         :label="$tc('views.camp.material.groupByList')"
       />
+
+      <v-btn small color="primary" class="ml-5" @click="downloadXlsx">
+        <v-icon left>mdi-microsoft-excel</v-icon>
+        {{ $tc('views.camp.material.downloadXlsx') }}
+      </v-btn>
     </template>
     <v-expansion-panels v-model="openPeriods" multiple flat accordion>
       <period-material-lists
@@ -39,6 +44,7 @@ Admin screen of a camp: Displays MaterialLists and MaterialItems
 import ContentCard from '@/components/layout/ContentCard.vue'
 import PeriodMaterialLists from '@/components/material/PeriodMaterialLists.vue'
 import { campRoleMixin } from '@/mixins/campRoleMixin'
+import * as XLSX from 'xlsx'
 
 export default {
   name: 'Material',
@@ -78,6 +84,67 @@ export default {
       })
 
     this.camp().activities().$reload()
+  },
+  methods: {
+    async downloadXlsx() {
+      await this.camp().activities().$loadItems()
+
+      var workbook = XLSX.utils.book_new()
+
+      var sheets = await Promise.all(
+        this.camp()
+          .periods()
+          .items.map(async (p) => {
+            var rows = await Promise.all(
+              p.materialItems().items.map(async (mi) => {
+                var activity = await this.getActivity(mi)
+                return [
+                  mi.quantity,
+                  mi.unit,
+                  mi.article,
+                  mi.materialList().name,
+                  activity?.title,
+                ]
+              })
+            )
+
+            var data = [
+              [
+                this.$tc('views.camp.material.xlsx.quantity'),
+                this.$tc('views.camp.material.xlsx.unit'),
+                this.$tc('views.camp.material.xlsx.article'),
+                this.$tc('views.camp.material.xlsx.matlist'),
+                this.$tc('views.camp.material.xlsx.activity'),
+              ],
+            ]
+            rows.forEach((r) => data.push(r))
+            console.log(data)
+
+            return { description: p.description, data }
+          })
+      )
+
+      sheets.forEach((s) => {
+        var worksheet = XLSX.utils.aoa_to_sheet(s.data)
+        workbook.SheetNames.push(s.description)
+        workbook.Sheets[s.description] = worksheet
+      })
+
+      XLSX.writeFile(workbook, 'Materialliste.xlsx')
+    },
+
+    async getActivity(mi) {
+      if (mi.materialNode) {
+        const root = await mi.materialNode().$href('root')
+        return this.camp()
+          .activities()
+          .items.find(async (activity) => {
+            var rootNode = await activity.rootContentNode()
+            return rootNode._meta.self === root
+          })
+      }
+      return null
+    },
   },
 }
 </script>
