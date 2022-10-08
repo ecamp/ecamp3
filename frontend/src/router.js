19f936b0be6119c359b9736533775abb75d82396
Manuel Meister <news.manuelsworld@gmail.com>
2022-10-09 00:27:21 +0200

    Create new dashboard mockup

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -248,6 +248,12 @@ export default new Router({
           component: () => import('./views/camp/Dashboard.vue'),
         },
         {
+          path: 'dashboard-new',
+          name: 'camp/dashboard/new',
+          component: () =>
+            import(/* webpackChungName: "camp" */ './views/camp/DashboardNew.vue'),
+        },
+        {
           path: '',
           name: 'camp/home',
           redirect: { name: 'camp/dashboard' },
