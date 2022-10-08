51755fbc6b97e14ad149fe797869389e48dec719
Pirmin Mattmann <pimattmann@gmail.com>
2022-10-08 03:20:35 +0200

    Move ScheduleEntries and DayResponsibles

--- api/src/DataPersister/PeriodDataPersister.php
+++ api/src/DataPersister/PeriodDataPersister.php
@@ -5,6 +5,7 @@ namespace App\DataPersister;
 use App\DataPersister\Util\AbstractDataPersister;
 use App\DataPersister\Util\DataPersisterObservable;
 use App\Entity\Day;
+use App\Entity\DayResponsible;
 use App\Entity\Period;
 use App\Util\DateTimeUtil;
 use Doctrine\ORM\EntityManagerInterface;
@@ -43,33 +44,67 @@ class PeriodDataPersister extends AbstractDataPersister {
     public static function updateDaysAndScheduleEntries(Period $period, array $orig = null) {
         $length = $period->getPeriodLength();
         $days = $period->getDays();
+        $daysCount = count($days);
 
-        // Add Days
-        $i = count($days);
-        while ($i < $length) {
-            $day = new Day();
-            $day->dayOffset = $i++;
-            $period->addDay($day);
-        }
-
-        // Move Schedule-Entries
-        if (!$period->moveScheduleEntries) {
-            $deltaMinutes = 0;
-            if (null != $orig) {
+        if (0 == $daysCount) {
+            // Create all Days:
+            for ($i = 0; $i < $length; ++$i) {
+                $day = new Day();
+                $day->dayOffset = $i;
+                $period->addDay($day);
+            }
+        } else {
+            // Move Schedule-Entries
+            if ($period->moveScheduleEntries) {
+                // Add Days at end
+                for ($i = $daysCount; $i < $length; ++$i) {
+                    $day = new Day();
+                    $day->dayOffset = $i++;
+                    $period->addDay($day);
+                }
+                // Remove Days at end
+                for ($i = $daysCount; $i > $length; --$i) {
+                    $day = $days[$i - 1];
+                    $period->removeDay($day);
+                }
+            } else {
                 $deltaMinutes = DateTimeUtil::differenceInMinutes($period->start, $orig['start']);
-            }
+                $deltaDaysAtStart = floor($deltaMinutes / 60 / 24);
 
-            foreach ($period->scheduleEntries as $scheduleEntry) {
-                $scheduleEntry->startOffset += $deltaMinutes;
-                $scheduleEntry->endOffset += $deltaMinutes;
-            }
-        }
+                // Tage erstellen
+                for ($i = $daysCount; $i < $length; ++$i) {
+                    $day = new Day();
+                    $day->dayOffset = $i;
+                    $period->addDay($day);
+                }
 
-        // Remove Days
-        $i = count($days);
-        while ($i > $length) {
-            $day = $days[--$i];
-            $period->removeDay($day);
+                // Move ScheduleEntries
+                foreach ($period->scheduleEntries as $scheduleEntry) {
+                    $scheduleEntry->startOffset += $deltaMinutes;
+                    $scheduleEntry->endOffset += $deltaMinutes;
+                }
+
+                // Move DayResponsibles
+                foreach ($period->days as $day) {
+                    /** @var Day $day */
+                    $newDay = $period->days->filter(fn ($d) => $d->dayOffset == $day->dayOffset + $deltaDaysAtStart)->first();
+
+                    /** @var DayResponsible $dayResp */
+                    foreach ($day->dayResponsibles as $dayResp) {
+                        if (null != $newDay) {
+                            $dayResp->day = $newDay;
+                        } else {
+                            $day->removeDayResponsible($dayResp);
+                        }
+                    }
+                }
+
+                // Remove Days at end
+                for ($i = $daysCount; $i > $length; --$i) {
+                    $day = $days[$i - 1];
+                    $period->removeDay($day);
+                }
+            }
         }
     }
 }
