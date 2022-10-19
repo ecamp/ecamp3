16c878c03802b642150ee206682eded14f7b11a3
Carlo Beltrame <carlo@beltra.me>
2022-10-19 17:16:35 +0200

    Replace vite-plugin-vue2 with @vitejs/plugin-vue2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import vue from '@vitejs/plugin-vue2'
 import { createSvgPlugin } from 'vite-plugin-vue2-svg'
 import { comlink } from 'vite-plugin-comlink'
 import * as path from 'path'
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     comlink(), // must be first
-    createVuePlugin(),
+    vue(),
     Components({
       resolvers: [
         // Vuetify
@@ -26,28 +26,37 @@ export default defineConfig(({ mode }) => ({
   },
   optimizeDeps: {
     include: [
-      'core-js/modules/es.symbol.js',
-      'core-js/modules/es.symbol.description.js',
-      'core-js/modules/es.function.name.js',
+      '@sentry/browser',
+      '@sentry/vue',
+      '@zxcvbn-ts/core',
+      '@zxcvbn-ts/language-common',
+      '@zxcvbn-ts/language-de',
+      '@zxcvbn-ts/language-en',
+      '@zxcvbn-ts/language-fr',
+      '@zxcvbn-ts/language-it',
       'core-js/modules/es.array.concat.js',
-      'core-js/modules/es.array.slice.js',
-      'core-js/modules/es.array.splice.js',
       'core-js/modules/es.array.find.js',
       'core-js/modules/es.array.push.js',
+      'core-js/modules/es.array.slice.js',
+      'core-js/modules/es.array.splice.js',
+      'core-js/modules/es.function.name.js',
       'core-js/modules/es.object.to-string.js',
       'core-js/modules/es.regexp.exec.js',
       'core-js/modules/es.regexp.test.js',
-      '@sentry/browser',
-      '@zxcvbn-ts/core',
-      '@zxcvbn-ts/language-common',
-      '@zxcvbn-ts/language-en',
-      '@zxcvbn-ts/language-de',
-      '@zxcvbn-ts/language-fr',
-      '@zxcvbn-ts/language-it',
+      'core-js/modules/es.symbol.description.js',
+      'core-js/modules/es.symbol.js',
+      'dayjs',
+      'dayjs/locale/de',
+      'dayjs/locale/de-ch',
+      'dayjs/locale/fr',
+      'dayjs/locale/it',
+      'lodash/keyBy.js',
       'raf/polyfill',
+      'vee-validate',
+      'vue',
+      'vue-toastification',
       'vuetify/es5/components/VCalendar/modes/column.js',
       'vuetify/es5/components/VCalendar/util/events.js',
-      'lodash/keyBy.js',
     ],
   },
   build: {
