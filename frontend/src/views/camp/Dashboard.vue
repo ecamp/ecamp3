2d57eda012a80d3dce945d828848579677c1d76b
Carlo Beltrame <carlo@beltra.me>
2022-11-08 14:27:50 +0100

    Reimplement filtering and clean up dashboard

--- frontend/src/views/camp/Dashboard.vue
+++ frontend/src/views/camp/Dashboard.vue
@@ -36,7 +36,7 @@
           </template>
         </SelectFilter>
         <SelectFilter
-          v-if="results.length > 1"
+          v-if="groupedScheduleEntries.length > 1"
           v-model="filter.period"
           :items="periods"
           display-field="description"
@@ -63,59 +63,59 @@
         </v-chip>
       </div>
       <table
-        v-for="period in events"
-        :key="period.id"
+        v-for="(periodDays, uri) in groupedScheduleEntries"
+        :key="uri"
         class="mx-4 mt-6 mb-3"
         style="border-collapse: collapse"
       >
         <caption class="font-weight-bold text-left">
           {{
-            period.id
+            periods[uri].description
           }}
         </caption>
-        <thead :key="period.id + '_head'">
+        <thead :key="uri + '_head'">
           <tr class="d-sr-only">
-            <th :id="period.id + 'th-number'" scope="col">
+            <th :id="uri + 'th-number'" scope="col">
               {{ $tc('views.camp.dashboard.columns.number') }}
             </th>
-            <th :id="period.id + 'th-category'" scope="col">
+            <th :id="uri + 'th-category'" scope="col">
               {{ $tc('views.camp.dashboard.columns.category') }}
             </th>
-            <th :id="period.id + 'th-time'" scope="col">
+            <th :id="uri + 'th-time'" scope="col">
               {{ $tc('views.camp.dashboard.columns.time') }}
             </th>
-            <th :id="period.id + 'th-title'" scope="col">
+            <th :id="uri + 'th-title'" scope="col">
               {{ $tc('views.camp.dashboard.columns.title') }}
             </th>
-            <th :id="period.id + 'th-responsible'" scope="col">
+            <th :id="uri + 'th-responsible'" scope="col">
               {{ $tc('views.camp.dashboard.columns.responsible') }}
             </th>
           </tr>
         </thead>
         <tbody
-          v-for="day in period.days"
-          :key="period.id + day.id"
-          :aria-labelledby="period.id + day.id + 'th'"
+          v-for="(dayScheduleEntries, dayUri) in periodDays"
+          :key="dayUri"
+          :aria-labelledby="dayUri + 'th'"
         >
           <tr>
             <th
-              :id="period.id + day.id + 'th'"
+              :id="dayUri + 'th'"
               colspan="5"
               scope="colgroup"
-              align="left"
               style="
                 padding-top: 0.75rem;
                 font-weight: 400;
                 color: #666;
                 font-size: 0.9rem;
+                text-align: left;
               "
             >
-              {{ day.date }}
+              {{ days[dayUri].date }}
             </th>
           </tr>
           <ActivityRow
-            v-for="scheduleEntry in day.scheduleEntries"
-            :key="day.id + scheduleEntry.id"
+            v-for="scheduleEntry in dayScheduleEntries"
+            :key="scheduleEntry._meta.self"
             :schedule-entry="scheduleEntry"
           />
         </tbody>
@@ -127,18 +127,12 @@
 <script>
 import ContentCard from '@/components/layout/ContentCard.vue'
 import UserAvatar from '../../components/user/UserAvatar.vue'
-import {
-  dateShort,
-  dateLong,
-  hourShort,
-} from '@/common/helpers/dateHelperUTCFormatted.js'
 import CategoryChip from '@/components/generic/CategoryChip.vue'
 import BooleanFilter from '@/components/dashboard/BooleanFilter.vue'
 import SelectFilter from '@/components/dashboard/SelectFilter.vue'
 import ActivityRow from '@/components/dashboard/ActivityRow.vue'
-import dayjs from 'dayjs'
 import FilterDivider from '@/components/dashboard/FilterDivider.vue'
-import { keyBy } from 'lodash'
+import { keyBy, groupBy, mapValues } from 'lodash'
 
 function filterEquals(arr1, arr2) {
   return JSON.stringify(arr1) === JSON.stringify(arr2)
@@ -162,129 +156,11 @@ export default {
     return {
       loggedInUser: null,
       loading: true,
-      openPeriods: [],
       filter: {
         period: null,
         responsible: [],
         category: [],
       },
-      results: [
-        {
-          id: 'Vorlager',
-          days: [
-            {
-              id: 1,
-              scheduleEntries: [
-                {
-                  id: 1,
-                  number: '1.1',
-                  start: '18',
-                  duration: '2h',
-                  title: 'Stafetten',
-                  location: 'Sportfeld',
-                  collaborators: ['Ikarus', 'Forte', 'Linux'],
-                  category: 'LS',
-                },
-                {
-                  id: 2,
-                  number: '1.2',
-                  start: '20',
-                  duration: '1h',
-                  title: 'Essen',
-                  location: 'Aufenthaltsraum',
-                  collaborators: ['Forte'],
-                  category: 'ES',
-                },
-                {
-                  id: 3,
-                  number: '1.3',
-                  start: '21',
-                  duration: '30m',
-                  title: 'Once upon a time there was a long title',
-                  location: 'Gschichtliruum',
-                  collaborators: ['Cosinus'],
-                  category: 'LS',
-                },
-              ],
-            },
-          ],
-        },
-        {
-          id: 'Hauptlager',
-          days: [
-            {
-              id: 2,
-              scheduleEntries: [
-                {
-                  id: 1,
-                  number: '2.1',
-                  start: '8',
-                  duration: '2h',
-                  title: 'Morgenfit',
-                  location: 'Wiese',
-                  collaborators: ['Linux', 'Smiley'],
-                  category: 'LA',
-                },
-                {
-                  id: 2,
-                  number: '2.2',
-                  start: '10',
-                  duration: '1h',
-                  title: 'Atelier',
-                  location: 'Aufenthaltsraum',
-                  collaborators: ['Cosinus', 'Linux'],
-                  category: 'LP',
-                },
-                {
-                  id: 3,
-                  number: '2.3',
-                  start: '12',
-                  duration: '30m',
-                  title: 'Zmittag',
-                  location: 'Aufenthaltsraum',
-                  collaborators: ['Ikarus'],
-                  category: 'ES',
-                },
-              ],
-            },
-            {
-              id: 3,
-              scheduleEntries: [
-                {
-                  id: 1,
-                  number: '3.1',
-                  start: '10',
-                  duration: '7h',
-                  title: 'Geländegame',
-                  location: 'Wald',
-                  collaborators: ['Olippo', 'Forte', 'Linux'],
-                  category: 'LS',
-                },
-                {
-                  id: 2,
-                  number: '3.2',
-                  start: '17',
-                  duration: '4h',
-                  title: 'Partyznacht',
-                  location: 'Aufenthaltsraum',
-                  collaborators: ['Linux'],
-                  category: 'ES',
-                },
-                {
-                  id: 3,
-                  number: '3.3',
-                  start: '21',
-                  duration: '30m',
-                  title: 'Tabs',
-                  location: 'Feuerschale',
-                  collaborators: ['Smiley'],
-                  category: 'TA',
-                },
-              ],
-            },
-          ],
-        },
-      ],
     }
   },
   computed: {
@@ -311,54 +187,51 @@ export default {
     periods() {
       return keyBy(this.camp().periods().items, '_meta.self')
     },
-    events() {
-      return this.results
-        .filter(
-          (period) =>
-            this.filter.period.length === 0 ||
-            (Array.isArray(this.filter.period)
-              ? this.filter.period.some((filter) => period.id.includes(filter))
-              : period.id.includes(this.filter.period))
-        )
-        .map((period) => ({
-          ...period,
-          days: period.days
-            .map(({ id, scheduleEntries }) => ({
-              id,
-              date: dayjs().add(id, 'day').format('dd, D. MMM YYYY'),
-              scheduleEntries: scheduleEntries
-                .filter((scheduleEntry) => {
-                  return this.filterScheduleEntry(scheduleEntry)
-                })
-                .map((scheduleEntry) => ({
-                  ...scheduleEntry,
-                  collaborators: scheduleEntry.collaborators.map(
-                    (collaborator) => this.users[collaborator]
-                  ),
-                  category: this.categories[scheduleEntry.category],
-                })),
-            }))
-            .filter(({ scheduleEntries }) => scheduleEntries.length > 0),
-        }))
-        .filter(({ days }) => days.length > 0)
+    scheduleEntries() {
+      return Object.values(this.periods).flatMap(
+        (period) => period.scheduleEntries().items
+      )
     },
-    // returns scheduleEntries per day without the need for an additional API call for each day
-    scheduleEntriesByDay() {
-      let scheduleEntriesByDay = []
-
-      this.camp()
-        .periods()
-        .items.forEach(function (period) {
-          period.scheduleEntries().items.forEach(function (scheduleEntry) {
-            const dayUri = scheduleEntry.day()._meta.self
-            if (!(dayUri in scheduleEntriesByDay)) {
-              scheduleEntriesByDay[dayUri] = []
-            }
-            scheduleEntriesByDay[dayUri].push(scheduleEntry)
-          })
+    days() {
+      return keyBy(
+        Object.values(this.periods).flatMap((period) => period.days().items),
+        '_meta.self'
+      )
+    },
+    filteredScheduleEntries() {
+      return this.scheduleEntries.filter(
+        (scheduleEntry) =>
+          // filter by period
+          (this.filter.period === null ||
+            scheduleEntry.period()._meta.self === this.filter.period) &&
+          // filter by categories: OR filter
+          (this.filter.category === null ||
+            this.filter.category.length === 0 ||
+            this.filter.category?.includes(
+              scheduleEntry.activity().category()._meta.self
+            )) &&
+          // filter by responsibles: AND filter
+          (this.filter.responsible === null ||
+            this.filter.responsible.length === 0 ||
+            this.filter.responsible?.every((responsible) => {
+              return scheduleEntry
+                .activity()
+                .activityResponsibles()
+                .items.map((responsible) => responsible._meta.self)
+                .includes(responsible)
+            }))
+      )
+    },
+    groupedScheduleEntries() {
+      const groupedByPeriod = groupBy(
+        this.filteredScheduleEntries,
+        (scheduleEntry) => scheduleEntry.period()._meta.self
+      )
+      return mapValues(groupedByPeriod, (scheduleEntries) =>
+        groupBy(scheduleEntries, (scheduleEntry) => {
+          return scheduleEntry.day()._meta.self
         })
-
-      return scheduleEntriesByDay
+      )
     },
     showOnlyMyActivities: {
       get() {
@@ -376,40 +249,15 @@ export default {
     },
   },
   async mounted() {
-    const [loggedInUser, periods] = await Promise.all([
+    const [loggedInUser] = await Promise.all([
       this.$auth.loadUser(),
       this.camp().periods()._meta.load,
       this.camp().activities()._meta.load,
       this.camp().categories()._meta.load,
     ])
 
-    this.openPeriods = periods.items
-      .map((period, idx) => (Date.parse(period.end) >= new Date() ? idx : null))
-      .filter((idx) => idx !== null)
-
     this.loggedInUser = loggedInUser
     this.loading = false
-  },
-  methods: {
-    dayjs,
-    dateShort,
-    dateLong,
-    hourShort,
-    matchCollaborators: function (scheduleEntry) {
-      return (
-        this.filter.responsible === null ||
-        this.filter.responsible.length === 0 ||
-        this.filter.responsible.every((collaborator) =>
-          scheduleEntry.collaborators.some((col) => col === collaborator)
-        )
-      )
-    },
-    matchCategory: function (scheduleEntry) {
-      return scheduleEntry.category.includes(this.filter.category)
-    },
-    filterScheduleEntry(scheduleEntry) {
-      return this.matchCollaborators(scheduleEntry) && this.matchCategory(scheduleEntry)
-    },
   },
 }
 </script>
