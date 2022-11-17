7bb0974cdddadb669778b4c94d6650ebc1acf71f
Carlo Beltrame <carlo@beltra.me>
2022-11-17 17:20:59 +0100

    Rewrite date picker specs using testing library

--- frontend/src/components/form/base/__tests__/EDatePicker.spec.js
+++ frontend/src/components/form/base/__tests__/EDatePicker.spec.js
@@ -1,149 +1,143 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-
-import i18n from '@/plugins/i18n'
-import formBaseComponents from '@/plugins/formBaseComponents'
-import dayjs from '@/plugins/dayjs'
-
-import { mount as mountComponent } from '@vue/test-utils'
+import { screen, waitFor } from '@testing-library/vue'
+import { render, setTestLocale, snapshotOf } from '@/test/renderWithVuetify.js'
+import user from '@testing-library/user-event'
 import EDatePicker from '../EDatePicker'
-import { waitForDebounce } from '@/test/util'
-import flushPromises from 'flush-promises'
-
-Vue.use(Vuetify)
-Vue.use(formBaseComponents)
-Vue.use(dayjs)
 
 describe('An EDatePicker', () => {
-  let vuetify
+  const DATE1_ISO = '2020-03-01'
+  const DATE2_ISO = '2020-03-19'
 
-  const DATE_1 = '2020-03-01'
-  const DATE_2 = '2015-12-31'
-  const INVALID_DATE_1 = 'some date'
-  const INVALID_DATE_2 = '2026-13-01'
+  const localeData = {
+    de: {
+      date1: '01.03.2020',
+      date2: '19.03.2020',
+      dateInWrongLocale: '03/19/2020',
+      labelText: 'Dialog für Datum test öffnen',
+      date1Heading: 'März 2020',
+      closeButton: 'Schliessen',
+      validationMessage:
+        'Ungültiges Format, bitte gib das Datum im Format DD.MM.YYYY ein',
+    },
+    en: {
+      date1: '03/01/2020',
+      date2: '03/19/2020',
+      dateInWrongLocale: '19.03.2020',
+      labelText: 'Open dialog to select the test date',
+      date1Heading: 'March 2020',
+      closeButton: 'Close',
+      validationMessage: 'Invalid format, please enter the date in the format MM/DD/YYYY',
+    },
+  }
 
-  const localeData = [
-    [
-      'de',
-      {
-        date_1: '01.03.2020',
-        date_2: '31.12.2015',
-        date_3: '24.03.2020',
-      },
-    ],
-    [
-      'en',
-      {
-        date_1: '03/01/2020',
-        date_2: '12/31/2015',
-        date_3: '03/24/2020',
-      },
-    ],
-  ]
-
-  const mount = (options) => mountComponent(EDatePicker, { vuetify, i18n, ...options })
-
-  describe.each(localeData)('in locale %s', (locale, data) => {
+  describe.each(Object.entries(localeData))('in locale %s', (locale, data) => {
     beforeEach(() => {
-      i18n.locale = locale
-      Vue.dayjs.locale(locale)
-      vuetify = new Vuetify()
+      setTestLocale(locale)
     })
 
-    test('renders', async () => {
-      const wrapper = mount({
-        propsData: {
-          value: DATE_1,
-        },
+    it('renders the component', async () => {
+      // given
+
+      // when
+      render(EDatePicker, { props: { value: DATE1_ISO, name: 'test' } })
+
+      // then
+      await screen.findByDisplayValue(data.date1)
+      screen.getByLabelText(data.labelText)
+    })
+
+    it('looks like a date picker', async () => {
+      // given
+
+      // when
+      const { container } = render(EDatePicker, {
+        props: { value: DATE1_ISO, name: 'test' },
       })
-      await flushPromises()
-      expect(wrapper.find('input[type=text]').element.value).toBe(data.date_1)
+
+      // then
+      expect(snapshotOf(container)).toMatchSnapshot('pickerclosed')
+
+      // when
+      await user.click(screen.getByLabelText(data.labelText))
+
+      // then
+      await screen.findByText(data.closeButton)
+      await screen.findByText(data.date1Heading)
+      expect(snapshotOf(container)).toMatchSnapshot('pickeropen')
     })
 
-    test('looks like a date picker', async () => {
-      const wrapper = mountComponent(
-        {
-          data: () => ({ date: DATE_1 }),
-          template: '<div data-app><e-date-picker v-model="date"></e-date-picker></div>',
-          components: { 'e-date-picker': EDatePicker },
-        },
-        {
-          vuetify,
-          attachTo: document.body,
-          i18n,
-        }
-      )
-      await waitForDebounce()
-      expect(wrapper).toMatchSnapshot('pickerclosed')
-      await wrapper.find('button').trigger('click')
-      expect(wrapper).toMatchSnapshot('pickeropen')
-      wrapper.destroy()
-    })
-
-    test('updates v-model when the value changes', async () => {
-      const wrapper = mountComponent(
-        {
-          data: () => ({ date: DATE_1 }),
-          template: '<div><e-date-picker v-model="date"></e-date-picker></div>',
-          components: { 'e-date-picker': EDatePicker },
-        },
-        {
-          vuetify,
-          i18n,
-        }
-      )
-      expect(wrapper.vm.date).toBe(DATE_1)
-      const inputSpy = jest.fn()
-      wrapper.findComponent(EDatePicker).vm.$on('input', (event) => inputSpy(event))
-      const input = wrapper.find('input[type=text]')
-      await input.setValue(data.date_2)
-      await waitForDebounce()
-      expect(inputSpy).toBeCalledTimes(1)
-      expect(inputSpy).toBeCalledWith(DATE_2)
-      expect(wrapper.vm.date).toBe(DATE_2)
-    })
-
-    test('validates the input', async () => {
-      const wrapper = mount({
-        propsData: {
-          value: DATE_1,
-        },
+    it('updates v-model when the input field is changed', async () => {
+      // given
+      const { emitted } = render(EDatePicker, {
+        props: { value: DATE1_ISO, name: 'test' },
       })
-      const input = wrapper.find('input[type=text]')
-      await input.setValue(INVALID_DATE_1)
-      await waitForDebounce()
-      expect(wrapper.text()).toContain('invalid format')
-      await input.setValue(INVALID_DATE_2)
-      await waitForDebounce()
-      expect(wrapper.text()).toContain('invalid format')
+      const inputField = await screen.findByDisplayValue(data.date1)
+
+      // when
+      await user.clear(inputField)
+      await user.keyboard(data.date2)
+
+      // then
+      await waitFor(async () => {
+        const events = emitted().input
+        // some input events were fired
+        expect(events.length).toBeGreaterThan(0)
+        // the last one included the parsed version of our entered date
+        expect(events[events.length - 1]).toEqual([DATE2_ISO])
+      })
+      // Our entered date should be visible...
+      screen.getByDisplayValue(data.date2)
+      // ...and stay visible
+      return expect(
+        waitFor(() => {
+          expect(screen.getByDisplayValue(data.date2)).not.toBeVisible()
+        })
+      ).rejects.toThrow(/Received element is visible/)
     })
 
-    test('updates its value when a date is picked', async () => {
-      const wrapper = mountComponent(
-        {
-          data: () => ({ date: DATE_1 }),
-          template: '<div data-app><e-date-picker v-model="date"></e-date-picker></div>',
-          components: { 'e-date-picker': EDatePicker },
-        },
-        {
-          vuetify,
-          attachTo: document.body,
-          i18n,
-        }
-      )
-      await waitForDebounce()
-      // open the date picker
-      const openPicker = wrapper.find('button')
-      await openPicker.trigger('click')
-      // click on day 24 of the month
-      await wrapper
-        .findAll('button')
-        .filter((node) => node.text() === '24')
-        .at(0)
-        .trigger('click')
-      await waitForDebounce()
-      expect(wrapper.find('input[type=text]').element.value).toBe(data.date_3)
-      wrapper.destroy()
+    it('updates v-model when a new date is selected in the picker', async () => {
+      // given
+      const { emitted } = render(EDatePicker, {
+        props: { value: DATE1_ISO, name: 'test' },
+      })
+      await screen.findByDisplayValue(data.date1)
+
+      // when
+      // click the button to open the picker
+      await user.click(screen.getByLabelText(data.labelText))
+      // click the 19th day of the month
+      await user.click(screen.getByText('19'))
+
+      // then
+      await waitFor(async () => {
+        const events = emitted().input
+        // some input events were fired
+        expect(events.length).toBeGreaterThan(0)
+        // the last one included the parsed version of our entered date
+        expect(events[events.length - 1]).toEqual([DATE2_ISO])
+      })
+      // Our selected date should be visible...
+      screen.getByDisplayValue(data.date2)
+      // ...and stay visible
+      return expect(
+        waitFor(() => {
+          expect(screen.getByDisplayValue(data.date2)).not.toBeVisible()
+        })
+      ).rejects.toThrow(/Received element is visible/)
+    })
+
+    it('validates the input', async () => {
+      // given
+      render(EDatePicker, {
+        props: { value: DATE1_ISO, name: 'test' },
+      })
+      const inputField = await screen.findByDisplayValue(data.date1)
+
+      // when
+      await user.clear(inputField)
+      await user.keyboard(data.dateInWrongLocale)
+
+      // then
+      await screen.findByText(data.validationMessage)
     })
   })
 })
---
+++ frontend/src/test/renderWithVuetify.js
@@ -0,0 +1,51 @@
+import { render as testingLibraryRender } from '@testing-library/vue'
+import i18n from '@/plugins/i18n/index.js'
+import dayjs from '@/plugins/dayjs.js'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import VueI18n from '../plugins/i18n/index.js'
+import formBaseComponents from '@/plugins/formBaseComponents'
+import { Wrapper } from '@vue/test-utils'
+
+Vue.use(Vuetify)
+Vue.use(formBaseComponents)
+Vue.use(dayjs)
+
+export const render = (component, options, callback) => {
+  const root = document.createElement('div')
+  root.setAttribute('data-app', 'true')
+
+  return testingLibraryRender(
+    component,
+    {
+      container: document.body.appendChild(root),
+      vuetify: new Vuetify(),
+      i18n,
+      ...options,
+    },
+    callback
+  )
+}
+
+/**
+ * This function emulates the setLanguage mutation on the Vuex store,
+ * for use in tests which need to set the application language.
+ */
+export const setTestLocale = (locale) => {
+  VueI18n.locale = locale
+  Vue.dayjs.locale(locale)
+}
+
+/**
+ * Wraps DOM nodes (such as the container object returned from a vue testing library render)
+ * in a vue-test-utils wrapper object, so it is processed by
+ */
+export const snapshotOf = (testingLibraryContainer) => {
+  if (!(testingLibraryContainer instanceof Element)) {
+    throw new Error(
+      'snapshotOf expects a DOM element as argument, but received ' +
+        testingLibraryContainer.toString()
+    )
+  }
+  return new Wrapper(testingLibraryContainer)
+}
