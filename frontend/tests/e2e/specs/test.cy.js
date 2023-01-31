853685b5da4b3bdea16fec7b6081af10e996ab67
Samuel Imfeld <sml.imfeld@gmail.com>
2023-01-31 22:09:14 +0100

    Adapt e2e smoke test

--- frontend/tests/e2e/specs/test.cy.js
+++ frontend/tests/e2e/specs/test.cy.js
@@ -4,7 +4,7 @@ describe('Smoke test', () => {
   it('displays the login page', () => {
     cy.visit('/')
     cy.contains('Login')
-    cy.contains('This is the site of the new eCamp.')
+    cy.contains('This is the development version of eCamp v3.')
     cy.contains('Register now')
   })
 })
