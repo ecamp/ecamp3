e1a72972e58d1634df352f6014649a46f4926168
Samuel Imfeld <sml.imfeld@gmail.com>
2023-01-31 22:43:42 +0100

    No v-html

--- frontend/src/views/auth/Login.vue
+++ frontend/src/views/auth/Login.vue
@@ -10,14 +10,9 @@
       style="hypens: auto"
       color="warning"
     >
-      <!-- eslint-disable-next-line vue/no-v-html -->
-      <div
-        v-html="
-          $tc(
-            `views.auth.login.infoText.${window.environment.LOGIN_INFO_TEXT_KEY ?? 'dev'}`
-          )
-        "
-      />
+      <div>
+        {{ $tc(`views.auth.login.infoText.${window.environment.LOGIN_INFO_TEXT_KEY ?? 'dev'}`) }}
+      </div>
     </v-alert>
     <v-alert v-if="error" outlined text border="left" type="error">
       {{ error }}
