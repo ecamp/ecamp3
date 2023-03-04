3329d829b8eafbcac980db8cf6f500ff38871c03
Urban Suppiger <urban@suppiger.net>
2023-03-04 08:04:19 +0100

    revert removal of package-lock.spec.js

---
+++ frontend/tests/infrastructure/package-lock.spec.js
@@ -0,0 +1,7 @@
+import { lockfileVersion } from '../../package-lock.json'
+
+describe('The package-lock.json', () => {
+  test('uses lockfileVersion 3', () => {
+    expect(lockfileVersion).toBe(3)
+  })
+})
