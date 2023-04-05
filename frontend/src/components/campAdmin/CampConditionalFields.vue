0ae8adfe320d59519e0172600bd88532ac263bde
Carlo Beltrame <carlo@beltra.me>
2023-04-05 14:23:28 +0200

    Hide fields which don't apply to all camps by default

--- frontend/src/components/campAdmin/CampConditionalFields.vue
+++ frontend/src/components/campAdmin/CampConditionalFields.vue
@@ -3,72 +3,81 @@ Displays fields which don't apply to all camps, but are required for some
 -->
 
 <template>
-  <div>
-    <content-group
-      :title="$tc('components.campAdmin.campConditionalFields.ysCamp.title')"
-    >
-      <v-skeleton-loader v-if="camp()._meta.loading" type="article" />
-      <div v-else class="mt-3">
-        <api-form :entity="camp()">
-          <api-text-field
-            fieldname="organizer"
-            :name="$tc('entity.camp.fields.organizer')"
-            :disabled="disabled"
-          />
-          <api-text-field
-            fieldname="kind"
-            :name="$tc('entity.camp.fields.kind')"
-            :disabled="disabled"
-          />
-          <api-text-field
-            fieldname="coachName"
-            :name="$tc('entity.camp.fields.coachName')"
-            :disabled="disabled"
-          />
-        </api-form>
-      </div>
-    </content-group>
-    <content-group
-      :title="$tc('components.campAdmin.campConditionalFields.course.title')"
-    >
-      <v-skeleton-loader v-if="camp()._meta.loading" type="article" />
-      <div v-else class="mt-3">
-        <api-form :entity="camp()">
-          <api-text-field
-            fieldname="courseNumber"
-            :name="$tc('entity.camp.fields.courseNumber')"
-            :disabled="disabled"
-          />
-          <api-text-field
-            fieldname="courseKind"
-            :name="$tc('entity.camp.fields.courseKind')"
-            :disabled="disabled"
-          />
-          <api-text-field
-            fieldname="trainingAdvisorName"
-            :name="$tc('entity.camp.fields.trainingAdvisorName')"
-            :disabled="disabled"
-          />
-          <api-checkbox
-            fieldname="printYSLogoOnPicasso"
-            :name="$tc('entity.camp.fields.printYSLogoOnPicasso')"
-            :disabled="disabled"
-          />
-        </api-form>
-      </div>
-    </content-group>
-  </div>
+  <v-expansion-panels class="mb-8 mt-2">
+    <v-expansion-panel>
+      <v-expansion-panel-header>
+        <div class="subtitle-1 conditional-group-title">
+          {{ $tc('components.campAdmin.campConditionalFields.ysCamp.title') }}
+        </div>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-skeleton-loader v-if="camp()._meta.loading" type="article" />
+        <div v-else class="mt-3">
+          <api-form :entity="camp()">
+            <api-text-field
+              fieldname="organizer"
+              :name="$tc('entity.camp.fields.organizer')"
+              :disabled="disabled"
+            />
+            <api-text-field
+              fieldname="kind"
+              :name="$tc('entity.camp.fields.kind')"
+              :disabled="disabled"
+            />
+            <api-text-field
+              fieldname="coachName"
+              :name="$tc('entity.camp.fields.coachName')"
+              :disabled="disabled"
+            />
+          </api-form>
+        </div>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+    <v-expansion-panel>
+      <v-expansion-panel-header>
+        <div class="subtitle-1 conditional-group-title">
+          {{ $tc('components.campAdmin.campConditionalFields.course.title') }}
+        </div>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <v-skeleton-loader v-if="camp()._meta.loading" type="article" />
+        <div v-else class="mt-3">
+          <api-form :entity="camp()">
+            <api-text-field
+              fieldname="courseNumber"
+              :name="$tc('entity.camp.fields.courseNumber')"
+              :disabled="disabled"
+            />
+            <api-text-field
+              fieldname="courseKind"
+              :name="$tc('entity.camp.fields.courseKind')"
+              :disabled="disabled"
+            />
+            <api-text-field
+              fieldname="trainingAdvisorName"
+              :name="$tc('entity.camp.fields.trainingAdvisorName')"
+              :disabled="disabled"
+            />
+            <api-checkbox
+              fieldname="printYSLogoOnPicasso"
+              :name="$tc('entity.camp.fields.printYSLogoOnPicasso')"
+              :disabled="disabled"
+            />
+          </api-form>
+        </div>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
 import ApiTextField from '@/components/form/api/ApiTextField.vue'
-import ContentGroup from '@/components/layout/ContentGroup.vue'
 import ApiForm from '@/components/form/api/ApiForm.vue'
 import ApiCheckbox from '../form/api/ApiCheckbox.vue'
 
 export default {
   name: 'CampConditionalFields',
-  components: { ContentGroup, ApiTextField, ApiCheckbox, ApiForm },
+  components: { ApiTextField, ApiCheckbox, ApiForm },
   props: {
     camp: {
       type: Function,
@@ -82,4 +91,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.conditional-group-title {
+  font-size: 1rem !important;
+}
+</style>
