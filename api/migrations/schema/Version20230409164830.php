795084f17d62c59de15d61b1a5393772d8b0354f
Pirmin Mattmann <pimattmann@gmail.com>
2023-04-09 19:28:37 +0200

    Add migration

---
+++ api/migrations/schema/Version20230409164830.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230409164830 extends AbstractMigration {
+    public function getDescription(): string {
+        return 'Create ActivityProgressLabels for camps from their prototype';
+    }
+
+    public function up(Schema $schema): void {
+        $this->addSql('CREATE EXTENSION pgcrypto');
+
+        // Test:
+        // J+S
+        $this->addSql('INSERT INTO activity_progress_label (id, campId, position, title, createTime, updateTime) 
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'e5027d852487\', 1, \'In Planung\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'e5027d852487\'
+            UNION
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'e5027d852487\', 2, \'Geplant\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'e5027d852487\'
+            UNION
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'e5027d852487\', 3, \'Lagerleiter:in OK\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'e5027d852487\'
+            UNION
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'e5027d852487\', 4, \'Coach OK\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'e5027d852487\'
+        ');
+
+        // Prod:
+        // J+S Lager (Deutsch)
+        $this->addSql('INSERT INTO activity_progress_label (id, campId, position, title, createTime, updateTime) 
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'75b3572a338e\', 1, \'In Planung\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'75b3572a338e\'
+            UNION
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'75b3572a338e\', 2, \'Geplant\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'75b3572a338e\'
+            UNION
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'75b3572a338e\', 3, \'Lagerleiter:in OK\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'75b3572a338e\'
+            UNION
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'75b3572a338e\', 4, \'Coach OK\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'75b3572a338e\'
+        ');
+
+        // Camp J+S (français)
+        $this->addSql('INSERT INTO activity_progress_label (id, campId, position, title, createTime, updateTime) 
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'497f974e7d5d\', 1, \'En cours de planification\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'497f974e7d5d\'
+            UNION
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'497f974e7d5d\', 2, \'Planifié\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'497f974e7d5d\'
+            UNION
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'497f974e7d5d\', 3, \'Chef de camp OK\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'497f974e7d5d\'
+            UNION
+            SELECT  encode(gen_random_bytes(6), \'hex\'), \'497f974e7d5d\', 4, \'Coach OK\', 
+                    date_trunc(\'second\', now()::timestamp), date_trunc(\'second\', now()::timestamp)
+            FROM    camp c
+            WHERE   c.id = \'497f974e7d5d\'
+        ');
+
+        // All ohter camps
+        $this->addSql('INSERT INTO activity_progress_label (id, campId, position, title, createTime, updateTime) 
+            SELECT  encode(gen_random_bytes(6), \'hex\')
+            ,       c.id
+            ,       lp.position
+            ,       lp.title
+            ,       date_trunc(\'second\', now()::timestamp)
+            ,       date_trunc(\'second\', now()::timestamp)
+            FROM camp c
+            JOIN camp cp on cp.id = c.campprototypeid
+            JOIN activity_progress_label lp on lp.campid = cp.id
+            WHERE not exists (select 1 from activity_progress_label l where l.campid = c.id)
+        ');
+
+        $this->addSql('DROP EXTENSION pgcrypto');
+    }
+
+    public function down(Schema $schema): void {
+    }
+}
