6b56ed2a5e50d2722112b45d8c3a0cb42382a816
Nicola <57986114+DeNic0la@users.noreply.github.com>
2023-04-09 10:46:59 +0200

    Update frontend/src/plugins/store/auth.js
    Co-authored-by: Carlo Beltrame <carlo@beltra.me>

--- frontend/src/plugins/store/auth.js
+++ frontend/src/plugins/store/auth.js
@@ -14,22 +14,14 @@ export const mutations = {
   },
 }
 export const getters = {
-  /**
-   * @returns The latest Logged-in User Object with a profile()
-   */
-  getLoggedInUserWithProfile: (_authState, getters) => {
-    let loggedInUser = getters.getLoggedInUser
-    if (typeof loggedInUser?.profile !== 'object') return loggedInUser
-    if (!loggedInUser) return loggedInUser
-    let profileHref =
-      loggedInUser._storeData?.profile?.href ?? loggedInUser?.profile?.href
-    let profile = () => apiStore.get(profileHref)
-    return { ...loggedInUser, profile }
-  },
   getLoggedInUser: (authState, _getters, _rootState, rootGetters) => {
     if (!authState.user) return authState.user
-    let userFromApiStoreById = rootGetters.getUserFromApiStoreById(authState.user.id)
-    return userFromApiStoreById ? userFromApiStoreById : authState.user
+    // Get the URI of the user - we use URIs instead of just IDs to identify objects uniquely in the frontend
+    userUri = authState.user._meta.self
+    
+    // Let the API store work its magic. We just give it a URI, and it automagically retrieves the requested
+    // data from the store (which acts as a cache), or from the API if it hasn't been fetched before.
+    return apiStore.get(userUri)
   },
 }
 
