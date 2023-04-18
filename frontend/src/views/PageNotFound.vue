5c7fedd70f68167cf6382ecfbe87f5d8fcc1492b
DeNic0la <nicola@fioretti.info>
2023-04-18 20:57:32 +0200

    make camp-not-found display a 404 while still displaying the requested url

--- frontend/src/views/PageNotFound.vue
+++ frontend/src/views/PageNotFound.vue
@@ -3,15 +3,28 @@
     <content-card
       class="contentCard"
       :title="$tc('views.pageNotFound.title')"
-      max-width="800"
+      max-width="1200"
+      toolbar
     >
+      <template #title-actions>
+        <v-btn
+          class="d-md-none"
+          icon
+          :to="{ name: 'profile', query: { isDetail: true } }"
+        >
+          <user-avatar :user="user" :size="36" />
+        </v-btn>
+      </template>
+
       <v-row>
         <v-col
-          offset-sm="0"
+          offset="0"
+          offset-sm="1"
           offset-md="1"
           offset-lg="2"
           offset-xl="2"
-          sm="12"
+          cols="12"
+          sm="10"
           md="10"
           lg="8"
           xl="8"
@@ -19,23 +32,25 @@
           <four-zero-four-display />
         </v-col>
         <v-col offset-sm="0" sm="12" offset="1" cols="10">
-          <h1 class="font-weight-bold text-center">Page not found</h1>
+          <h1 class="font-weight-bold text-center">
+            {{ $tc('views.pageNotFound.detail') }}
+          </h1>
         </v-col>
       </v-row>
     </content-card>
-    <template v-if="$vuetify.breakpoint.mdAndUp"> </template>
-    <template v-else> </template>
   </v-container>
 </template>
 
 <script>
 import ContentCard from '@/components/layout/ContentCard.vue'
 import FourZeroFourDisplay from '@/components/navigation/FourZeroFourDisplay.vue'
+import UserAvatar from '@/components/user/UserAvatar.vue'
 
 export default {
   name: 'PageNotFound',
   components: {
     FourZeroFourDisplay,
+    UserAvatar,
     ContentCard,
   },
   computed: {
@@ -48,17 +63,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
+h1 {
+  font-size: 4em;
+}
 @media #{map-get($display-breakpoints, 'lg-and-down')} {
   h1 {
-    font-size: 10rem;
+    font-size: 3em;
   }
 }
 @media #{map-get($display-breakpoints, 'md-and-down')} {
   h1 {
-    font-size: 8rem;
+    font-size: 2em;
   }
-}
-.contentCard > * {
-  background-color: lightgrey !important;
 }
 </style>
