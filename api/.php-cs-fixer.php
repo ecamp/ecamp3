bbe63f506e8513064e0f3aefd9f6bb766a57418f
Urban Suppiger <urban@suppiger.net>
2023-05-01 17:17:46 -0400

    fix deprecated 'braces' config

--- api/.php-cs-fixer.php
+++ api/.php-cs-fixer.php
@@ -11,7 +11,10 @@ return (new PhpCsFixer\Config())
         '@PSR2' => true,
         '@DoctrineAnnotation' => true,
         '@PhpCsFixer' => true,
-        'braces' => ['position_after_functions_and_oop_constructs' => 'same'],
+        'curly_braces_position' => [
+            'functions_opening_brace' => 'same_line',
+            'classes_opening_brace' => 'same_line'
+        ],
         'php_unit_test_class_requires_covers' => false,
         'no_superfluous_phpdoc_tags' => true,
     ])
