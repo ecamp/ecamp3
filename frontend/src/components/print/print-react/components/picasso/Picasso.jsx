2f610961e123e65abf7cd6c72eea7dce06fe9453
Carlo Beltrame <carlo@beltra.me>
2023-06-12 14:25:49 +0200

    Make sure days are sorted in react print

--- frontend/src/components/print/print-react/components/picasso/Picasso.jsx
+++ frontend/src/components/print/print-react/components/picasso/Picasso.jsx
@@ -3,18 +3,20 @@ import React from 'react'
 import PicassoPage from './PicassoPage.jsx'
 import { calculateBedtime, splitDaysIntoPages } from '@/common/helpers/picasso.js'
 import dayjs from '@/common/helpers/dayjs.js'
+import sortBy from 'lodash/sortBy.js'
 
 function Picasso(props) {
   return props.content.options.periods.map((periodUri) => {
     const period = props.store.get(periodUri)
+    const days = sortBy(period.days().items, (day) => dayjs.utc(day.start).unix())
     const maxDaysPerPage = props.content.options.orientation === 'L' ? 8 : 4
-    const picassoPages = splitDaysIntoPages(period.days().items, maxDaysPerPage)
+    const picassoPages = splitDaysIntoPages(days, maxDaysPerPage)
     const timeStep = 1
     const { getUpTime, bedtime } = calculateBedtime(
       period.scheduleEntries().items,
       dayjs,
-      dayjs.utc(period.days().items[0].start),
-      dayjs.utc(period.days().items[period.days().items.length - 1].end),
+      dayjs.utc(days[0].start),
+      dayjs.utc(days[days.length - 1].end),
       timeStep
     )
 
