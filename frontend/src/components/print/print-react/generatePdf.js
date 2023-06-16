78b1a9c6b018ad8a58b2b5f1cb4c7bcae80d35b0
Carlo Beltrame <carlo@beltra.me>
2023-06-16 21:07:18 +0200

    Clean up now that firefox has module worker support
    https://caniuse.com/mdn-api_worker_worker_ecmascript_modules
    https://www.mozilla.org/en-US/firefox/114.0/releasenotes/#note-789543
    https://bugzilla.mozilla.org/show_bug.cgi?id=1247687
    https://bugzilla.mozilla.org/show_bug.cgi?id=1540913

--- frontend/src/components/print/print-react/generatePdf.js
+++ frontend/src/components/print/print-react/generatePdf.js
@@ -1,43 +1,19 @@
 import prepareInMainThread from './documents/campPrint/index.js'
 import cloneDeep from 'lodash/cloneDeep.js'
 
-function browserSupportsWorkerType() {
-  let supports = false
-  const tester = {
-    get type() {
-      supports = true
-      return 'module'
-    }, // it's been called, it's supported
-  }
-  try {
-    new Worker('blob://', tester)
-  } finally {
-    // we need the return statement inside finally for this type of feature detection
-    // eslint-disable-next-line no-unsafe-finally
-    return supports
-  }
-}
-
 export const generatePdf = async (data) => {
   await prepareInMainThread(data.config)
 
   const serializableData = prepareDataForSerialization(data)
 
-  // Firefox does not support the ESM workers which Vite generates in development.
-  // Production is fine though.
-  const workerSupported =
-    process.env.NODE_ENV === 'production' || browserSupportsWorkerType()
-
-  if (data.renderInWorker && workerSupported) {
+  if (data.renderInWorker) {
+    // ComlinkWorker is provided by vite-plugin-comlink
     // eslint-disable-next-line no-undef
     const instance = new ComlinkWorker(
       new URL('./documents/campPrint/renderPdf.worker.js', import.meta.url)
     )
     return await instance.renderPdfInWorker(serializableData)
   } else {
-    // In Firefox, dynamic imports are only available in the main thread:
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=1540913
-    // So we use dynamic imports if we are in the main thread, but static imports if we are in the worker.
     return await (
       await import('./documents/campPrint/renderPdf.js')
     ).renderPdf(serializableData)
