d11073f6a473ac2384fd2567d6a5110cf4bcf964
Carlo Beltrame <carlo@beltra.me>
2023-06-23 16:32:05 +0200

    Revert unintentional change

--- frontend/src/components/print/PrintConfigurator.vue
+++ frontend/src/components/print/PrintConfigurator.vue
@@ -147,7 +147,7 @@ export default {
         camp: this.camp()._meta.self,
         contents: this.defaultContents(),
       },
-      previewTab: 1,
+      previewTab: null,
     }
   },
   computed: {
