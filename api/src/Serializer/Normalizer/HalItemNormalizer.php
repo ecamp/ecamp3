b49fc4f71f3261c09739e6ec40b90c4dd3698fd5
Urban Suppiger <urban@suppiger.net>
2023-06-25 00:34:29 -0400

    Proof of concept: integrate RelatedCollectionLinkNormalizer into HalItemNormalizer

--- api/src/Serializer/Normalizer/HalItemNormalizer.php
+++ api/src/Serializer/Normalizer/HalItemNormalizer.php
@@ -13,14 +13,35 @@ declare(strict_types=1);
 
 namespace App\Serializer\Normalizer;
 
+use ApiPlatform\Api\IriConverterInterface;
+use ApiPlatform\Api\ResourceClassResolverInterface;
 use ApiPlatform\Api\UrlGeneratorInterface;
+use ApiPlatform\Doctrine\Common\PropertyHelperTrait;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\Property\Factory\PropertyMetadataFactoryInterface;
+use ApiPlatform\Metadata\Property\Factory\PropertyNameCollectionFactoryInterface;
+use ApiPlatform\Metadata\Resource\Factory\ResourceMetadataCollectionFactoryInterface;
 use ApiPlatform\Metadata\Util\ClassInfoTrait;
 use ApiPlatform\Serializer\AbstractItemNormalizer;
 use ApiPlatform\Serializer\CacheKeyTrait;
 use ApiPlatform\Serializer\ContextTrait;
+use ApiPlatform\Symfony\Security\ResourceAccessCheckerInterface;
+use App\Entity\BaseEntity;
+use App\Metadata\Resource\Factory\UriTemplateFactory;
+use App\Metadata\Resource\OperationHelper;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use Doctrine\ORM\Mapping\MappingException;
+use Doctrine\Persistence\ManagerRegistry;
+use Rize\UriTemplate;
+use Symfony\Component\DependencyInjection\ServiceLocator;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Serializer\Exception\LogicException;
 use Symfony\Component\Serializer\Exception\UnexpectedValueException;
 use Symfony\Component\Serializer\Mapping\AttributeMetadataInterface;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactoryInterface;
+use Symfony\Component\Serializer\NameConverter\AdvancedNameConverterInterface;
+use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
 
 /**
  * Converts between objects and array including HAL metadata.
@@ -31,11 +52,32 @@ final class HalItemNormalizer extends AbstractItemNormalizer {
     use CacheKeyTrait;
     use ClassInfoTrait;
     use ContextTrait;
+    use PropertyHelperTrait;
 
     public const FORMAT = 'jsonhal';
 
     private array $componentsCache = [];
     private array $attributesMetadataCache = [];
+
+    public function __construct(
+        protected PropertyNameCollectionFactoryInterface $propertyNameCollectionFactory,
+        protected PropertyMetadataFactoryInterface $propertyMetadataFactory,
+        protected IriConverterInterface $iriConverter,
+        protected ResourceClassResolverInterface $resourceClassResolver,
+        PropertyAccessorInterface $propertyAccessor,
+        NameConverterInterface $nameConverter,
+        ClassMetadataFactoryInterface $classMetadataFactory,
+        array $defaultContext,
+        ResourceMetadataCollectionFactoryInterface $resourceMetadataCollectionFactory,
+        protected ?ResourceAccessCheckerInterface $resourceAccessChecker,
+        private ServiceLocator $filterLocator,
+        private UriTemplate $uriTemplate,
+        private UriTemplateFactory $uriTemplateFactory,
+        private RouterInterface $router,
+        private ManagerRegistry $managerRegistry
+    ) {
+        parent::__construct($propertyNameCollectionFactory, $propertyMetadataFactory, $iriConverter, $resourceClassResolver, $propertyAccessor, $nameConverter, $classMetadataFactory, $defaultContext, $resourceMetadataCollectionFactory, $resourceAccessChecker);
+    }
 
     /**
      * {@inheritdoc}
@@ -107,6 +149,100 @@ final class HalItemNormalizer extends AbstractItemNormalizer {
      */
     protected function getAttributes($object, $format = null, array $context = []): array {
         return $this->getComponents($object, $format, $context)['states'];
+    }
+
+    protected function getRelatedCollectionLinkAnnotation(string $className, string $propertyName): ?RelatedCollectionLink {
+        try {
+            $reflClass = $this->getReflectionClass($className);
+            $method = $reflClass->getMethod('get'.ucfirst($propertyName));
+            $attributes = $method->getAttributes(RelatedCollectionLink::class);
+
+            return ($attributes[0] ?? null)?->newInstance();
+        } catch (\ReflectionException $e) {
+            return null;
+        }
+    }
+
+    protected function getReflectionClass($className): \ReflectionClass {
+        return new \ReflectionClass($className);
+    }
+
+    protected function extractUriParams($object, array $params): array {
+        $result = [];
+        foreach ($params as $param => $value) {
+            if ('$this' === $value) {
+                $result[$param] = $this->normalizeUriParam($object);
+            } else {
+                $result[$param] = $this->normalizeUriParam($this->propertyAccessor->getValue($object, $value));
+            }
+        }
+
+        return $result;
+    }
+
+    protected function normalizeUriParam($param): string {
+        if ($param instanceof \DateTimeInterface) {
+            $param = $param->format(\DateTime::W3C);
+        }
+        if ($param instanceof BaseEntity) {
+            $param = $this->iriConverter->getIriFromResource($param);
+        }
+
+        return $param;
+    }
+
+    protected function getManagerRegistry(): ManagerRegistry {
+        return $this->managerRegistry;
+    }
+
+    private function getRelatedCollectionHref($object, $rel, array $context = []): string {
+        $resourceClass = $this->getObjectClass($object);
+
+        if ($this->nameConverter instanceof AdvancedNameConverterInterface) {
+            $rel = $this->nameConverter->denormalize($rel, $resourceClass, null, array_merge($context, ['groups' => ['read']]));
+        }
+
+        if ($annotation = $this->getRelatedCollectionLinkAnnotation($resourceClass, $rel)) {
+            // If there is an explicit annotation, there is no need to inspect the Doctrine metadata
+            $params = $this->extractUriParams($object, $annotation->getParams());
+            [$uriTemplate] = $this->uriTemplateFactory->createFromResourceClass($annotation->getRelatedEntity());
+
+            return $this->uriTemplate->expand($uriTemplate, $params);
+        }
+
+        try {
+            $classMetadata = $this->getClassMetadata($resourceClass);
+
+            if (!$classMetadata instanceof ClassMetadataInfo) {
+                throw new \RuntimeException("The class metadata for {$resourceClass} must be an instance of ClassMetadataInfo.");
+            }
+
+            $relationMetadata = $classMetadata->getAssociationMapping($rel);
+        } catch (MappingException) {
+            throw new UnsupportedRelationException($resourceClass.'#'.$rel.' is not a Doctrine association. Embedding non-Doctrine collections is currently not implemented.');
+        }
+
+        $relatedResourceClass = $relationMetadata['targetEntity'];
+
+        $relatedFilterName = $relationMetadata['mappedBy'];
+        $relatedFilterName ??= $relationMetadata['inversedBy'];
+
+        if (empty($relatedResourceClass) || empty($relatedFilterName)) {
+            throw new UnsupportedRelationException('The '.$resourceClass.'#'.$rel.' relation does not have both a targetEntity and a mappedBy or inversedBy property');
+        }
+
+        $resourceMetadataCollection = $this->resourceMetadataCollectionFactory->create($relatedResourceClass);
+        $operation = OperationHelper::findOneByType($resourceMetadataCollection, GetCollection::class);
+
+        if (!$operation) {
+            throw new UnsupportedRelationException('The resource '.$relatedResourceClass.' does not implement GetCollection() operation.');
+        }
+
+        if (!$this->exactSearchFilterExists($relatedResourceClass, $relatedFilterName)) {
+            throw new UnsupportedRelationException('The resource '.$relatedResourceClass.' does not have a search filter for the relation '.$relatedFilterName.'.');
+        }
+
+        return $this->router->generate($operation->getName(), [$relatedFilterName => urlencode($this->iriConverter->getIriFromResource($object))], UrlGeneratorInterface::ABS_PATH);
     }
 
     /**
@@ -182,12 +318,22 @@ final class HalItemNormalizer extends AbstractItemNormalizer {
                 continue;
             }
 
-            $attributeValue = $this->getAttributeValue($object, $relation['name'], $format, $context);
-
             $relationName = $relation['name'];
             if ($this->nameConverter) {
                 $relationName = $this->nameConverter->normalize($relationName, $class, $format, $context);
             }
+
+            if ('many' === $relation['cardinality'] && 'links' === $type) {
+                try {
+                    $data[$key][$relationName]['href'] = $this->getRelatedCollectionHref($object, $relationName, $context);
+
+                    continue;
+                } catch (UnsupportedRelationException $e) {
+                    // The relation is not supported, or there is no matching filter defined on the related entity
+                }
+            }
+
+            $attributeValue = $this->getAttributeValue($object, $relation['name'], $format, $context);
 
             if ('one' === $relation['cardinality']) {
                 if ('links' === $type) {
@@ -258,5 +404,26 @@ final class HalItemNormalizer extends AbstractItemNormalizer {
         ++$context[$key];
 
         return false;
+    }
+
+    /**
+     * @throws ResourceClassNotFoundException
+     */
+    private function exactSearchFilterExists(string $resourceClass, mixed $propertyName): bool {
+        $resourceMetadataCollection = $this->resourceMetadataCollectionFactory->create($resourceClass);
+        $filterIds = OperationHelper::findOneByType($resourceMetadataCollection, GetCollection::class)?->getFilters() ?? [];
+
+        return 0 < count(array_filter($filterIds, function ($filterId) use ($resourceClass, $propertyName) {
+            /** @var FilterInterface $filter */
+            $filter = $this->filterLocator->get($filterId);
+            if (!$filter instanceof SearchFilter) {
+                return false;
+            }
+            $filterDescription = $filter->getDescription($resourceClass);
+
+            return array_key_exists($propertyName, $filterDescription)
+                && isset($filterDescription[$propertyName]['strategy'])
+                && 'exact' === $filterDescription[$propertyName]['strategy'];
+        }));
     }
 }
