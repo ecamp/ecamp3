b9d2791173af75d721075ae190102dda50cc254d
Manuel Meister <news.manuelsworld@gmail.com>
2023-07-06 01:49:48 +0200

    Create table truncate wrapper
    This element disregards its min-width and instead adds ellipsis if the text is longer than the available space

---
+++ frontend/src/components/generic/Truncate.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="truncate">
+    <span><slot /></span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Truncate',
+}
+</script>
+
+<style>
+.truncate {
+  display: inline-table;
+  table-layout: fixed;
+  width: 100%;
+}
+
+.truncate > * {
+  display: block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
