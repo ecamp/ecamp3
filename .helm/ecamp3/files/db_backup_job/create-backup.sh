7c0200811857d39de20522eb5af8bcf34db735b0
BacLuc <lucius.bachmann@gmx.ch>
2023-07-31 18:14:34 +0200

    .helm/db_backup_job: remove owner and privilege statements from backup
    That we can restore it with another user.
    
    Issue: #3478

--- .helm/ecamp3/files/db_backup_job/create-backup.sh
+++ .helm/ecamp3/files/db_backup_job/create-backup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
 
-pg_dump $DATABASE_URL --column-inserts --rows-per-insert 10000 > /tmp/backup-dir/pgdump.sql
+pg_dump $DATABASE_URL \
+      --no-owner \
+      --no-privileges \
+      --column-inserts \
+      --rows-per-insert 10000 \
+      > /tmp/backup-dir/pgdump.sql
 gzip /tmp/backup-dir/pgdump.sql
 if [ -n "${ENCRYPTION_KEY}" ]; then
     echo "Encrypting backup"
