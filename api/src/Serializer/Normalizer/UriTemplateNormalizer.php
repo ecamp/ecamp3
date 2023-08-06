f464932ad282c629d822190ff1cdfcbbd16b1c9e
BacLuc <lucius.bachmann@gmx.ch>
2023-08-06 12:41:59 +0200

    UriTemplateNormalizer: switch to getSupportedTypes
    CacheableSupportsMethodInterface is deprecated.
    
    fixes:
       1x: Class "App\Serializer\Normalizer\UriTemplateNormalizer" should implement method "Symfony\Component\Serializer\Normalizer\NormalizerInterface::getSupportedTypes(?string $format): array".
        1x in CreateActivityTest::testCreateActivityIsDeniedForAnonymousUser from App\Tests\Api\Activities
    
      1x: The "App\Serializer\Normalizer\UriTemplateNormalizer" class implements "Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface" that is deprecated since Symfony 6.3, implement "getSupportedTypes(?string $format)" instead.
        1x in CreateActivityTest::testCreateActivityIsDeniedForAnonymousUser from App\Tests\Api\Activities

--- api/src/Serializer/Normalizer/UriTemplateNormalizer.php
+++ api/src/Serializer/Normalizer/UriTemplateNormalizer.php
@@ -4,7 +4,6 @@ namespace App\Serializer\Normalizer;
 
 use ApiPlatform\Api\UrlGeneratorInterface;
 use App\Metadata\Resource\Factory\UriTemplateFactory;
-use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\String\Inflector\EnglishInflector;
 
@@ -12,7 +11,7 @@ use Symfony\Component\String\Inflector\EnglishInflector;
  * This class modifies the API entrypoint when retrieved in HAL JSON format (/index.jsonhal) to include URI templates, and additionally
  * makes sure that the relation names of the _links are in plural rather than the default singular of API platform.
  */
-class UriTemplateNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface {
+class UriTemplateNormalizer implements NormalizerInterface {
     public function __construct(
         private NormalizerInterface $decorated,
         private EnglishInflector $inflector,
@@ -58,11 +57,11 @@ class UriTemplateNormalizer implements NormalizerInterface, CacheableSupportsMet
         return $result;
     }
 
-    public function hasCacheableSupportsMethod(): bool {
-        if (!$this->decorated instanceof CacheableSupportsMethodInterface) {
-            return false;
+    public function getSupportedTypes(?string $format): array {
+        if (method_exists($this->decorated, 'getSupportedTypes')) {
+            return $this->decorated->getSupportedTypes($format);
         }
 
-        return $this->decorated->hasCacheableSupportsMethod();
+        return ['*' => false];
     }
 }
