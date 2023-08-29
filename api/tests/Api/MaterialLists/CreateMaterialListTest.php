fa4e50a7933a845be2e3991ba2a2476f3b6267d8
BacLuc <lucius.bachmann@gmx.ch>
2023-08-29 13:34:11 +0200

    api: test that create and readItem responses are hal compatible
    Here i could not generalize the tests because
    generating valid post bodies dynamically
    was not easily possible.

--- api/tests/Api/MaterialLists/CreateMaterialListTest.php
+++ api/tests/Api/MaterialLists/CreateMaterialListTest.php
@@ -6,6 +6,14 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Post;
 use App\Entity\MaterialList;
 use App\Tests\Api\ECampApiTestCase;
+use App\Tests\Constraints\CompatibleHalResponse;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+use function PHPUnit\Framework\assertThat;
 
 /**
  * @internal
@@ -190,6 +198,33 @@ class CreateMaterialListTest extends ECampApiTestCase {
         $this->assertJsonContains([
             'name' => '<b>t</b',
         ]);
+    }
+
+    /**
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     */
+    public function testCreateResponseStructureMatchesReadResponseStructure() {
+        $client = static::createClientWithCredentials();
+        $client->disableReboot();
+        $createResponse = $client->request(
+            'POST',
+            '/material_lists',
+            [
+                'json' => $this->getExampleWritePayload(),
+            ]
+        );
+
+        $this->assertResponseStatusCodeSame(201);
+
+        $createArray = $createResponse->toArray();
+        $newItemLink = $createArray['_links']['self']['href'];
+        $getItemResponse = $client->request('GET', $newItemLink);
+
+        assertThat($createArray, CompatibleHalResponse::isHalCompatibleWith($getItemResponse->toArray()));
     }
 
     public function getExampleWritePayload($attributes = [], $except = []) {
