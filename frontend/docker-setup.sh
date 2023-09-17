35e8b268d9d2cd9357e28b1abe6b9dbd1ffd6f0c
Manuel Meister <news.manuelsworld@gmail.com>
2023-09-17 13:17:09 +0200

    Prevent build error on repository clone when pdf service has not been run yet
    Signed-off-by: Manuel Meister <news.manuelsworld@gmail.com>

--- frontend/docker-setup.sh
+++ frontend/docker-setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
+BASEDIR=$(dirname "$0")
+PDF_DIST=$BASEDIR"/src/pdf"
+
+if [ ! -f "$PDF_DIST/pdf.mjs" ] || [ ! -f "$PDF_DIST/prepareInMainThread.mjs" ]; then
+    # Copy dummy versions of the pdf build outputs, to make sure there is always something to import
+    cp "$PDF_DIST/pdf.mjs.dist" "$PDF_DIST/pdf.mjs"
+    cp "$PDF_DIST/prepareInMainThread.mjs.dist" "$PDF_DIST/prepareInMainThread.mjs"
+fi
+
 if [ "$CI" = 'true' ] ; then
   npm ci --verbose
   npm run build
