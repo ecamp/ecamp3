ecamp/ecamp3

commit 3d4c73ea4157f5d45a605a0aa698dec6e789ed8a

Make picasso schedule entries clickable

pdf/src/campPrint/picasso/ScheduleEntry.vue
@@ -4,9 +4,11 @@
     :style="{ backgroundColor: color, color: textColor }"
   >
     <View class="picasso-schedule-entry-spacer" />
-    <Text class="picasso-schedule-entry-title">
-      {{ category }} {{ scheduleEntry.number }} {{ title }}
-    </Text>
+    <Link class="picasso-schedule-entry-link" :href="linkTarget">
+      <Text class="picasso-schedule-entry-title">
+        {{ category }} {{ scheduleEntry.number }} {{ title }}
+      </Text>
+    </Link>
     <View class="picasso-schedule-entry-spacer" />
     <View class="picasso-schedule-entry-responsibles-container">
       <View class="picasso-schedule-entry-spacer" />
@@ -44,10 +46,17 @@ export default {
     title() {
       return this.scheduleEntry.activity().title
     },
+    linkTarget() {
+      return `#scheduleEntry_${this.scheduleEntry.id}`
+    },
   },
 }
 </script>
 <pdf-style>
+.picasso-schedule-entry-link {
+  text-decoration: none;
+  color: black;
+}
 .picasso-schedule-entry {
   position: absolute;
   padding: 0 4pt;

pdf/src/campPrint/scheduleEntry/ScheduleEntry.vue
@@ -4,7 +4,7 @@
       class="schedule-entry-header-title"
       :style="{ borderBottomColor: activity.category().color }"
     >
-      <View class="schedule-entry-title">
+      <View :id="`scheduleEntry_${scheduleEntry.id}`" class="schedule-entry-title">
         <CategoryLabel
           :category="activity.category()"
           class="schedule-entry-category-label"

commit 1d3e45ef99630b9d38f6c8ae66ad7ae5f6af9998

Fix font coloring
This probably broke in 3d4c73ea

pdf/src/campPrint/picasso/ScheduleEntry.vue
@@ -1,11 +1,11 @@
 <template>
-  <View
-    class="picasso-schedule-entry"
-    :style="{ backgroundColor: color, color: textColor }"
-  >
+  <View class="picasso-schedule-entry" :style="{ backgroundColor: color }">
     <View class="picasso-schedule-entry-spacer" />
     <Link class="picasso-schedule-entry-link" :href="linkTarget">
-      <Text class="picasso-schedule-entry-title" :style="{ fontSize: fontSize }">
+      <Text
+        class="picasso-schedule-entry-title"
+        :style="{ fontSize: fontSize, color: textColor }"
+      >
         {{ category }} {{ scheduleEntry.number }} {{ title }}
       </Text>
     </Link>