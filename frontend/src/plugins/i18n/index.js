d42778d96145d1a5aaa5cb69fb7bb248c4c71c68
BacLuc <lucius.bachmann@gmx.ch>
2023-11-18 18:13:55 +0100

    frontend: use fallback logic from vue-i18n to fallback to de from rm
    As documented here: https://kazupon.github.io/vue-i18n/guide/fallback.html#explicit-fallback-with-decision-maps
    Reimplement the vue-i18n fallback logic for the api error message
    translation in apiFallbackLocalesFor.js
    Add empty rumantsch translations to print and common,
    and test that the translation works in print with one
    rumantsch translation in common.

--- frontend/src/plugins/i18n/index.js
+++ frontend/src/plugins/i18n/index.js
@@ -10,6 +10,8 @@ import enCommon from '@/common/locales/en.json'
 import enCHScoutCommon from '@/common/locales/en-CH-scout.json'
 import deCommon from '@/common/locales/de.json'
 import deCHScoutCommon from '@/common/locales/de-CH-scout.json'
+import rmCommon from '@/common/locales/rm.json'
+import rmCHScoutCommon from '@/common/locales/rm-CH-scout.json'
 
 import it from '@/locales/it.json'
 import itCHScout from '@/locales/it-CH-scout.json'
@@ -34,10 +36,14 @@ import vuetifyIt from 'vuetify/lib/locale/it'
 
 Vue.use(VueI18n)
 
-const fallbackLocale = 'en'
+const fallbackLocales = {
+  rm: ['de'],
+  default: 'en',
+}
+
 const i18n = new VueI18n({
   locale: 'de',
-  fallbackLocale,
+  fallbackLocale: fallbackLocales,
   messages: deepmerge.all([
     // vee-validate locales
     {
@@ -81,6 +87,8 @@ const i18n = new VueI18n({
       'en-CH-scout': enCHScoutCommon,
       de: deCommon,
       'de-CH-scout': deCHScoutCommon,
+      rm: rmCommon,
+      'rm-CH-scout': rmCHScoutCommon,
     },
 
     // eCamp frontend only locales
@@ -113,10 +121,10 @@ Object.defineProperty(i18n, 'browserPreferredLocale', {
         return languageFallback
       }
     }
-    return fallbackLocale
+    return fallbackLocales.default
   },
 })
 
 export default i18n
 
-export { i18n, fallbackLocale }
+export { i18n, fallbackLocales }
