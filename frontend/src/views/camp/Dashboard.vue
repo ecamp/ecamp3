c6e9dc8082a6795ec4d5ccd45ab0a0cb0023e295
BacLuc <lucius.bachmann@gmx.ch>
2023-11-20 14:45:28 +0100

    frontend/api: add camp filter on days for Dashboard.vue
    That it's possible to load all days of a camp in one request.

--- frontend/src/views/camp/Dashboard.vue
+++ frontend/src/views/camp/Dashboard.vue
@@ -404,12 +404,7 @@ export default {
 
     const { categories, periods, collaborators, progressLabels } =
       await loadAndProcessCollections(this.camp())
-    await Promise.all(
-      this.camp()
-        .periods()
-        .items.map((period) => period._meta.self)
-        .map((periodHref) => this.api.get().days({ period: periodHref }))
-    )
+    await this.api.get().days({ 'period.camp': this.camp()._meta.self })
 
     const queryFilters = processRouteQuery(this.$route.query)
     const { period, responsible, category, progressLabel } = {
