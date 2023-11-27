6b0d9773a289e2ae4b801f3bfef3ac015c23e20f
Manuel Meister <news.manuelsworld@gmail.com>
2023-11-27 12:08:18 +0100

    Improve ux of camp create

---
+++ frontend/src/components/buttons/ButtonContinue.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn color="primary" v-bind="$attrs" v-on="$listeners">
+    {{ $tc('global.button.continue') }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'ButtonContinue',
+}
+</script>
---
+++ frontend/src/components/layout/ContentActions.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-card-actions>
+    <div class="d-flex gap-2 flex-wrap flex-grow-1">
+      <slot />
+    </div>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: 'ContentActions',
+}
+</script>
