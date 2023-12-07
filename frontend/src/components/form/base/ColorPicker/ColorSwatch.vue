447be7299aef8d58f902bf96252541442191e4fa
Manuel Meister <news.manuelsworld@gmail.com>
2023-12-07 16:57:29 +0100

    Add color swatches to colorpicker
    The colors should be relatively colorblind safe.

---
+++ frontend/src/components/form/base/ColorPicker/ColorSwatch.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-btn
+    class="e-colorswatch"
+    fab
+    elevation="0"
+    width="30"
+    height="30"
+    :color="color"
+    @click="picker.onInput(color)"
+  ></v-btn>
+</template>
+<script>
+import { contrastColor } from '@/common/helpers/colors.js'
+
+export default {
+  name: 'ColorSwatch',
+  props: {
+    picker: { type: Object, required: true },
+    color: { type: String, required: true },
+  },
+  computed: {
+    contrast() {
+      return this.color ? contrastColor(this.color) : 'black'
+    },
+  },
+}
+</script>
+<style scoped>
+.e-colorswatch::after {
+  content: '•';
+  color: v-bind(contrast);
+  display: block;
+  width: 100%;
+  height: 100%;
+  line-height: 26px;
+  font-size: 28px;
+  text-align: center;
+}
+</style>
--- frontend/src/components/form/base/EColorPicker.vue
+++ frontend/src/components/form/base/EColorPicker.vue
@@ -16,15 +16,30 @@ Displays a field as a color picker (can be used with v-model)
     @input="$emit('input', $event)"
   >
     <template #default="picker">
-      <v-card>
+      <v-card :style="{ '--picker-contrast-color': contrast }" data-testid="colorpicker">
         <v-color-picker
           :value="removeAlpha(picker.value || '')"
           flat
           @input="picker.onInput"
         />
-        <v-btn text color="primary" @click="picker.close">
-          {{ $tc('global.button.close') }}
-        </v-btn>
+        <v-divider />
+        <div class="d-flex gap-2 pa-4 flex-wrap">
+          <ColorSwatch color="#90B7E4" :picker="picker" />
+          <ColorSwatch color="#6EDBE9" :picker="picker" />
+          <ColorSwatch color="#4dbb52" :picker="picker" />
+          <ColorSwatch color="#FF9800" :picker="picker" />
+          <ColorSwatch color="#FD7A7A" :picker="picker" />
+          <ColorSwatch color="#d584e9" :picker="picker" />
+          <ColorSwatch color="#BBBBBB" :picker="picker" />
+
+          <ColorSwatch color="#1964B1" :picker="picker" />
+          <ColorSwatch color="#1E86CA" :picker="picker" />
+          <ColorSwatch color="#3DB842" :picker="picker" />
+          <ColorSwatch color="#F1810D" :picker="picker" />
+          <ColorSwatch color="#C71A1A" :picker="picker" />
+          <ColorSwatch color="#CF3BD6" :picker="picker" />
+          <ColorSwatch color="#575757" :picker="picker" />
+        </div>
       </v-card>
     </template>
 
@@ -38,13 +53,21 @@ Displays a field as a color picker (can be used with v-model)
 <script>
 import BasePicker from './BasePicker.vue'
 import { formComponentMixin } from '@/mixins/formComponentMixin.js'
+import { contrastColor } from '@/common/helpers/colors.js'
+import ColorSwatch from '@/components/form/base/ColorPicker/ColorSwatch.vue'
 
 export default {
   name: 'EColorPicker',
-  components: { BasePicker },
+  components: { ColorSwatch, BasePicker },
   mixins: [formComponentMixin],
   props: {
     value: { type: String, required: true },
+  },
+  computed: {
+    contrast() {
+      // Vuetify returns invalid value #NANNAN in the initialization phase
+      return this.value && this.value !== '#NANNAN' ? contrastColor(this.value) : 'black'
+    },
   },
   methods: {
     /**
@@ -71,4 +94,14 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+:deep(.v-color-picker__dot > div::before) {
+  content: '•';
+  color: var(--picker-contrast-color);
+  display: block;
+  width: 100%;
+  line-height: 26px;
+  font-size: 28px;
+  text-align: center;
+}
+</style>
