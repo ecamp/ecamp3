c5f654b67070eb4d6de39465680ef7fec141d27b
BacLuc <lucius.bachmann@gmx.ch>
2023-12-24 17:32:37 +0100

    vCalendarDragAndDrop.spec.js: make other tests more descriptive

--- frontend/src/helpers/__tests__/vCalendarDragAndDrop.spec.js
+++ frontend/src/helpers/__tests__/vCalendarDragAndDrop.spec.js
@@ -33,22 +33,50 @@ describe('toTime', () => {
 
 describe('roundTimeToNearestQuarterHour', () => {
   it.each([
-    [1649190120000, 1649189700000],
-    [1683447600000, 1683447300000],
-    [1683543980000, 1683543600000],
-    [1683547980999, 1683548100000],
-  ])('maps %p to %p', (input, expected) => {
-    expect(roundTimeToNearestQuarterHour(input)).toEqual(expected)
+    ['22:22:00', '22:15'],
+    ['10:20:00', '10:15'],
+    ['13:00:20', '13:00'],
+    ['14:13:00', '14:15'],
+  ])('maps %s to %s', (input, expected) => {
+    const epochMillis = asEpochMillis(input)
+    const roundedEpoch = roundTimeToNearestQuarterHour(epochMillis)
+    expect(fromEpochMillis(roundedEpoch)).toEqual(expected)
   })
 })
 
 describe('roundTimeUpToNextQuarterHour', () => {
   it.each([
-    [1649190120000, 1649190600000],
-    [1683447600000, 1683448200000],
-    [1683543980000, 1683544500000],
-    [1683547980999, 1683548100000],
-  ])('maps %p to %p', (input, expected) => {
-    expect(roundTimeUpToNextQuarterHour(input)).toEqual(expected)
+    ['22:22:00', '22:30'],
+    ['10:20:00', '10:30'],
+    ['13:00:20', '13:15'],
+    ['14:13:00', '14:15'],
+  ])('maps %s to %s', (input, expected) => {
+    const epochMillis = asEpochMillis(input)
+    const roundedEpoch = roundTimeUpToNextQuarterHour(epochMillis)
+    expect(fromEpochMillis(roundedEpoch)).toEqual(expected)
   })
 })
+
+/**
+ * Converts a time string (HH:mm:ss) in local timezone into epoch millis
+ *
+ * @param timeStr {string} HH:mm:ss
+ * @returns {number} epoch millis
+ */
+function asEpochMillis(timeStr) {
+  return dayjs('2023-12-24')
+    .hour(Number(timeStr.slice(0, 2)))
+    .minute(Number(timeStr.slice(3, 5)))
+    .second(Number(timeStr.slice(6, 8)))
+    .valueOf()
+}
+
+/**
+ * Converts a given epoch millisecond value to a formatted time string in the format 'HH:mm'.
+ *
+ * @param {number} epochMillis - The epoch millisecond value to be converted.
+ * @return {string} The formatted time string in the format 'HH:mm'.
+ */
+function fromEpochMillis(epochMillis) {
+  return dayjs(epochMillis).year(2023).month(12).date(24).format('HH:mm')
+}
