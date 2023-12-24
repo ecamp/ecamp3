59d6ec24d18b3ea72d84e4bb2fd4515f16b53f4a
BacLuc <lucius.bachmann@gmx.ch>
2023-12-24 17:03:44 +0100

    vCalendarDragAndDrop.spec.js: make timezone independent
    Also use the vCalendar parseDate function to create the CalendarTimestamp object instead of doing it ourselves.
    Now we don't have to run the frontend tests in a specific timezone anymore,
    and can later run the tests in 2 timezones to be sure that we handle timezones correctly.

--- frontend/src/helpers/__tests__/vCalendarDragAndDrop.spec.js
+++ frontend/src/helpers/__tests__/vCalendarDragAndDrop.spec.js
@@ -3,66 +3,30 @@ import {
   roundTimeToNearestQuarterHour,
   roundTimeUpToNextQuarterHour,
 } from '@/helpers/vCalendarDragAndDrop.js'
+import dayjs from '@/common/helpers/dayjs.js'
+import { parseDate } from 'vuetify/src/components/VCalendar/util/timestamp'
 
 describe('toTime', () => {
-  it.each([
-    [
-      {
-        date: '2023-05-08',
-        time: '14:13',
-        year: 2023,
-        month: 5,
-        day: 8,
-        weekday: 1,
-        hour: 14,
-        minute: 13,
-        hasDay: true,
-        hasTime: true,
-        past: false,
-        present: false,
-        future: true,
-      },
-      1683508380000,
-    ],
-    [
-      {
-        date: '2023-05-07',
-        time: '10:20',
-        year: 2023,
-        month: 5,
-        day: 7,
-        weekday: 0,
-        hour: 10,
-        minute: 20,
-        hasDay: true,
-        hasTime: true,
-        past: false,
-        present: false,
-        future: true,
-      },
-      1683408000000,
-    ],
-    [
-      {
-        date: '2022-04-05',
-        time: '22:22',
-        year: 2022,
-        month: 4,
-        day: 5,
-        weekday: 5,
-        hour: 22,
-        minute: 22,
-        hasDay: true,
-        hasTime: true,
-        past: false,
-        present: false,
-        future: true,
-      },
-      1649150520000,
-    ],
-  ])('maps %p to %p', (input, expected) => {
-    expect(toTime(input)).toEqual(expected)
-  })
+  const dateTimesToTest = [
+    dayjs('2023-05-08T14:13'),
+    dayjs('2023-05-07T10:20'),
+    dayjs('2023-04-05T22:22'),
+  ]
+
+  const parametersToTest = dateTimesToTest.map((dateTime) => ({
+    calendarTimestamp: {
+      ...parseDate(dateTime.toDate()),
+      json: JSON.stringify(parseDate(dateTime.toDate())),
+    },
+    epochMillis: dateTime.valueOf(),
+  }))
+
+  it.each(parametersToTest)(
+    'maps $calendarTimestamp.json to $epochMillis',
+    ({ calendarTimestamp, _, epochMillis }) => {
+      expect(toTime(calendarTimestamp)).toEqual(epochMillis)
+    }
+  )
 })
 
 describe('roundTimeToNearestQuarterHour', () => {
