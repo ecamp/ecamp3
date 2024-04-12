1f2ceadbf1042547dfc5062828d3cd47ec977d51
Pirmin Mattmann <pimattmann@gmail.com>
2024-04-13 01:56:49 +0200

    filterByContentNode move Joins to where-clause

--- api/src/Repository/FiltersByContentNode.php
+++ api/src/Repository/FiltersByContentNode.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Activity;
 use App\Entity\Category;
+use App\Entity\ContentNode;
 use App\Entity\User;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
@@ -20,20 +21,24 @@ trait FiltersByContentNode {
      * the alias of the contentNode as the third argument.
      */
     protected function filterByContentNode(QueryBuilder $queryBuilder, User $user, string $contentNodeAlias): void {
-        $queryBuilder->innerJoin("{$contentNodeAlias}.root", 'root');
+        $rootQry = $queryBuilder->getEntityManager()->createQueryBuilder();
+        $rootQry->from(ContentNode::class, 'cn')->select('cn');
 
         // assuming owner is an Activity
-        $queryBuilder->leftJoin(Activity::class, 'cn_activity', Join::WITH, 'cn_activity.rootContentNode = root.id');
+        $rootQry->leftJoin(Activity::class, 'cn_activity', Join::WITH, 'cn_activity.rootContentNode = cn.id');
 
         /*
          *   If owner is an Activity --> cn_activity.category is not null
          *   If owner is a Category --> cn_category.rootContentNode = root.id is a match
          */
-        $queryBuilder->join(Category::class, 'cn_category', Join::WITH, 'cn_category = cn_activity.category OR cn_category.rootContentNode = root');
+        $rootQry->join(Category::class, 'cn_category', Join::WITH, 'cn_category = cn_activity.category OR cn_category.rootContentNode = cn');
 
         // load owning camp via category
-        $queryBuilder->join('cn_category.camp', 'cn_camp');
+        $rootQry->join('cn_category.camp', 'cn_camp');
 
-        $this->filterByCampCollaboration($queryBuilder, $user, 'cn_camp');
+        $this->filterByCampCollaboration($rootQry, $user, 'cn_camp');
+
+        $queryBuilder->andWhere($queryBuilder->expr()->in("{$contentNodeAlias}.root", $rootQry->getDQL()));
+        $this->queryBuilderAddParameters($queryBuilder, $rootQry);
     }
 }
