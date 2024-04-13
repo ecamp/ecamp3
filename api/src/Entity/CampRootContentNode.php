e0d19e08c923f2e434f7c5c0cecafce223c6f68b
Pirmin Mattmann <pimattmann@gmail.com>
2024-04-13 17:49:33 +0200

    fix unittest

--- api/src/Entity/CampRootContentNode.php
+++ api/src/Entity/CampRootContentNode.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * view_camp_root_content_nodes 
+ * view_camp_root_content_nodes
  * For a ContentNode (only root nodes) list the camp to which it belongs.
  */
 #[ORM\Entity(readOnly: true)]
