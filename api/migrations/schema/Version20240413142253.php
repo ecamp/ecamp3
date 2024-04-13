e9290d0b075258c129d7e0f676c0b749356111b1
Pirmin Mattmann <pimattmann@gmail.com>
2024-04-13 16:54:34 +0200

    remove comment

--- api/migrations/schema/Version20240413142253.php
+++ api/migrations/schema/Version20240413142253.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20240413142253 extends AbstractMigration {
     public function getDescription(): string {
         return 'Add View view_camp_root_content_nodes to fast resolve contentNode-Owner';
