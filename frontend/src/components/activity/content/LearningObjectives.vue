59ee4fc6f27a51a65a5d84ec2b270fb04b00bc9a
Carlo Beltrame <carlo@beltra.me>
2024-04-16 02:49:31 +0200

    Add new content types

---
+++ frontend/src/components/activity/content/LearningObjectives.vue
@@ -0,0 +1,29 @@
+<template>
+  <ContentNodeCard v-bind="$props">
+    <api-richtext
+      :uri="contentNode._meta.self"
+      path="data.html"
+      :placeholder="$tc('contentNode.learningObjectives.name')"
+      rows="2"
+      :disabled="layoutMode || disabled"
+      class="grow-v-slot"
+    />
+  </ContentNodeCard>
+</template>
+
+<script>
+import ApiRichtext from '@/components/form/api/ApiRichtext.vue'
+import ContentNodeCard from '@/components/activity/content/layout/ContentNodeCard.vue'
+import { contentNodeMixin } from '@/mixins/contentNodeMixin.js'
+
+export default {
+  name: 'LearningObjectives',
+  components: {
+    ContentNodeCard,
+    ApiRichtext,
+  },
+  mixins: [contentNodeMixin],
+}
+</script>
+
+<style scoped></style>
---
+++ pdf/src/campPrint/scheduleEntry/contentNode/LearningObjectives.vue
@@ -0,0 +1,24 @@
+<template>
+  <View class="content-node">
+    <InstanceName :content-node="contentNode" />
+    <View style="line-height: 1.4">
+      <RichText :rich-text="contentNode.data.html" />
+    </View>
+  </View>
+</template>
+<script>
+import PdfComponent from '@/PdfComponent.js'
+import InstanceName from '../InstanceName.vue'
+import RichText from '../../RichText.vue'
+
+export default {
+  name: 'LearningObjectives',
+  components: { RichText, InstanceName },
+  extends: PdfComponent,
+  props: {
+    contentNode: { type: Object, required: true },
+  },
+}
+</script>
+<pdf-style>
+</pdf-style>
