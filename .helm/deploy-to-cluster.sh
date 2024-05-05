0aa39764e037a68f50d634e810565f88c36a2fd6
Urban Suppiger <urban@suppiger.net>
2024-05-05 14:14:29 +0200

    fix manual deployment

--- .helm/deploy-to-cluster.sh
+++ .helm/deploy-to-cluster.sh
@@ -46,10 +46,10 @@ for i in 1; do
   values="$values --set postgresql.url=$POSTGRES_URL/ecamp3$instance_name-"$i"?sslmode=require"
   values="$values --set postgresql.adminUrl=$POSTGRES_ADMIN_URL/ecamp3$instance_name-"$i"?sslmode=require"
   values="$values --set postgresql.dropDBOnUninstall=true"
-  values="$values --set php.dataMigrationsDir=$migrations_dir"
-  values="$values --set php.appSecret=$app_secret"
+  values="$values --set api.dataMigrationsDir=$migrations_dir"
+  values="$values --set api.appSecret=$app_secret"
   if [ -n "$API_SENTRY_DSN" ]; then
-    values="$values --set php.sentryDsn=$API_SENTRY_DSN"
+    values="$values --set api.sentryDsn=$API_SENTRY_DSN"
   fi
   if [ -n "$FRONTEND_SENTRY_DSN" ]; then
     values="$values --set frontend.sentryDsn=$FRONTEND_SENTRY_DSN"
@@ -57,9 +57,9 @@ for i in 1; do
   if [ -n "$PRINT_SENTRY_DSN" ]; then
     values="$values --set print.sentryDsn=$PRINT_SENTRY_DSN"
   fi
-  values="$values --set php.jwt.passphrase=$app_jwt_passphrase"
-  values="$values --set-file php.jwt.publicKey=$SCRIPT_DIR/public.pem"
-  values="$values --set-file php.jwt.privateKey=$SCRIPT_DIR/private.pem"
+  values="$values --set api.jwt.passphrase=$app_jwt_passphrase"
+  values="$values --set-file api.jwt.publicKey=$SCRIPT_DIR/public.pem"
+  values="$values --set-file api.jwt.privateKey=$SCRIPT_DIR/private.pem"
   values="$values --set deploymentTime=$(date -u +%s)"
   values="$values --set deployedVersion=\"$(git rev-parse --short HEAD)\""
   values="$values --set featureToggle.developer=true"
