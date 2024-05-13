a05fdec3237169dcf8c7003a77bc64921b9ca643
Urban Suppiger <urban@suppiger.net>
2024-05-13 20:27:41 +0200

    revert changes to nuxtPrint.cy.js

--- e2e/specs/nuxtPrint.cy.js
+++ e2e/specs/nuxtPrint.cy.js
@@ -6,35 +6,34 @@ describe('Nuxt print test', () => {
   it('shows print preview', () => {
     cy.login('test@example.com')
 
-    cy.request(Cypress.env('API_ROOT_URL') + '/camps/3c79b99ab424.jsonhal').then(
-      (response) => {
-        const camp = response.body
-        const campUri = camp._links.self.href
+    cy.request(Cypress.env('API_ROOT_URL') + '/camps.jsonhal').then((response) => {
+      const body = response.body
+      const campUri = body._links.items[1].href
+      const camp = body._embedded.items[1]
 
-        const printConfig = {
-          language: 'en',
-          documentName: 'camp',
-          camp: campUri,
-          contents: [
-            {
-              type: 'Cover',
-              options: {},
-            },
-          ],
-        }
-
-        cy.visit(
-          Cypress.env('PRINT_URL') +
-            '/?config=' +
-            encodeURIComponent(JSON.stringify(printConfig))
-        )
-        cy.contains(camp.name)
-        cy.contains(camp.title)
-        cy.contains(camp.motto)
-
-        cy.get('#content_0_cover').should('have.css', 'font-size', '50px') // this ensures Tailwind is properly built and integrated
+      const printConfig = {
+        language: 'en',
+        documentName: 'camp',
+        camp: campUri,
+        contents: [
+          {
+            type: 'Cover',
+            options: {},
+          },
+        ],
       }
-    )
+
+      cy.visit(
+        Cypress.env('PRINT_URL') +
+          '/?config=' +
+          encodeURIComponent(JSON.stringify(printConfig))
+      )
+      cy.contains(camp.name)
+      cy.contains(camp.title)
+      cy.contains(camp.motto)
+
+      cy.get('#content_0_cover').should('have.css', 'font-size', '50px') // this ensures Tailwind is properly built and integrated
+    })
   })
 
   it('downloads PDF', () => {
