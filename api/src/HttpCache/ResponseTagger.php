c04ebb75051ad38f842ee9e4cf8181b62bcd3543
Urban Suppiger <urban@suppiger.net>
2024-05-13 20:45:54 +0200

    use parameter for path match regex

--- api/src/HttpCache/ResponseTagger.php
+++ api/src/HttpCache/ResponseTagger.php
@@ -8,15 +8,13 @@ use FOS\HttpCacheBundle\Http\SymfonyResponseTagger;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
- * Wrapper around SymfonyResponseTagger which only adds tags for specific URIs.
+ * Wrapper around SymfonyResponseTagger which only adds tags for specific URIs, which match the regex $matchPath.
  *
  * @author Urban Suppiger <urban@suppiger.net>
  */
 class ResponseTagger {
-    // only add tags for request URI matching below regex
-    private const PATH_REGEX = '^/(content_types|camps/[0-9a-f]*/categories)';
-
     public function __construct(
+        private string $matchPath,
         private SymfonyResponseTagger $responseTagger,
         private RequestStack $requestStack
     ) {}
@@ -43,6 +41,6 @@ class ResponseTagger {
 
         $requestUri = $request->getRequestUri();
 
-        return (bool) preg_match('{'.self::PATH_REGEX.'}', $requestUri);
+        return (bool) preg_match('{'.$this->matchPath.'}', $requestUri);
     }
 }
