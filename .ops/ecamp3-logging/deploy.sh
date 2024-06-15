5da94915e6e77a1ff328729a1454f904503609a6
BacLuc <lucius.bachmann@gmx.ch>
2024-06-15 13:52:13 +0200

    ecamp3-logging: split deploy command onto multiple lines
    That it is more readable

--- .ops/ecamp3-logging/deploy.sh
+++ .ops/ecamp3-logging/deploy.sh
@@ -5,4 +5,9 @@ set -e
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 cd $SCRIPT_DIR
 # to debug: --dry-run --debug
-helm dep build && helm upgrade --install ecamp3-logging --namespace=ecamp3-logging --create-namespace $SCRIPT_DIR
+helm dep build
+helm upgrade --install ecamp3-logging \
+    --namespace=ecamp3-logging \
+    --create-namespace \
+    $SCRIPT_DIR \ 
+    --values $SCRIPT_DIR/values.yaml
