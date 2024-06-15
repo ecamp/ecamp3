6a750942586011ef3712cb2051f2d624ad4d90ff
BacLuc <lucius.bachmann@gmx.ch>
2024-06-15 13:52:50 +0200

    ecamp3-logging: allow to set different values for prod

--- .ops/ecamp3-logging/deploy.sh
+++ .ops/ecamp3-logging/deploy.sh
@@ -4,10 +4,18 @@ set -e
 
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 cd $SCRIPT_DIR
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <prod|dev>"
+    echo "(or other environments)"
+    exit 1
+fi
+
 # to debug: --dry-run --debug
 helm dep build
 helm upgrade --install ecamp3-logging \
     --namespace=ecamp3-logging \
     --create-namespace \
-    $SCRIPT_DIR \ 
-    --values $SCRIPT_DIR/values.yaml
+    $SCRIPT_DIR \
+    --values $SCRIPT_DIR/values.yaml \
+    --values $SCRIPT_DIR/values-$1.yaml
