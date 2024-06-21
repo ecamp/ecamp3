3b67e15e4c04eb817eff9d469e17d0732c604e49
Pirmin Mattmann <pimattmann@gmail.com>
2024-06-21 22:09:27 +0200

    UnitTest: ChecklistNode add/remove ChecklistItem

--- api/tests/Api/ContentNodes/ChecklistNode/UpdateChecklistNodeTest.php
+++ api/tests/Api/ContentNodes/ChecklistNode/UpdateChecklistNodeTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Api\ContentNodes\ChecklistNode;
 
+use App\Entity\ContentNode\ChecklistNode;
 use App\Tests\Api\ContentNodes\UpdateContentNodeTestCase;
 
 /**
@@ -13,5 +14,93 @@ class UpdateChecklistNodeTest extends UpdateContentNodeTestCase {
 
         $this->endpoint = '/content_node/checklist_nodes';
         $this->defaultEntity = static::getFixture('checklistNode1');
+    }
+
+    public function testAddChecklistItemIsDeniedForGuest() {
+        $checklistItemId = static::getFixture('checklistItem1_1_2')->getId();
+        static::createClientWithCredentials(['email' => static::getFixture('user3guest')->getEmail()])
+            ->request('PATCH', $this->endpoint.'/'.$this->defaultEntity->getId(), ['json' => [
+                'addChecklistItemIds' => [$checklistItemId],
+            ], 'headers' => ['Content-Type' => 'application/merge-patch+json']])
+        ;
+        $this->assertResponseStatusCodeSame(403);
+        $this->assertJsonContains([
+            'title' => 'An error occurred',
+            'detail' => 'Access Denied.',
+        ]);
+    }
+
+    public function testAddChecklistItemIsDeniedForMember() {
+        $checklistItemId = static::getFixture('checklistItem1_1_2')->getId();
+        static::createClientWithCredentials(['email' => static::getFixture('user2member')->getEmail()])
+            ->request('PATCH', $this->endpoint.'/'.$this->defaultEntity->getId(), ['json' => [
+                'addChecklistItemIds' => [$checklistItemId],
+            ], 'headers' => ['Content-Type' => 'application/merge-patch+json']])
+        ;
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertJsonContains([
+            '_links' => [
+                'checklistItems' => [
+                    1 => [
+                        'href' => '/checklist_items/'.$checklistItemId,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function testAddChecklistItemIsDeniedForManager() {
+        $checklistItemId = static::getFixture('checklistItem1_1_2')->getId();
+        static::createClientWithCredentials()->request('PATCH', $this->endpoint.'/'.$this->defaultEntity->getId(), ['json' => [
+            'addChecklistItemIds' => [$checklistItemId],
+        ], 'headers' => ['Content-Type' => 'application/merge-patch+json']]);
+
+        $this->assertResponseStatusCodeSame(200);
+        $this->assertJsonContains([
+            '_links' => [
+                'checklistItems' => [
+                    1 => [
+                        'href' => '/checklist_items/'.$checklistItemId,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function testRemoveChecklistItemIsDeniedForGuest() {
+        $checklistItemId = static::getFixture('checklistItem1_1_1')->getId();
+        static::createClientWithCredentials(['email' => static::getFixture('user3guest')->getEmail()])
+            ->request('PATCH', $this->endpoint.'/'.$this->defaultEntity->getId(), ['json' => [
+                'removeChecklistItemIds' => [$checklistItemId],
+            ], 'headers' => ['Content-Type' => 'application/merge-patch+json']])
+        ;
+        $this->assertResponseStatusCodeSame(403);
+        $this->assertJsonContains([
+            'title' => 'An error occurred',
+            'detail' => 'Access Denied.',
+        ]);
+    }
+
+    public function testRemoveChecklistItemIsDeniedForMember() {
+        $checklistItem = static::getFixture('checklistItem1_1_1');
+        static::createClientWithCredentials(['email' => static::getFixture('user2member')->getEmail()])
+            ->request('PATCH', $this->endpoint.'/'.$this->defaultEntity->getId(), ['json' => [
+                'removeChecklistItemIds' => [$checklistItem->getId()],
+            ], 'headers' => ['Content-Type' => 'application/merge-patch+json']])
+        ;
+        $this->assertResponseStatusCodeSame(200);
+        $checklistNode = $this->getEntityManager()->getRepository(ChecklistNode::class)->find($this->defaultEntity->getId());
+        $this->assertFalse(in_array($checklistItem, $checklistNode->getChecklistItems()));
+    }
+
+    public function testRemoveChecklistItemIsDeniedForManager() {
+        $checklistItem = static::getFixture('checklistItem1_1_1');
+        static::createClientWithCredentials()->request('PATCH', $this->endpoint.'/'.$this->defaultEntity->getId(), ['json' => [
+            'removeChecklistItemIds' => [$checklistItem->getId()],
+        ], 'headers' => ['Content-Type' => 'application/merge-patch+json']]);
+
+        $this->assertResponseStatusCodeSame(200);
+        $checklistNode = $this->getEntityManager()->getRepository(ChecklistNode::class)->find($this->defaultEntity->getId());
+        $this->assertFalse(in_array($checklistItem, $checklistNode->getChecklistItems()));
     }
 }
