cc0632fe693650d26e37121cd5c4483da4d08839
BacLuc <lucius.bachmann@gmx.ch>
2024-07-18 22:19:04 +0200

    e2e: wait for backend to be ready before running tests
    This may help when retrying tests.
    The backend has a while to restart, thus
    we need to wait with the retry until the backend is back up.

--- e2e/support/index.js
+++ e2e/support/index.js
@@ -16,5 +16,31 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+function apiAvailable(retries = 0) {
+  if (retries > 10) {
+    throw new Error('Request failed')
+  }
+
+  cy.log('executing request')
+
+  cy.request('OPTIONS', Cypress.env('API_ROOT_URL'), {
+    headers: {
+      Origin: 'http://localhost:3000',
+      'Access-Control-Request-Method': 'GET',
+      'Access-Control-Request-Headers': 'Origin, Content-Type, Accept, Authorization',
+    },
+  }).then((resp) => {
+    if (resp.status === 200) {
+      return
+    }
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(10000).then(() => apiAvailable())
+  })
+}
+
+beforeEach(() => {
+  apiAvailable()
+})
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
