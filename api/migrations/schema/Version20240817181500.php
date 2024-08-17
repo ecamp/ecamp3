22aac191ea5cd6046f175bb22fe69cf9b54d62c8
Manuel Meister <news.manuelsworld@gmail.com>
2024-08-17 18:26:03 +0200

    Fix migration use name instead of entityclass

--- api/migrations/schema/Version20240817181500.php
+++ api/migrations/schema/Version20240817181500.php
@@ -10,18 +10,18 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240806120000 extends AbstractMigration {
+final class Version20240817181500 extends AbstractMigration {
     public function getDescription(): string {
         return 'Renames safety concept to safety considerations';
     }
 
     public function up(Schema $schema): void {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('UPDATE public.content_type SET entityclass=\'SafetyConsiderations\' WHERE entityclass=\'SafetyConcept\';');
+        $this->addSql("UPDATE content_type SET name = 'SafetyConsiderations' WHERE name = 'SafetyConcept';");
     }
 
     public function down(Schema $schema): void {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('UPDATE public.content_type SET entityclass=\'SafetyConcept\' WHERE entityclass=\'SafetyConsiderations\';');
+        $this->addSql("UPDATE content_type SET name = 'SafetyConcept' WHERE name = 'SafetyConsiderations';");
     }
 }
