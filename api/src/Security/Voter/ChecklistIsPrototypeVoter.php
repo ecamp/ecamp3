61c268ff6ae55b169c5648255928e4281602a9cb
Urban Suppiger <urban@suppiger.net>
2024-09-28 17:30:17 +0200

    fix phpstan

--- api/src/Security/Voter/ChecklistIsPrototypeVoter.php
+++ api/src/Security/Voter/ChecklistIsPrototypeVoter.php
@@ -8,7 +8,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 /**
- * @extends Voter<string,Checklist>
+ * @extends Voter<string,Checklist|ChecklistItem>
  */
 class ChecklistIsPrototypeVoter extends Voter {
     public function __construct() {}
