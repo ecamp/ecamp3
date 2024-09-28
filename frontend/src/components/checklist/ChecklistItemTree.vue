7ad4b27ce37f88487ec42c706e3a9d82e716184f
Pirmin Mattmann <pimattmann@gmail.com>
2024-09-29 00:28:52 +0200

    checklist-overview function-complete

--- frontend/src/components/checklist/ChecklistItemTree.vue
+++ frontend/src/components/checklist/ChecklistItemTree.vue
@@ -1,7 +1,7 @@
 <template>
-  <table style="width: 100%">
+  <table style="width: 100%; border-spacing: 0" :style="getRowStyle()">
     <tr>
-      <td style="width: 30px">({{ checklistItem.position + 1 }})</td>
+      <td style="width: 30px; padding: 2px">({{ checklistItem.position + 1 }})</td>
       <td style="text-wrap: wrap">
         {{ checklistItem.text }}
       </td>
@@ -11,15 +11,17 @@
           :key="activity._meta.self"
           style="display: inline"
         >
-          <div
-            v-for="scheduleEntry in activity.scheduleEntries().items"
-            :key="scheduleEntry._meta.self"
-            style="display: inline"
-          >
-            ({{ scheduleEntry.number }})
-          </div>
-          {{ activity.title }}
+          <ScheduleEntryLinks :activity-promise="activity._meta.load" />
         </div>
+      </td>
+      <td style="width: 30px">
+        <v-btn
+          v-if="getActivities(checklistItem).length > 0"
+          icon
+          @click="copyToClipboard(checklistItem)"
+        >
+          <v-icon>mdi-content-copy</v-icon>
+        </v-btn>
       </td>
     </tr>
     <tr
@@ -27,7 +29,7 @@
       :key="subItem._meta.self"
     >
       <td style="width: 30px"></td>
-      <td colspan="2">
+      <td colspan="4">
         <ChecklistItemTree :checklist-item="subItem" />
       </td>
     </tr>
@@ -36,10 +38,13 @@
 
 <script>
 import { sortBy } from 'lodash'
+import ScheduleEntryLinks from '../material/ScheduleEntryLinks.vue'
 
 export default {
   name: 'ChecklistItemTree',
-
+  components: {
+    ScheduleEntryLinks,
+  },
   props: {
     checklistItem: { type: Object, required: true },
   },
@@ -54,7 +59,30 @@ export default {
         checklistNodes.some((cn) => cn.root().id == a.rootContentNode().id)
       )
     },
+    getRowStyle() {
+      return this.checklistItem.position % 2 == 0
+        ? { backgroundColor: 'rgba(0, 0, 0, 0.1)' }
+        : { backgroundColor: 'rgba(0, 0, 0, 0.2)' }
+    },
+    copyToClipboard(checklistItem) {
+      const activities = this.getActivities(checklistItem)
+
+      console.log(activities)
+      console.log(activities.map((a) => a.scheduleEntries().items))
+
+      const scheduleEntries = activities
+        .map((a) => a.scheduleEntries().items)
+        .reduce(function (items, item) {
+          return items.concat(item)
+        }, [])
+        .map((s) => s.number)
+        .join(', ')
+
+      navigator.clipboard.writeText(scheduleEntries)
+    },
     sortBy,
   },
 }
 </script>
+
+<style scoped></style>
