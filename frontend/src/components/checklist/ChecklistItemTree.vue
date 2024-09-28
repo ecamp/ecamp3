43c203fe778ca0246b69788822faa5d40256326f
Pirmin Mattmann <pimattmann@gmail.com>
2024-09-28 19:39:06 +0200

    lint

--- frontend/src/components/checklist/ChecklistItemTree.vue
+++ frontend/src/components/checklist/ChecklistItemTree.vue
@@ -1,41 +1,37 @@
 <template>
-    <table style="width: 100%;">
-        <tr>
-            <td style="width: 30px;">
-                ({{ checklistItem.position + 1 }})
-            </td>
-            <td style="text-wrap: wrap;">
-                {{ checklistItem.text }}
-            </td>
-            <td style="width: 400px">
-                <div
-                    v-for="activity in this.getActivities(checklistItem)"
-                    :key="activity._meta.self"
-                    style="display: inline"
-                >
-                    <div
-                        v-for="scheduleEntry in activity.scheduleEntries().items"
-                        :key="scheduleEntry._meta.self"
-                        style="display: inline"
-                    >
-                        ({{ scheduleEntry.number }})
-                    </div>
-                    {{ activity.title }}
-                </div>
-                
-            </td>
-        </tr>
-        <tr
-            v-for="subItem in this.sortBy(checklistItem.children().items, c => c.position)"
-            :key="subItem._meta.self"
+  <table style="width: 100%">
+    <tr>
+      <td style="width: 30px">({{ checklistItem.position + 1 }})</td>
+      <td style="text-wrap: wrap">
+        {{ checklistItem.text }}
+      </td>
+      <td style="width: 400px">
+        <div
+          v-for="activity in getActivities(checklistItem)"
+          :key="activity._meta.self"
+          style="display: inline"
         >
-            <td style="width: 30px;"></td>
-            <td colspan="2">
-                <ChecklistItemTree :checklistItem="subItem" />
-            </td>
-        </tr>
-    </table>
-
+          <div
+            v-for="scheduleEntry in activity.scheduleEntries().items"
+            :key="scheduleEntry._meta.self"
+            style="display: inline"
+          >
+            ({{ scheduleEntry.number }})
+          </div>
+          {{ activity.title }}
+        </div>
+      </td>
+    </tr>
+    <tr
+      v-for="subItem in sortBy(checklistItem.children().items, (c) => c.position)"
+      :key="subItem._meta.self"
+    >
+      <td style="width: 30px"></td>
+      <td colspan="2">
+        <ChecklistItemTree :checklist-item="subItem" />
+      </td>
+    </tr>
+  </table>
 </template>
 
 <script>
@@ -50,14 +46,15 @@ export default {
 
   methods: {
     getActivities(checklistItem) {
-        const camp = checklistItem.checklist().camp()
-        const activities = camp.activities().items
-        const checklistNodes = checklistItem.checklistNodes().items
+      const camp = checklistItem.checklist().camp()
+      const activities = camp.activities().items
+      const checklistNodes = checklistItem.checklistNodes().items
 
-        return activities.filter(a => checklistNodes.some(cn => cn.root().id == a.rootContentNode().id))
+      return activities.filter((a) =>
+        checklistNodes.some((cn) => cn.root().id == a.rootContentNode().id)
+      )
     },
-    sortBy
-  }
+    sortBy,
+  },
 }
-
 </script>
--- frontend/src/views/camp/Checklist.vue
+++ frontend/src/views/camp/Checklist.vue
@@ -5,24 +5,24 @@
     :no-border="$vuetify.breakpoint.mdAndUp"
   >
     <v-card-text>
-        <v-list class="mx-n2 py-0">
-            <v-list-item
-                v-for="checklist in checklists"
-                :key="checklist._meta.self"
-                class="px-2 rounded"
-            >
-                <v-list-item-content>
-                    <v-list-item-title>
-                        <h3>{{ checklist.name }}</h3>
-                        <ChecklistItemTree
-                            v-for="rootChecklistItem in getRootChecklistItems(checklist)"
-                            :key="rootChecklistItem._meta.self"
-                            :checklistItem="rootChecklistItem"
-                        />
-                    </v-list-item-title>
-                </v-list-item-content>
-            </v-list-item>
-        </v-list>
+      <v-list class="mx-n2 py-0">
+        <v-list-item
+          v-for="checklist in checklists"
+          :key="checklist._meta.self"
+          class="px-2 rounded"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              <h3>{{ checklist.name }}</h3>
+              <ChecklistItemTree
+                v-for="rootChecklistItem in getRootChecklistItems(checklist)"
+                :key="rootChecklistItem._meta.self"
+                :checklist-item="rootChecklistItem"
+              />
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-card-text>
   </content-card>
 </template>
@@ -44,7 +44,7 @@ export default {
   computed: {
     checklists() {
       return this.camp.checklists().items
-    }
+    },
   },
   methods: {
     getRootChecklistItems(checklist) {
@@ -52,7 +52,7 @@ export default {
         checklist.checklistItems().items.filter((c) => c.parent == null),
         (c) => c.position
       )
-    }
-  }
+    },
+  },
 }
 </script>
