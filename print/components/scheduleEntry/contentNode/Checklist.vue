2d9f10902610b981647f7bb1a329a7a04ecfa857
Urban Suppiger <urban@suppiger.net>
2024-09-29 02:53:48 +0200

    add checklist numbers and sort according to numbers

--- print/components/scheduleEntry/contentNode/Checklist.vue
+++ print/components/scheduleEntry/contentNode/Checklist.vue
@@ -6,7 +6,7 @@
       <div class="tw-font-bold">{{ entry.checklist.name }}</div>
       <ul class="tw-list-disc tw-list-inside">
         <li v-for="item in entry.items" :key="item.id">
-          {{ item.text }}
+          <span class="tw-mr-1">{{ item.number }}</span> {{ item.text }}
         </li>
       </ul>
     </div>
@@ -14,6 +14,9 @@
 </template>
 
 <script setup>
+import uniqWith from 'lodash/uniqWith.js'
+import sortBy from 'lodash/sortBy.js'
+
 const props = defineProps({
   contentNode: { type: Object, required: true },
 })
@@ -24,12 +27,47 @@ const { error } = await useAsyncData(
     return await props.contentNode.checklistItems().$loadItems()
   }
 )
+
+async function calculateItemNumber(item) {
+  if (!item.parent) {
+    return item.position + 1
+  }
+
+  const parent = await item.parent()._meta.load
+  return (await calculateItemNumber(parent)) + '.' + (item.position + 1)
+}
+
+const items = await Promise.all(
+  props.contentNode.checklistItems().items.map(async (item) => {
+    const number = await calculateItemNumber(item)
+    return {
+      ...item,
+      number,
+    }
+  })
+)
+
+const checklists = uniqWith(
+  props.contentNode
+    .checklistItems()
+    .items.map((checklistItem) => checklistItem.checklist()),
+  function (checklist1, checklist2) {
+    return checklist1._meta.self === checklist2._meta.self
+  }
+)
+
+const checklistsWithItems = checklists.map((checklist) => ({
+  checklist,
+  items: sortBy(
+    items.filter((item) => item.checklist()._meta.self === checklist._meta.self),
+    'number'
+  ),
+}))
 </script>
 
 <script>
 import ContentNodeContent from './ContentNodeContent.vue'
 import { mdiClipboardListOutline } from '@mdi/js'
-import uniqWith from 'lodash/uniqWith.js'
 
 export default {
   components: {
@@ -39,29 +77,6 @@ export default {
     return {
       mdiClipboardListOutline,
     }
-  },
-  computed: {
-    items() {
-      return this.contentNode.checklistItems().items
-    },
-    checklists() {
-      return uniqWith(
-        this.contentNode
-          .checklistItems()
-          .items.map((checklistItem) => checklistItem.checklist()),
-        function (checklist1, checklist2) {
-          return checklist1._meta.self === checklist2._meta.self
-        }
-      )
-    },
-    checklistsWithItems() {
-      return this.checklists.map((checklist) => ({
-        checklist,
-        items: this.items.filter(
-          (item) => item.checklist()._meta.self === checklist._meta.self
-        ),
-      }))
-    },
   },
 }
 </script>
