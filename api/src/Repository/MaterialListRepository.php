655ef1b8e4b4e7126f5cca75189f80233936d09f
carlobeltrame <carlo@beltra.me>
2025-08-29 14:58:01 +0200

    Filter out personal material lists

--- api/src/Repository/MaterialListRepository.php
+++ api/src/Repository/MaterialListRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Camp;
 use App\Entity\MaterialList;
 use App\Entity\User;
+use App\Entity\UserCamp;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
@@ -16,14 +18,32 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method MaterialList[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class MaterialListRepository extends ServiceEntityRepository implements CanFilterByUserInterface {
-    use FiltersByCampCollaboration;
-
     public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, MaterialList::class);
     }
 
     public function filterByUser(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, User $user): void {
         $rootAlias = $queryBuilder->getRootAliases()[0];
-        $this->filterByCampCollaborationOrPublic($queryBuilder, $user, "{$rootAlias}.camp");
+
+        $campsQry = $queryBuilder->getEntityManager()->createQueryBuilder();
+        $campsQry->select('identity(uc.camp)');
+        $campsQry->from(UserCamp::class, 'uc');
+        $campsQry->where('uc.user = :current_user');
+
+        $publicCampsQry = $queryBuilder->getEntityManager()->createQueryBuilder();
+        $publicCampsQry->select('c');
+        $publicCampsQry->from(Camp::class, 'c');
+        $publicCampsQry->where($queryBuilder->expr()->orX('c.isPrototype = true', 'c.isShared = true'));
+
+        $queryBuilder->andWhere(
+            $queryBuilder->expr()->orX(
+                $queryBuilder->expr()->andX(
+                    "{$rootAlias}.campCollaboration IS NULL",
+                    $queryBuilder->expr()->in("{$rootAlias}.camp", $publicCampsQry->getDQL())
+                ),
+                $queryBuilder->expr()->in("{$rootAlias}.camp", $campsQry->getDQL())
+            )
+        );
+        $queryBuilder->setParameter('current_user', $user);
     }
 }
