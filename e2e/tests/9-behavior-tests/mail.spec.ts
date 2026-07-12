94c43304d932f296574ec5b68ef2c6443e896e73
BacLuc <lucius.bachmann@clubpage.ch>
2026-07-12 17:13:49 +0200

    mail: add healthcheck and e2e test
    With a new version of mail the missing trailing slash will lead to a 404.
    
    Signed-off-by: BacLuc <lucius.bachmann@clubpage.ch>

---
+++ e2e/tests/9-behavior-tests/mail.spec.ts
@@ -0,0 +1,6 @@
+import { test } from '@/utils/etest'
+import { expect } from '@playwright/test'
+
+test('mail service responds with 200', async ({ page }) => {
+  await expect(await page.request.get('/mail')).toBeOK()
+})
