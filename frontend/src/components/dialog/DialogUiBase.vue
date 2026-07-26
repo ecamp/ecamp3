5e6e83381ad902514812ba11379df7bea69912c9
Manuel Meister <news.manuelsworld@gmail.com>
2026-07-26 16:22:59 +0200

    Fix static translations

--- frontend/src/components/dialog/DialogUiBase.vue
+++ frontend/src/components/dialog/DialogUiBase.vue
@@ -1,31 +1,17 @@
 <script>
-import { componentI18n } from '@/plugins/i18n/index.js'
-
 export default {
   name: 'DialogUiBase',
   props: {
     modelValue: { type: Boolean, required: true },
     submitAction: { type: Function, default: null, required: false },
     submitIcon: { type: String, default: 'mdi-send-variant', required: false },
-    submitLabel: {
-      type: String,
-      default: function () {
-        return componentI18n.t('global.button.submit')
-      },
-      required: false,
-    },
+    submitLabel: { type: String, default: null, required: false },
     submitColor: { type: String, default: 'primary', required: false },
     submitEnabled: { type: Boolean, default: true, required: false },
 
     cancelAction: { type: Function, default: null, required: false },
     cancelIcon: { type: String, default: 'mdi-window-close', required: false },
-    cancelLabel: {
-      type: String,
-      default: function () {
-        return componentI18n.t('global.button.cancel')
-      },
-      required: false,
-    },
+    cancelLabel: { type: String, default: null, required: false },
     cancelColor: { type: String, default: 'secondary', required: false },
     cancelEnabled: { type: Boolean, default: true, required: false },
     cancelVisible: { type: Boolean, default: true, required: false },
@@ -41,6 +27,14 @@ export default {
     return {
       isSaving: false,
     }
+  },
+  computed: {
+    submitLabelOrDefault() {
+      return this.submitLabel ?? this.$t('global.button.submit')
+    },
+    cancelLabelOrDefault() {
+      return this.cancelLabel ?? this.$t('global.button.cancel')
+    },
   },
 }
 </script>
