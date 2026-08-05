4e75c0db902abaf2a7ecbcb2eab452340874a3e4
BacLuc <lucius.bachmann@clubpage.ch>
2026-08-05 21:40:15 +0200

    e2e: also delete camp in create-category after it was used

--- e2e/utils/fixtures/pageObjects/camp/admin/campInfo.ts
+++ e2e/utils/fixtures/pageObjects/camp/admin/campInfo.ts
@@ -1,4 +1,6 @@
 import { expect, Locator, Page } from '@playwright/test'
+import { boxedStep } from '@/utils/decorators/boxedStep'
+import { DialogDeleteCamp } from '@/utils/fixtures/pageObjects/camp/admin/dialogDeleteCamp'
 
 export class CampInfo {
   static readonly ROUTE = '/admin/info'
@@ -17,6 +19,24 @@ export class CampInfo {
 
   async loaded() {
     await expect(this._titleField).toBeVisible()
+  }
+
+  @boxedStep
+  async openDeleteDialog() {
+    await this._page
+      .locator('.v-expansion-panel')
+      .filter({ hasText: 'Gefahrenzone' })
+      .locator('.v-expansion-panel-title')
+      .click()
+
+    await this._page
+      .locator('.v-expansion-panel-text')
+      .getByRole('button', { name: /Löschen/i })
+      .click()
+
+    const dialog = new DialogDeleteCamp(this._page)
+    await dialog.loaded()
+    return dialog
   }
 
   get campId(): string {
