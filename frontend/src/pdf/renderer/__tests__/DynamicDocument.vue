9808eef54de4f908195f3087ac394b56859de04b
carlobeltrame <carlo@beltra.me>
2026-09-13 01:29:06 +0200

    Adapt to breaking changes in react-pdf layout upgrade

---
+++ frontend/src/pdf/renderer/__tests__/DynamicDocument.vue
@@ -0,0 +1,20 @@
+<template>
+  <Document>
+    <Page>
+      <Text :render="({ pageNumber, totalPages }) => `${pageNumber} / ${totalPages}`" />
+      <Text :render="() => $toc.marked ?? ''" />
+      <View
+        :render="
+          ({ pageNumber }) => {
+            $toc.marked = pageNumber
+          }
+        "
+      />
+    </Page>
+  </Document>
+</template>
+<script>
+export default {
+  name: 'DynamicDocument',
+}
+</script>
